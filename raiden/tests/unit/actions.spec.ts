70d73f8f9b775dcf4dd8f1d57a8c59e2c66a07fd
André Vitor de Lima Matos <andre.vmatos@gmail.com>
2019-03-19 19:51:35 -0300

    Add actions.test.ts for action factories not yet tested

---
+++ raiden/tests/unit/actions.spec.ts
@@ -0,0 +1,67 @@
+import {
+  RaidenActionType,
+  channelDeposit,
+  channelDepositFailed,
+  channelMonitor,
+  tokenMonitor,
+  tokenMonitorFailed,
+} from 'raiden/store';
+import { bigNumberify } from 'raiden/store/types';
+
+describe('action factories not tested in reducers.spec.ts', () => {
+  test('tokenMonitor', () => {
+    const token = '0xtoken';
+    expect(tokenMonitor(token)).toEqual({
+      type: RaidenActionType.TOKEN_MONITOR,
+      token,
+    });
+  });
+
+  test('tokenMonitorFailed', () => {
+    const token = '0xtoken',
+      error = new Error('tokenNetwork not found');
+    expect(tokenMonitorFailed(token, error)).toEqual({
+      type: RaidenActionType.TOKEN_MONITOR_FAILED,
+      token,
+      error,
+    });
+  });
+
+  test('channelMonitor', () => {
+    const tokenNetwork = '0xtokenNetwork',
+      partner = '0xpartner',
+      id = 12,
+      fromBlock = 5123;
+    expect(channelMonitor(tokenNetwork, partner, id, fromBlock)).toEqual({
+      type: RaidenActionType.CHANNEL_MONITOR,
+      tokenNetwork,
+      partner,
+      id,
+      fromBlock,
+    });
+  });
+
+  test('channelDeposit', () => {
+    const tokenNetwork = '0xtokenNetwork',
+      partner = '0xpartner',
+      deposit = bigNumberify(999);
+    expect(channelDeposit(tokenNetwork, partner, deposit)).toEqual({
+      type: RaidenActionType.CHANNEL_DEPOSIT,
+      tokenNetwork,
+      partner,
+      deposit,
+    });
+  });
+
+  test('channelDepositFailed', () => {
+    const tokenNetwork = '0xtokenNetwork',
+      partner = '0xpartner',
+      error = new Error('not enough funds');
+    expect(channelDepositFailed(tokenNetwork, partner, error)).toEqual({
+      type: RaidenActionType.CHANNEL_DEPOSIT_FAILED,
+      tokenNetwork,
+      partner,
+      error,
+    });
+  });
+});
