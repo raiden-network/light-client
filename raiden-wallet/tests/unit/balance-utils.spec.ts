1bd885cac93ecd5cd23ef765ed0cd30e7094b516
Konstantinos Paparas <kelsos86@gmail.com>
2019-03-19 14:08:42 +0100

    Adds more tests for balance utils

--- raiden-wallet/tests/unit/balance-utils.spec.ts
+++ raiden-wallet/tests/unit/balance-utils.spec.ts
@@ -31,11 +31,37 @@ describe('BalanceUtils', () => {
     expect(BalanceUtils.decimalsOverflow('1.00001', token)).toBe(false);
   });
 
+  it('should return false if the number (comma) of decimals is greater than the token supported and the integer part is non-zero', function() {
+    expect(BalanceUtils.decimalsOverflow('1,00001', token)).toBe(false);
+  });
+
   it('should return false if the number is integer', function() {
     expect(BalanceUtils.decimalsOverflow('100', token)).toBe(false);
   });
 
   it('should return false if the number is zero', function() {
     expect(BalanceUtils.decimalsOverflow('0', token)).toBe(false);
+  });
+
+  it('should return false if the deposit balance is greater than tha available balance', function() {
+    expect(
+      BalanceUtils.hasBalance('0.1', {
+        units: '0.01',
+        balance: BalanceUtils.parse('0.01', 18),
+        decimals: 18,
+        address: '0xtoken'
+      })
+    ).toBe(false);
+  });
+
+  it('should return true if the account balance is less than the available balance', function() {
+    expect(
+      BalanceUtils.hasBalance('0.001', {
+        units: '0.01',
+        balance: BalanceUtils.parse('0.01', 18),
+        decimals: 18,
+        address: '0xtoken'
+      })
+    ).toBe(true);
   });
 });
