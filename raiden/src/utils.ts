c1f770da29f27bfe3e99867ad5118e48e77c498c
André Vitor de Lima Matos <andre.vmatos@gmail.com>
2019-03-19 19:08:20 -0300

    Some minor fixes, alias import paths from 'raiden/*' to src/

--- raiden/src/utils.ts
+++ raiden/src/utils.ts
@@ -1,11 +1,9 @@
 import { Contract, EventFilter, Event } from 'ethers';
 import { Provider, JsonRpcProvider, Listener } from 'ethers/providers';
-import { flatten } from 'lodash';
+import { flatten, sortBy } from 'lodash';
 
 import { Observable, fromEventPattern, merge, from } from 'rxjs';
-import { filter, first, map, mergeAll, switchMap, withLatestFrom } from 'rxjs/operators';
-
-import { TokenNetwork } from '../contracts/TokenNetwork';
+import { filter, first, map, mergeAll, switchMap, withLatestFrom, mergeMap } from 'rxjs/operators';
 
 /**
  * Like rxjs' fromEvent, but event can be an EventFilter
@@ -23,15 +21,13 @@ export function fromEthersEvent<T>(
 }
 
 /**
- * getEventsStream returns a stream of T-type tuples (arrays) from TokenNetwork's
+ * getEventsStream returns a stream of T-type tuples (arrays) from Contract's
  * events from filters. These events are polled since provider's [re]setEventsBlock to newest
- * polled block. If 'fromBlock$' and 'lastSeenBlock$' are specified, also fetch past events
+ * polled block. If both 'fromBlock$' and 'lastSeenBlock$' are specified, also fetch past events
  * since fromBlock up to lastSeenBlock$ === provider.resetEventsBlock - 1
  * T must be a tuple-like type receiving all filters arguments plus the respective Event in the end
- * It also checks if there's already an stream for given filters, and return a completed/empty
- * Observable then, to avoid dupicate-processing events
  *
- * @param tokenNetworkContract  TokenNetwork contract connected to a provider
+ * @param contract  Contract source instance for filters, connected to a provider
  * @param filters  array of OR filters from tokenNetwork
  * @param fromBlock$  Observable of a past blockNumber since when to fetch past events
  * @param lastSeenBlock$  Observable of latest seen block, to be used as toBlock of pastEvents.
@@ -40,17 +36,15 @@ export function fromEthersEvent<T>(
  */
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function getEventsStream<T extends any[]>(
-  tokenNetworkContract: TokenNetwork,
+  contract: Contract,
   filters: EventFilter[],
   fromBlock$?: Observable<number>,
   lastSeenBlock$?: Observable<number>,
 ): Observable<T> {
-  const provider = tokenNetworkContract.provider as JsonRpcProvider;
+  const provider = contract.provider as JsonRpcProvider;
   // filters for channels opened by and with us
-  const newEvents$: Observable<T> = merge(
-    ...filters.map(filter =>
-      fromEthersEvent(tokenNetworkContract, filter, (...args) => args as T),
-    ),
+  const newEvents$: Observable<T> = from(filters).pipe(
+    mergeMap(filter => fromEthersEvent(contract, filter, (...args) => args as T)),
   );
 
   let pastEvents$: Observable<T> | undefined = undefined;
@@ -66,13 +60,13 @@ export function getEventsStream<T extends any[]>(
         );
         // flatten array of each getLogs query response and sort them
         // emit log array elements as separate logs into stream
-        return from(flatten(logs).sort((a, b) => (a.blockNumber || 0) - (b.blockNumber || 0)));
+        return from(sortBy(flatten(logs), ['blockNumber']));
       }),
       mergeAll(), // async return above will be a Promise of an Observable, so unpack inner$
       map(log => {
         // parse log into [...args, event: Event] array,
         // the same that contract.on events/callbacks
-        const parsed = tokenNetworkContract.interface.parseLog(log);
+        const parsed = contract.interface.parseLog(log);
         if (!parsed) return;
         const args = Array.prototype.slice.call(parsed.values);
         // not all parameters quite needed right now, but let's comply with the interface
