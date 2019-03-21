f6513c34cea9a2b16df2f6d08decb017d80fc269
André Vitor de Lima Matos <andre.vmatos@gmail.com>
2019-03-21 20:30:07 -0300

    Add ganache

---
+++ raiden/tests/types.d.ts
@@ -0,0 +1,41 @@
+declare module 'ganache-cli' {
+  import { Http2Server } from 'http2';
+  import { AsyncSendable } from 'ethers/providers';
+
+  export interface GanacheServerOptions {
+    accounts?: { balance: string; secretKey?: string }[];
+    debug?: boolean;
+    blockTime?: number;
+    logger?: Console;
+    mnemonic?: string;
+    port?: number;
+    seed?: string;
+    default_balance_ether?: number;
+    total_accounts?: number;
+    fork?: string;
+    fork_block_number?: string | number;
+    network_id?: number;
+    time?: Date;
+    locked?: boolean;
+    unlocked_accounts?: string[];
+    db_path?: string;
+    db?: any; // eslint-disable-line
+    ws?: boolean;
+    account_keys_path?: string;
+    vmErrorsOnRPCResponse?: boolean;
+    hdPath?: string;
+    hardfork?: string;
+    allowUnlimitedContractSize?: boolean;
+    gasPrice?: string;
+    gasLimit?: string;
+    keepAliveTimeout?: number;
+  }
+
+  export interface Ganache {
+    provider(options?: GanacheServerOptions): AsyncSendable;
+    server(options?: GanacheServerOptions): Http2Server;
+  }
+
+  const ganache: Ganache;
+  export default ganache;
+}
