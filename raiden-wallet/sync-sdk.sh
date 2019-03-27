ab1ee35eacd18937127244690504e4053de3cf33
Konstantinos Paparas <kelsos86@gmail.com>
2019-03-27 18:20:20 +0100

    Excludes node_modules from rsync
    Otherwise when running on CI there are some errors.

--- raiden-wallet/sync-sdk.sh
+++ raiden-wallet/sync-sdk.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
 cd ../raiden
+rm -rf ./dist
 npm install
 npm run build
 cd ../raiden-wallet
+if [[ "$CI" ]]
+then
+    echo 'Installing SDK dependencies'
+    npm install io-ts redux-observable redux redux-logger typechain
+fi
 npm install
 rm -rf ./node_modules/raiden
-rsync --stats -aAvX ../raiden/* node_modules/raiden
+
+if [[ "$CI" ]]
+then
+    echo 'rsync excluding node_modules'
+    rsync --stats -aAvX --exclude 'node_modules' ../raiden/* node_modules/raiden
+else
+    echo 'rsync including node_modules'
+    rsync --stats -aAvX ../raiden/* node_modules/raiden
+fi
+
