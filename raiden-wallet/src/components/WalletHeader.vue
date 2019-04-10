68951bf2fc45fa46ba5a4a2604a3c0ebdf43fd8c
Konstantinos Paparas <kelsos86@gmail.com>
2019-04-10 10:43:18 +0200

    Applies new background and header design

--- raiden-wallet/src/components/WalletHeader.vue
+++ raiden-wallet/src/components/WalletHeader.vue
@@ -1,49 +1,43 @@
 <template>
-  <v-layout
-    class="header"
-    justify-center
-    align-center
-    v-if="!loading && defaultAccount"
-  >
-    <v-flex lg6 md8 xs12>
-      <div id="header-content">
-        <div>
-          <v-img
-            class="logo"
-            height="70"
-            width="70"
-            contain
-            aspect-ratio="1"
-            :src="require('../assets/logo.svg')"
-          ></v-img>
-        </div>
-        <div>
-          <div class="headline">Raiden Wallet</div>
-          <div class="font-weight-light">{{ defaultAccount | truncate }}</div>
-        </div>
-        <v-spacer></v-spacer>
-        <div>
-          <div class="subheading">Balance</div>
-          <div id="balance">
-            <span class="font-weight-light">{{
-              accountBalance | decimals
-            }}</span>
-            <sup>ETHER</sup>
+  <div>
+    <v-layout
+      class="header"
+      justify-center
+      align-center
+      v-if="!loading && defaultAccount"
+    >
+      <v-flex lg12 md12 xs12>
+        <div id="header-content">
+          <div class="navigation-button"></div>
+          <v-spacer></v-spacer>
+          <div class="raiden-wallet">Raiden Wallet</div>
+          <v-spacer></v-spacer>
+          <div>
+            <v-img
+              height="36"
+              width="36"
+              contain
+              aspect-ratio="1"
+              class="blockie"
+              :src="blockie"
+            ></v-img>
           </div>
         </div>
-        <div>
-          <v-img
-            height="70"
-            width="70"
-            contain
-            aspect-ratio="1"
-            class="blockie"
-            :src="blockie"
-          ></v-img>
+      </v-flex>
+    </v-layout>
+    <v-layout class="row-2" align-center>
+      <v-flex lg6 md6 xs6>
+        <div class="address text-xs-left">
+          {{ defaultAccount | truncate }}
         </div>
-      </div>
-    </v-flex>
-  </v-layout>
+      </v-flex>
+      <v-flex lg6 md6 xs6>
+        <div class="balance text-xs-right">
+          {{ accountBalance | decimals }} ETHER
+        </div>
+      </v-flex>
+    </v-layout>
+  </div>
 </template>
 
 <script lang="ts">
@@ -67,18 +61,55 @@ export default class WalletHeader extends Vue {
 <style scoped lang="scss">
 .blockie {
   border-radius: 50%;
+  box-sizing: border-box;
+  height: 36px;
+  width: 36px;
+  border: 1px solid #979797;
+  background-color: #d8d8d8;
+}
+.navigation-button {
+  width: 36px;
+  height: 36px;
 }
 
-.logo {
-  filter: invert(100%);
+.raiden-wallet {
+  color: #ffffff;
+  font-family: Roboto, sans-serif;
+  font-size: 24px;
+  line-height: 28px;
+  text-align: center;
 }
+
+.address {
+  color: #ffffff;
+  font-family: Roboto, sans-serif;
+  font-size: 16px;
+  line-height: 19px;
+}
+
+.balance {
+  color: #ffffff;
+  font-family: Roboto, sans-serif;
+  font-size: 16px;
+  line-height: 19px;
+}
+
 .header {
-  color: #fff;
-  max-height: 120px;
-  background: black;
-  padding-top: 20px;
-  padding-bottom: 20px;
+  height: 80px;
+  width: 620px;
+  border-radius: 14px;
+  background-color: #141414;
+  box-shadow: 5px 5px 15px 0 rgba(0, 0, 0, 0.3);
 }
+
+$row-horizontal-padding: 34px;
+.row-2 {
+  padding-left: $row-horizontal-padding;
+  padding-right: $row-horizontal-padding;
+  height: 40px;
+  background-color: #323232;
+}
+
 #balance sup {
   padding-left: 4px;
 }
