5e722d41f06bad838a9749d55eb43de560f3d629
Konstantinos Paparas <kelsos86@gmail.com>
2019-04-29 18:00:43 +0200

    Adds initial hub selection functionality

--- raiden-wallet/src/views/Connect.vue
+++ raiden-wallet/src/views/Connect.vue
@@ -1,6 +1,7 @@
 <template>
   <v-container>
     <select-token v-if="!token"></select-token>
+    <select-hub :token="tokenInfo" v-else-if="!partner"></select-hub>
     <deposit
       v-else
       :token="token"
@@ -15,9 +16,10 @@ import { Component, Vue } from 'vue-property-decorator';
 import Deposit from '@/components/Deposit.vue';
 import { Token, TokenPlaceholder } from '@/model/types';
 import SelectToken from '@/components/SelectToken.vue';
+import SelectHub from '@/components/SelectHub.vue';
 
 @Component({
-  components: { SelectToken, Deposit }
+  components: { SelectHub, SelectToken, Deposit }
 })
 export default class Connect extends Vue {
   token: string = '';
@@ -27,30 +29,14 @@ export default class Connect extends Vue {
   async created() {
     const route = this.$router.currentRoute;
     const params = route.params;
-    // if (!params.partner) {
-    //   this.$router.push({
-    //     name: 'connect',
-    //     params: {
-    //       token: '0xd0A1E359811322d97991E03f863a0C30C2cF029C',
-    //       partner: '0x1D36124C90f53d491b6832F1c073F43E2550E35b'
-    //     }
-    //   });
-    // }
-  }
-
-  async mounted() {
-    const route = this.$router.currentRoute;
-    const params = route.params;
     this.token = params.token;
     this.partner = params.partner;
 
-    // this.tokenInfo = await this.$raiden.getToken(this.token);
+    if (this.token) {
+      this.tokenInfo = await this.$raiden.getToken(this.token);
+    }
   }
 }
 </script>
 
-<style lang="scss" scoped>
-.container {
-  height: calc(100% - 120px);
-}
-</style>
+<style lang="scss" scoped></style>
