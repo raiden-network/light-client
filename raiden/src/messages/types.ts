41a878390c7d847d551df110498581c7876f410a
André Vitor de Lima Matos <andre.vmatos@gmail.com>
2019-06-05 01:58:46 -0300

    First messages data types and codecs

---
+++ raiden/src/messages/types.ts
@@ -0,0 +1,122 @@
+/* eslint-disable @typescript-eslint/camelcase */
+import * as t from 'io-ts';
+// import { ThrowReporter } from 'io-ts/lib/ThrowReporter';
+import { Address, BigNumberType, Hash, PositiveInt, Secret, Signature } from '../store/types';
+
+// types
+
+const SignedMessage = t.type({
+  signature: Signature,
+});
+
+const RetrieableMessage = t.type({
+  message_identifier: t.Int,
+});
+
+const SignedRetrieableMessage = t.intersection([SignedMessage, RetrieableMessage]);
+
+export const Delivered = t.intersection([
+  t.type({
+    type: t.literal('Delivered'),
+    delivered_message_identifier: t.Int,
+  }),
+  SignedMessage,
+]);
+export type Delivered = t.TypeOf<typeof Delivered>;
+
+export const Processed = t.intersection([
+  t.type({
+    type: t.literal('Processed'),
+  }),
+  SignedRetrieableMessage,
+]);
+export type Processed = t.TypeOf<typeof Processed>;
+
+export const SecretRequest = t.intersection([
+  t.type({
+    type: t.literal('SecretRequest'),
+    payment_identifier: t.Int,
+    secrethash: Hash,
+    amount: BigNumberType,
+    expiration: PositiveInt,
+  }),
+  SignedRetrieableMessage,
+]);
+export type SecretRequest = t.TypeOf<typeof SecretRequest>;
+
+export const RevealSecret = t.intersection([
+  t.type({
+    type: t.literal('RevealSecret'),
+    secret: Secret,
+  }),
+  SignedRetrieableMessage,
+]);
+export type RevealSecret = t.TypeOf<typeof RevealSecret>;
+
+const EnvelopeMessage = t.intersection([
+  t.type({
+    chain_id: PositiveInt,
+    nonce: PositiveInt,
+    transferred_amount: BigNumberType,
+    locked_amount: BigNumberType,
+    locksroot: Hash,
+    channel_identifier: PositiveInt,
+    token_network_address: Address,
+  }),
+  SignedRetrieableMessage,
+]);
+
+const Lock = t.type({
+  amount: BigNumberType,
+  expiration: PositiveInt,
+  secrethash: Hash,
+});
+
+const LockedTransferBase = t.intersection([
+  t.type({
+    payment_identifier: t.Int,
+    token: Address,
+    recipient: Address,
+    lock: Lock,
+    target: Address,
+    initiator: Address,
+    // fee: PositiveInt,
+  }),
+  EnvelopeMessage,
+]);
+
+export const LockedTransfer = t.intersection([
+  t.type({
+    type: t.literal('LockedTransfer'),
+  }),
+  LockedTransferBase,
+]);
+export type LockedTransfer = t.TypeOf<typeof LockedTransfer>;
+
+export const RefundTransfer = t.intersection([
+  t.type({
+    type: t.literal('RefundTransfer'),
+  }),
+  LockedTransferBase,
+]);
+export type RefundTransfer = t.TypeOf<typeof RefundTransfer>;
+
+export const Unlock = t.intersection([
+  t.type({
+    type: t.literal('Unlock'),
+    payment_identifier: t.Int,
+    secret: Secret,
+  }),
+  EnvelopeMessage,
+]);
+export type Unlock = t.TypeOf<typeof Unlock>;
+
+export const LockExpired = t.intersection([
+  t.type({
+    type: t.literal('LockExpired'),
+    recipient: Address,
+    secrethash: Hash,
+  }),
+  EnvelopeMessage,
+]);
+export type LockExpired = t.TypeOf<typeof LockExpired>;
--- raiden/src/store/types.ts
+++ raiden/src/store/types.ts
@@ -39,3 +39,53 @@ export interface ChannelId {
   tokenNetwork: string;
   partner: string;
 }
+
+/**
+ * Helper type to declare and validate an arbitrary or variable-sized hex bytestring
+ * Like a branded codec, but allows custom/per size sub-types
+ */
+export class HexBytes<S extends number | undefined = undefined> extends t.Type<string> {
+  public readonly _tag: 'HexString' = 'HexString';
+  public size: S;
+  private regex = /^0x[0-9a-f]*$/i;
+  public constructor(name?: string, size?: S) {
+    super(
+      name || (size ? `HexString<${size}>` : 'HexString'),
+      (u): u is string => typeof u === 'string' && !!u.match(this.regex),
+      (u, c) => (this.is(u) ? t.success(u) : t.failure(u, c)),
+      t.identity,
+    );
+    this.size = (size as unknown) as S;
+    if (typeof size === 'number' && size > 0) {
+      this.regex = new RegExp(`^0x[0-9a-f]{${size * 2}}$`, 'i');
+    }
+  }
+}
+
+// Positive & PositiveInt taken from io-ts Readme
+export interface PositiveBrand {
+  readonly Positive: unique symbol; // use `unique symbol` here to ensure uniqueness across modules / packages
+}
+
+export const Positive = t.brand(
+  t.number, // a codec representing the type to be refined
+  (n): n is t.Branded<number, PositiveBrand> => n >= 0, // a custom type guard using the build-in helper `Branded`
+  'Positive', // the name must match the readonly field in the brand
+);
+export type Positive = t.TypeOf<typeof Positive>;
+
+export const PositiveInt = t.intersection([t.Int, Positive]);
+export type PositiveInt = t.TypeOf<typeof PositiveInt>;
+
+export const Signature = new HexBytes('Signature', 65);
+export type Signature = t.TypeOf<typeof Signature>;
+
+export const Hash = new HexBytes('Hash', 32);
+export type Hash = t.TypeOf<typeof Hash>;
+
+export const Secret = new HexBytes();
+export type Secret = t.TypeOf<typeof Secret>;
+
+// TODO?: validate checksum
+export const Address = new HexBytes('Address', 20);
+export type Address = t.TypeOf<typeof Address>;
