375a3ad9fa1e6f5603f112f255183394fed0c61a
Continuous Delivery <no-reply@raiden.network>
2019-07-24 09:09:33 +0000

    Automated deployment to GitHub Pages: 027c1342d747ce5d997313aa2e127dbe14491f96 [ci skip]

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5f5530837e948b812071ce1052ee0a33.js"
+  "/precache-manifest.fd1be2beb28ada893a62b4c905de5260.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "raiden-dapp"});
