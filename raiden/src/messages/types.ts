4722e4008db914673320ae7aab00bef27622d54c
André Vitor de Lima Matos <andre.vmatos@gmail.com>
2019-07-31 16:40:54 -0300

    Make messages readonly

--- raiden/src/messages/types.ts
+++ raiden/src/messages/types.ts
@@ -25,126 +25,148 @@ export enum MessageType {
 export const MessageTypeC = new EnumType<MessageType>(MessageType, 'MessageType');
 
 // Mixin for all tagged messages
-export const Message = t.type({ type: MessageTypeC });
+export const Message = t.readonly(t.type({ type: MessageTypeC }));
 
 // Mixin of a message that contains an identifier and should be ack'ed with a respective Delivered
-const RetrieableMessage = t.intersection([
-  t.type({
-    message_identifier: UInt(8),
-  }),
-  Message,
-]);
+const RetrieableMessage = t.readonly(
+  t.intersection([
+    t.type({
+      message_identifier: UInt(8),
+    }),
+    Message,
+  ]),
+);
 
 // Acknowledges to the sender that a RetrieableMessage was received
-export const Delivered = t.intersection([
-  t.type({
-    type: t.literal(MessageType.DELIVERED),
-    delivered_message_identifier: UInt(8),
-  }),
-  Message,
-]);
+export const Delivered = t.readonly(
+  t.intersection([
+    t.type({
+      type: t.literal(MessageType.DELIVERED),
+      delivered_message_identifier: UInt(8),
+    }),
+    Message,
+  ]),
+);
 export interface Delivered extends t.TypeOf<typeof Delivered> {}
 
 // Confirms some message that required state validation was successfuly processed
-export const Processed = t.intersection([
-  t.type({
-    type: t.literal(MessageType.PROCESSED),
-  }),
-  RetrieableMessage,
-]);
+export const Processed = t.readonly(
+  t.intersection([
+    t.type({
+      type: t.literal(MessageType.PROCESSED),
+    }),
+    RetrieableMessage,
+  ]),
+);
 export interface Processed extends t.TypeOf<typeof Processed> {}
 
 // Requests the initiator to reveal the secret for a LockedTransfer targeted to us
-export const SecretRequest = t.intersection([
-  t.type({
-    type: t.literal(MessageType.SECRET_REQUEST),
-    payment_identifier: UInt(8),
-    secrethash: Hash,
-    amount: UInt(32),
-    expiration: UInt(32),
-  }),
-  RetrieableMessage,
-]);
+export const SecretRequest = t.readonly(
+  t.intersection([
+    t.type({
+      type: t.literal(MessageType.SECRET_REQUEST),
+      payment_identifier: UInt(8),
+      secrethash: Hash,
+      amount: UInt(32),
+      expiration: UInt(32),
+    }),
+    RetrieableMessage,
+  ]),
+);
 export interface SecretRequest extends t.TypeOf<typeof SecretRequest> {}
 
 // Reveal to the target or the previous hop a secret we just learned off-chain
-export const SecretReveal = t.intersection([
-  t.type({
-    type: t.literal(MessageType.SECRET_REVEAL),
-    secret: Secret,
-  }),
-  RetrieableMessage,
-]);
+export const SecretReveal = t.readonly(
+  t.intersection([
+    t.type({
+      type: t.literal(MessageType.SECRET_REVEAL),
+      secret: Secret,
+    }),
+    RetrieableMessage,
+  ]),
+);
 export interface SecretReveal extends t.TypeOf<typeof SecretReveal> {}
 
 // Mixin for messages containing a balance proof
-export const EnvelopeMessage = t.intersection([
-  t.type({
-    chain_id: UInt(32),
-    token_network_address: Address,
-    channel_identifier: UInt(32),
-    nonce: UInt(8),
-    transferred_amount: UInt(32),
-    locked_amount: UInt(32),
-    locksroot: Hash,
-  }),
-  RetrieableMessage,
-]);
+export const EnvelopeMessage = t.readonly(
+  t.intersection([
+    t.type({
+      chain_id: UInt(32),
+      token_network_address: Address,
+      channel_identifier: UInt(32),
+      nonce: UInt(8),
+      transferred_amount: UInt(32),
+      locked_amount: UInt(32),
+      locksroot: Hash,
+    }),
+    RetrieableMessage,
+  ]),
+);
 
 // base for locked and refund transfer, they differentiate only on the type tag
-const LockedTransferBase = t.intersection([
-  t.type({
-    payment_identifier: UInt(8),
-    token: Address,
-    recipient: Address,
-    lock: Lock,
-    target: Address,
-    initiator: Address,
-    fee: UInt(32),
-  }),
-  EnvelopeMessage,
-]);
+const LockedTransferBase = t.readonly(
+  t.intersection([
+    t.type({
+      payment_identifier: UInt(8),
+      token: Address,
+      recipient: Address,
+      lock: Lock,
+      target: Address,
+      initiator: Address,
+      fee: UInt(32),
+    }),
+    EnvelopeMessage,
+  ]),
+);
 
 // a mediated transfer containing a locked amount
-export const LockedTransfer = t.intersection([
-  t.type({
-    type: t.literal(MessageType.LOCKED_TRANSFER),
-  }),
-  LockedTransferBase,
-]);
+export const LockedTransfer = t.readonly(
+  t.intersection([
+    t.type({
+      type: t.literal(MessageType.LOCKED_TRANSFER),
+    }),
+    LockedTransferBase,
+  ]),
+);
 export interface LockedTransfer extends t.TypeOf<typeof LockedTransfer> {}
 
 // if a mediated transfer didn't succeed, mediator can refund the amount with the same secrethash
 // so the previous hop can retry it with another neighbor
-export const RefundTransfer = t.intersection([
-  t.type({
-    type: t.literal(MessageType.REFUND_TRANSFER),
-  }),
-  LockedTransferBase,
-]);
+export const RefundTransfer = t.readonly(
+  t.intersection([
+    t.type({
+      type: t.literal(MessageType.REFUND_TRANSFER),
+    }),
+    LockedTransferBase,
+  ]),
+);
 export interface RefundTransfer extends t.TypeOf<typeof RefundTransfer> {}
 
 // when the secret is revealed, unlock sends a new balance proof without the lock and increasing
 // the total transfered to finish the offchain transfer
-export const Unlock = t.intersection([
-  t.type({
-    type: t.literal(MessageType.UNLOCK),
-    payment_identifier: UInt(8),
-    secret: Secret,
-  }),
-  EnvelopeMessage,
-]);
+export const Unlock = t.readonly(
+  t.intersection([
+    t.type({
+      type: t.literal(MessageType.UNLOCK),
+      payment_identifier: UInt(8),
+      secret: Secret,
+    }),
+    EnvelopeMessage,
+  ]),
+);
 export interface Unlock extends t.TypeOf<typeof Unlock> {}
 
 // after mediated transfer fails and the lock expire, clean it from the locks tree
-export const LockExpired = t.intersection([
-  t.type({
-    type: t.literal(MessageType.LOCK_EXPIRED),
-    recipient: Address,
-    secrethash: Hash,
-  }),
-  EnvelopeMessage,
-]);
+export const LockExpired = t.readonly(
+  t.intersection([
+    t.type({
+      type: t.literal(MessageType.LOCK_EXPIRED),
+      recipient: Address,
+      secrethash: Hash,
+    }),
+    EnvelopeMessage,
+  ]),
+);
 export interface LockExpired extends t.TypeOf<typeof LockExpired> {}
 
 export type Message =
@@ -168,12 +190,16 @@ export const Signed = memoize<
   ) => t.IntersectionC<
     [
       C,
-      t.TypeC<{
-        signature: typeof Signature;
-      }>,
+      t.ReadonlyC<
+        t.TypeC<{
+          signature: typeof Signature;
+        }>
+      >,
     ]
   >
->(<C extends t.Mixed>(codec: C) => t.intersection([codec, t.type({ signature: Signature })]));
+>(<C extends t.Mixed>(codec: C) =>
+  t.intersection([codec, t.readonly(t.type({ signature: Signature }))]),
+);
 export type Signed<M extends Message> = M & { signature: Signature };
 
 export const SignedMessageCodecs: { readonly [T in MessageType]: t.Mixed } = {
