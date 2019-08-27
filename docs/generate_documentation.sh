9fab59ea5a41b2c9a1194af0c19f0a96c5ef3789
Konstantinos Paparas <kelsos86@gmail.com>
2019-08-27 12:45:53 +0200

    Adds automatic documentation generation

---
+++ docs/generate_documentation.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+cd ../raiden-ts
+
+if [[ -d './docs' ]]
+then
+ rm -rf docs
+fi
+
+npm run docs:generate
+
+[[ -d 'docs/.vuepress' ]] || mkdir docs/.vuepress
+[[ -d 'docs/.vuepress/styles' ]] || mkdir docs/.vuepress/styles
+
+cp ../docs/config.js docs/.vuepress/config.js
+cp ../docs/palette.styl docs/.vuepress/styles/palette.styl
+
+npm run docs:build
+
+if [[ ! -d '../raiden-dapp/dist/' ]]
+then
+    echo 'The dApp dist directory was not available'
+    exit 1;
+fi
+
+[[ ! -d '../raiden-dapp/dist/docs' ]] || rm -rf ../raiden-dapp/dist/docs
+
+cp -r docs/.vuepress/dist ../raiden-dapp/dist/docs
