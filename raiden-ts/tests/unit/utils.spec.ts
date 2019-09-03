0377d865d4ccaf9abbd1c7e4b561f075c1be3ebe
André Vitor de Lima Matos <andre.vmatos@gmail.com>
2019-09-03 12:31:03 -0300

    Empty locksroot is keccak256([]), not HashZero

--- raiden-ts/tests/unit/utils.spec.ts
+++ raiden-ts/tests/unit/utils.spec.ts
@@ -293,7 +293,9 @@ test('rxjs splitCombined', async () => {
 
 describe('messages', () => {
   test('getLocksroot', () => {
-    expect(getLocksroot([])).toBe(HashZero);
+    expect(getLocksroot([])).toBe(
+      '0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470',
+    );
     const locks: Lock[] = [
       {
         amount: bigNumberify(1) as UInt<32>,
