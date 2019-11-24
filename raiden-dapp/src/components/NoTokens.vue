0011f95835097f0c803ea67b426d4653804dea49
taleldayekh <mail@talel.se>
2019-11-24 22:20:44 +0100

    Component for adding new tokens

---
+++ raiden-dapp/src/components/NoTokens.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-container class="new-token">
+    <v-col class="new-token__button">
+      <v-btn fab color="primary" @click="navigateToTokenSelect()">
+        <v-icon large>mdi-plus</v-icon>
+      </v-btn>
+    </v-col>
+    <v-col class="new-token__header">
+      {{ $t('tokens.connect-new') }}
+    </v-col>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { Component, Mixins } from 'vue-property-decorator';
+import NavigationMixin from '@/mixins/navigation-mixin';
+
+@Component({})
+export default class NoTokens extends Mixins(NavigationMixin) {}
+</script>
+
+<style lang="scss" scoped>
+.new-token {
+  text-align: center;
+}
+
+.new-token__button {
+  padding: 0;
+}
+
+.new-token__header {
+  padding: 15px 0px 35px 0px;
+}
+</style>
---
+++ raiden-dapp/tests/unit/components/app-core.spec.ts
@@ -0,0 +1,2 @@
+// Should displat no tokens component if list of connected tokens is empty
+// should redirect to transfer if tokes are availble
