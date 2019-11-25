facd79dabd6d91768b9b093ec3b92819b1817bf6
Konstantinos Paparas <kelsos86@gmail.com>
2019-11-25 14:44:12 +0100

    Fixes error message display and skips mint if user has balance

--- raiden-dapp/tests/unit/components/mint-deposit-dialog.spec.ts
+++ raiden-dapp/tests/unit/components/mint-deposit-dialog.spec.ts
@@ -6,6 +6,7 @@ import { Zero } from 'ethers/constants';
 import { Token } from '@/model/types';
 import store from '@/store/index';
 import flushPromises from 'flush-promises';
+import { bigNumberify } from 'ethers/utils';
 
 Vue.use(Vuetify);
 
@@ -60,12 +61,32 @@ describe('MintDepositDialog.vue', () => {
   });
 
   test('minting fails', async () => {
-    expect.assertions(3);
+    expect.assertions(4);
     $raiden.mint.mockRejectedValueOnce(new Error('error'));
     wrapper.find('.mint-deposit-dialog__action button').trigger('click');
     await flushPromises();
     expect($raiden.mint).toHaveBeenCalledTimes(1);
     expect($raiden.depositToUDC).toHaveBeenCalledTimes(0);
-    expect(wrapper.find('.error--text').exists()).toBe(true);
+    const errorText = wrapper.find('.error--text');
+    expect(errorText.exists()).toBe(true);
+    expect(errorText.text()).toMatch('error');
+  });
+
+  test('user has already enough tokens', async () => {
+    expect.assertions(3);
+    store.commit('updateTokens', {
+      '0x3a989D97388a39A0B5796306C615d10B7416bE77': {
+        ...token,
+        balance: bigNumberify('10000000000000000000')
+      }
+    });
+
+    $raiden.mint.mockResolvedValueOnce(undefined);
+    $raiden.depositToUDC.mockResolvedValueOnce(undefined);
+    wrapper.find('.mint-deposit-dialog__action button').trigger('click');
+    await flushPromises();
+    expect($raiden.mint).toHaveBeenCalledTimes(0);
+    expect($raiden.depositToUDC).toHaveBeenCalledTimes(1);
+    expect(wrapper.emitted()['done']).toHaveLength(1);
   });
 });
