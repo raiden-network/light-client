fefac906dd6b7e76fef6c6b7daa82c8865bf8b84
Talel Dayekh <43838780+taleldayekh@users.noreply.github.com>
2019-11-26 17:48:32 +0100

    Adds the new start screen

--- raiden-dapp/src/components/AppCore.vue
+++ raiden-dapp/src/components/AppCore.vue
@@ -1,13 +1,25 @@
 <template>
-  <tokens />
+  <no-tokens v-if="!tokens.length" />
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import Tokens from '@/components/Tokens.vue';
+import { Component, Mixins } from 'vue-property-decorator';
+import { mapGetters } from 'vuex';
+import { TokenModel } from '@/model/types';
+import NavigationMixin from '@/mixins/navigation-mixin';
+import NoTokens from '@/components/NoTokens.vue';
 
 @Component({
-  components: { Tokens }
+  computed: mapGetters(['tokens']),
+  components: { NoTokens }
 })
-export default class AppCore extends Vue {}
+export default class AppCore extends Mixins(NavigationMixin) {
+  tokens!: TokenModel[];
+
+  mounted() {
+    if (this.tokens.length) {
+      this.navigateToSelectTransferTarget(this.tokens[0].address);
+    }
+  }
+}
 </script>
