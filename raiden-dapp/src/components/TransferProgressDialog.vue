bd53c2e5d3886c40cfa9580382326e5d935f8de8
Konstantinos Paparas <kelsos86@gmail.com>
2019-12-05 18:07:03 +0100

    Extracts RaidenDialog.vue to wrap the dialog functionality

--- raiden-dapp/src/components/TransferProgressDialog.vue
+++ raiden-dapp/src/components/TransferProgressDialog.vue
@@ -1,73 +1,71 @@
 <template>
-  <v-card class="transfer-progress-dialog">
-    <v-btn icon class="transfer-progress-dialog__close" @click="dismiss">
-      <v-icon>mdi-close</v-icon>
-    </v-btn>
-    <v-card-text>
-      <v-row
-        class="transfer-progress-dialog__title"
-        align="center"
-        justify="center"
-      >
-        <v-col cols="12">
+  <raiden-dialog class="transfer-progress-dialog" @close="dismiss">
+    <v-row
+      class="transfer-progress-dialog__title"
+      align="center"
+      justify="center"
+    >
+      <v-col cols="12">
+        <span v-if="error">
+          {{ $t('transfer.error.title') }}
+        </span>
+        <span v-else-if="inProgress">
+          {{ $t('transfer.steps.transfer.title') }}
+        </span>
+        <span v-else>
+          {{ $t('transfer.steps.done.title') }}
+        </span>
+      </v-col>
+    </v-row>
+    <v-row align="center" justify="center">
+      <v-col cols="6">
+        <div v-if="error">
+          <v-img
+            :src="require('../assets/error.png')"
+            class="transfer-progress-dialog--error"
+          ></v-img>
+        </div>
+        <div v-else-if="!inProgress">
+          <v-img
+            :src="require('../assets/done.svg')"
+            class="transfer-progress-dialog--done"
+          ></v-img>
+        </div>
+        <v-progress-circular
+          v-else
+          :size="125"
+          :width="4"
+          color="primary"
+          class="transfer-progress-dialog--progress"
+          indeterminate
+        ></v-progress-circular>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12">
+        <div class="transfer-progress-dialog__description">
           <span v-if="error">
-            {{ $t('transfer.error.title') }}
+            {{ error }}
           </span>
           <span v-else-if="inProgress">
-            {{ $t('transfer.steps.transfer.title') }}
+            {{ $t('transfer.steps.transfer.description') }}
           </span>
           <span v-else>
-            {{ $t('transfer.steps.done.title') }}
+            {{ $t('transfer.steps.done.description') }}
           </span>
-        </v-col>
-      </v-row>
-      <v-row align="center" justify="center">
-        <v-col cols="6">
-          <div v-if="error">
-            <v-img
-              :src="require('../assets/error.png')"
-              class="transfer-progress-dialog--error"
-            ></v-img>
-          </div>
-          <div v-else-if="!inProgress">
-            <v-img
-              :src="require('../assets/done.svg')"
-              class="transfer-progress-dialog--done"
-            ></v-img>
-          </div>
-          <v-progress-circular
-            v-else
-            :size="125"
-            :width="4"
-            color="primary"
-            class="transfer-progress-dialog--progress"
-            indeterminate
-          ></v-progress-circular>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col cols="12">
-          <div class="transfer-progress-dialog__description">
-            <span v-if="error">
-              {{ error }}
-            </span>
-            <span v-else-if="inProgress">
-              {{ $t('transfer.steps.transfer.description') }}
-            </span>
-            <span v-else>
-              {{ $t('transfer.steps.done.description') }}
-            </span>
-          </div>
-        </v-col>
-      </v-row>
-    </v-card-text>
-  </v-card>
+        </div>
+      </v-col>
+    </v-row>
+  </raiden-dialog>
 </template>
 
 <script lang="ts">
 import { Component, Emit, Prop, Vue } from 'vue-property-decorator';
+import RaidenDialog from '@/components/RaidenDialog.vue';
 
-@Component({})
+@Component({
+  components: { RaidenDialog }
+})
 export default class TransferProgressDialog extends Vue {
   @Prop({ required: true })
   error!: string;
@@ -81,9 +79,9 @@ export default class TransferProgressDialog extends Vue {
 
 <style scoped lang="scss">
 @import '../scss/colors';
+
 .transfer-progress-dialog {
   background-color: $card-background;
-  height: 441.5px;
 
   &__title {
     padding-top: 60px;
@@ -113,12 +111,6 @@ export default class TransferProgressDialog extends Vue {
     margin-top: 26px;
     border-radius: 50%;
     width: 125px;
-  }
-
-  &__close {
-    position: absolute;
-    right: 15px;
-    top: 15px;
   }
 }
 </style>
