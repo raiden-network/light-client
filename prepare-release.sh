73be70c1ed7d6d3b548205d5e5afd9df9ca6a88a
Konstantinos Paparas <kelsos86@gmail.com>
2020-01-21 21:11:28 +0100

    Adds a bash script to handle releasing

---
+++ prepare-release.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+if [[ "$#" -ne 1 ]]; then
+    VERSION_PARAMS=`npm version --help | grep -oPm1 'npm version (.*)' | sed 's/npm version/''/g'`
+    echo "USAGE: $0 $VERSION_PARAMS"
+    exit 1
+fi
+
+cd raiden-ts
+
+PACKAGE_VERSION=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g' \
+  | tr -d '[[:space:]]')
+
+# Using `--no-git-tag-version` in January 2020 serves no purpose due to `npm version`
+# expecting the command to run from the same directory that also hosts the `.git` folder
+# You can find more information on https://github.com/npm/npm/issues/9111.
+# We disable the whole functionality because there no option exists to disable only automatic tagging.
+VERSION=$(npm --no-git-tag-version version $1)
+MESSAGE="$PACKAGE_VERSION -> $VERSION";
+
+echo "Preparing to commit version update $MESSAGE"
+
+git add package.json
+git add package-lock.json
+
+git commit -m "$MESSAGE"
