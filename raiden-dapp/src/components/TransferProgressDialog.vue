a4a6f482d6720dfa84df1ec4fbbcdf6eadd09a2b
Konstantinos Paparas <kelsos86@gmail.com>
2020-02-06 17:18:42 +0100

    Fixes TransferProgressDialog.vue visibility issues

--- raiden-dapp/src/components/TransferProgressDialog.vue
+++ raiden-dapp/src/components/TransferProgressDialog.vue
@@ -1,61 +1,61 @@
 <template>
-  <raiden-dialog class="transfer-progress-dialog" @close="dismiss">
-    <v-row
-      class="transfer-progress-dialog__title"
-      align="center"
-      justify="center"
-    >
-      <v-col cols="12">
-        <span v-if="error">
-          {{ $t('transfer.error.title') }}
-        </span>
-        <span v-else-if="inProgress">
-          {{ $t('transfer.steps.transfer.title') }}
-        </span>
-        <span v-else>
-          {{ $t('transfer.steps.done.title') }}
-        </span>
-      </v-col>
-    </v-row>
-    <v-row align="center" justify="center">
-      <v-col cols="6">
-        <div v-if="error">
-          <v-img
-            :src="require('../assets/error.png')"
-            class="transfer-progress-dialog--error"
-          ></v-img>
-        </div>
-        <div v-else-if="!inProgress">
-          <v-img
-            :src="require('../assets/done.svg')"
-            class="transfer-progress-dialog--done"
-          ></v-img>
-        </div>
-        <v-progress-circular
-          v-else
-          :size="125"
-          :width="4"
-          color="primary"
-          class="transfer-progress-dialog--progress"
-          indeterminate
-        ></v-progress-circular>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col cols="12">
-        <div class="transfer-progress-dialog__description">
-          <span v-if="error">
-            {{ error }}
-          </span>
-          <span v-else-if="inProgress">
-            {{ $t('transfer.steps.transfer.description') }}
-          </span>
-          <span v-else>
-            {{ $t('transfer.steps.done.description') }}
-          </span>
-        </div>
-      </v-col>
-    </v-row>
+  <raiden-dialog
+    :visible="visible"
+    class="transfer-progress-dialog"
+    @close="dismiss"
+  >
+    <v-card-title>
+      <span v-if="error">
+        {{ $t('transfer.error.title') }}
+      </span>
+      <span v-else-if="inProgress">
+        {{ $t('transfer.steps.transfer.title') }}
+      </span>
+      <span v-else>
+        {{ $t('transfer.steps.done.title') }}
+      </span>
+    </v-card-title>
+    <v-card-text>
+      <v-row align="center" justify="center">
+        <v-col cols="6">
+          <div v-if="error">
+            <v-img
+              :src="require('../assets/error.png')"
+              class="transfer-progress-dialog--error"
+            ></v-img>
+          </div>
+          <div v-else-if="!inProgress">
+            <v-img
+              :src="require('../assets/done.svg')"
+              class="transfer-progress-dialog--done"
+            ></v-img>
+          </div>
+          <v-progress-circular
+            v-else
+            :size="125"
+            :width="4"
+            color="primary"
+            class="transfer-progress-dialog--progress"
+            indeterminate
+          ></v-progress-circular>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12">
+          <div class="transfer-progress-dialog__description">
+            <span v-if="error">
+              {{ error }}
+            </span>
+            <span v-else-if="inProgress">
+              {{ $t('transfer.steps.transfer.description') }}
+            </span>
+            <span v-else>
+              {{ $t('transfer.steps.done.description') }}
+            </span>
+          </div>
+        </v-col>
+      </v-row>
+    </v-card-text>
   </raiden-dialog>
 </template>
 
@@ -69,8 +69,10 @@ import RaidenDialog from '@/components/RaidenDialog.vue';
 export default class TransferProgressDialog extends Vue {
   @Prop({ required: true })
   error!: string;
-  @Prop({ required: true })
+  @Prop({ required: true, type: Boolean })
   inProgress!: boolean;
+  @Prop({ required: true, type: Boolean })
+  visible!: boolean;
 
   @Emit()
   dismiss() {}
@@ -81,18 +83,6 @@ export default class TransferProgressDialog extends Vue {
 @import '../scss/colors';
 
 .transfer-progress-dialog {
-  background-color: $card-background;
-
-  &__title {
-    padding-top: 60px;
-    text-align: center;
-    font-size: 24px;
-    font-weight: 500;
-    letter-spacing: 0;
-    color: #ffffff;
-    opacity: 1;
-  }
-
   &__description {
     text-align: center;
     font-size: 14px;
