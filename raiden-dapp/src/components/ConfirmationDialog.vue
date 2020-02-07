c50ff9a44d63c9c06a6df128a314e157c8936a99
taleldayekh <mail@talel.se>
2020-02-07 17:38:51 +0100

    Adding new modal to close and settle confirmation dialog

--- raiden-dapp/src/components/ConfirmationDialog.vue
+++ raiden-dapp/src/components/ConfirmationDialog.vue
@@ -1,95 +1,67 @@
 <template>
-  <v-row class="confirmation" no-gutters>
-    <v-row align="center" justify="center">
-      <h1 class="confirmation__text__header"><slot name="header"></slot></h1>
-    </v-row>
-    <v-row align="center" justify="center">
-      <v-col cols="12">
-        <p class="confirmation__text__message"><slot></slot></p>
-      </v-col>
-    </v-row>
-    <v-row
-      align="end"
-      justify="center"
-      class="confirmation__buttons"
-      no-gutters
-    >
-      <v-btn
-        :id="`cancel-${identifier}`"
-        class="text-capitalize confirmation__buttons__cancel"
-        @click="cancel()"
-      >
-        {{ $t('confirmation.buttons.cancel') }}
-      </v-btn>
-      <v-btn
-        :id="`confirm-${identifier}`"
-        class="text-capitalize confirmation__buttons__confirm"
-        @click="confirm()"
-      >
-        {{ positiveAction }}
-      </v-btn>
-    </v-row>
-  </v-row>
+  <raiden-dialog
+    class="confirmation"
+    :visible="visible"
+    :fullscreen="false"
+    @close="cancel"
+  >
+    <v-card-title>
+      <v-row align="center" justify="center">
+        <v-col>
+          <span>
+            <slot name="header"></slot>
+          </span>
+        </v-col>
+      </v-row>
+    </v-card-title>
+
+    <v-card-text>
+      <v-row align="center" justify="center">
+        <span>
+          <slot></slot>
+        </span>
+      </v-row>
+    </v-card-text>
+
+    <v-card-actions>
+      <v-row align="center" justify="center">
+        <v-col>
+          <action-button
+            :id="`confirm-${identifier}`"
+            :enabled="true"
+            :text="positiveAction"
+            @click="confirm()"
+          >
+          </action-button>
+        </v-col>
+      </v-row>
+    </v-card-actions>
+  </raiden-dialog>
 </template>
 
 <script lang="ts">
 import { Component, Emit, Prop, Vue } from 'vue-property-decorator';
-@Component({})
-export default class Confirmation extends Vue {
+import ActionButton from '@/components/ActionButton.vue';
+import RaidenDialog from '@/components/RaidenDialog.vue';
+@Component({
+  components: {
+    RaidenDialog,
+    ActionButton
+  }
+})
+export default class ConfirmationDialog extends Vue {
   @Prop({ required: true })
-  identifier!: number;
+  visible!: boolean;
   @Prop({ required: true })
   positiveAction!: string;
+  @Prop({ required: true })
+  identifier!: number;
+
   @Emit()
-  public cancel() {}
+  cancel() {}
   @Emit()
   public confirm() {}
 }
 </script>
 
-<style scoped lang="scss">
-@import '../scss/colors';
-$background-color: #323232;
-.confirmation {
-  height: 252px;
-  padding: 25px;
-  background-color: $background-color;
-  box-shadow: 10px 10px 15px 0 rgba(0, 0, 0, 0.3);
-  &__text {
-    &__header {
-      color: #ffffff;
-      font-family: Roboto, sans-serif;
-      font-size: 24px;
-      font-weight: bold;
-      line-height: 28px;
-      text-align: center;
-    }
-    &__message {
-      padding-top: 10px;
-      text-align: center;
-      height: 100%;
-      color: #ffffff;
-    }
-  }
-  &__buttons {
-    button {
-      width: 135px !important;
-      height: 35px !important;
-      font-size: 16px;
-      line-height: 21px;
-      text-align: center;
-      border-radius: 29px;
-      margin-left: 15px;
-      margin-right: 15px;
-    }
-    &__cancel {
-      background-color: transparent !important;
-      border: 2px solid $primary-color;
-      color: white;
-    }
-    &__confirm {
-      background-color: $primary-color !important;
-    }
-  }
-}
-</style>
+<style scoped lang="scss"></style>
