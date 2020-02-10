3046d1d9062933a930514713668af327a7cc85ad
taleldayekh <mail@talel.se>
2020-02-10 17:08:16 +0100

    Adding spinner and done icon

--- raiden-dapp/src/components/OpenChannelDialog.vue
+++ raiden-dapp/src/components/OpenChannelDialog.vue
@@ -1,14 +1,37 @@
 <template>
-  <raiden-dialog :visible="visible" @close="cancel">
+  <raiden-dialog class="open-channel-dialog" :visible="visible" @close="cancel">
     <v-card-title>
       <v-row align="center" justify="center">
         <v-col>
           <span>
-            {{}}
+            {{ steps[current].title }}
           </span>
         </v-col>
       </v-row>
     </v-card-title>
+
+    <v-card-actions>
+      <v-row v-if="done" align="center" justify="center">
+        <v-col cols="6">
+          <v-img
+            class="open-channel-dialog__done"
+            :src="require('../assets/done.svg')"
+          ></v-img>
+        </v-col>
+      </v-row>
+
+      <v-row v-else align="center" justify="center">
+        <v-col cols="6">
+          <v-progress-circular
+            class="open-channel-dialog__progress"
+            :size="110"
+            :width="7"
+            indeterminate
+          >
+          </v-progress-circular>
+        </v-col>
+      </v-row>
+    </v-card-actions>
   </raiden-dialog>
 </template>
 
@@ -26,10 +49,22 @@ export default class OpenChannelDialog extends Vue {
   visible!: boolean;
   @Prop({ required: true })
   steps!: StepDescription[];
+  @Prop({ required: false, default: 0 })
+  current!: number;
+  @Prop({ required: false })
+  done?: boolean;
 
   @Emit()
   cancel() {}
 }
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+@import '../scss/colors';
+
+.open-channel-dialog {
+  &__progress {
+    color: $secondary-color;
+  }
+}
+</style>
