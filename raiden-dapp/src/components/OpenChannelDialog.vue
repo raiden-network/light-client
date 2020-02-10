2fde0ffbf372b46323aeb9eb5bd45a60a67ddac5
taleldayekh <mail@talel.se>
2020-02-10 16:07:29 +0100

    Initial commit of OpenChannelDialog component

---
+++ raiden-dapp/src/components/OpenChannelDialog.vue
@@ -0,0 +1,35 @@
+<template>
+  <raiden-dialog :visible="visible" @close="cancel">
+    <v-card-title>
+      <v-row align="center" justify="center">
+        <v-col>
+          <span>
+            {{}}
+          </span>
+        </v-col>
+      </v-row>
+    </v-card-title>
+  </raiden-dialog>
+</template>
+
+<script lang="ts">
+import { Component, Emit, Prop, Vue } from 'vue-property-decorator';
+import RaidenDialog from '@/components/RaidenDialog.vue';
+import { StepDescription } from '@/model/types';
+@Component({
+  components: {
+    RaidenDialog
+  }
+})
+export default class OpenChannelDialog extends Vue {
+  @Prop({ required: true })
+  visible!: boolean;
+  @Prop({ required: true })
+  steps!: StepDescription[];
+
+  @Emit()
+  cancel() {}
+}
+</script>
+
+<style scoped lang="scss"></style>
