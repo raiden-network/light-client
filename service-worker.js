71f07ba44a4dabe496ab3c7212e006975e98eadb
Continuous Delivery <no-reply@raiden.network>
2020-02-27 08:42:40 +0000

    Automated deployment to GitHub Pages: 8c011d730d6b71dacc61150a3a15da6aacb99e32 [ci skip]

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.198d49542d56cad9f2fdaf626261b0fb.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/precache-manifest.e0eb43c8c21a2dfb031af990b2989fc4.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 self.addEventListener('message', e => {
   if (!e.data) {
