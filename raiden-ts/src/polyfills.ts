d62fb9a6af38b0d2788423a9d1fd4c8650f0f6df
André Vitor de Lima Matos <andre.vmatos@gmail.com>
2020-03-27 06:29:22 -0300

    Workaround for setLevel on undefined logger (#1245)
    * Workaround for storing Error objects in IndexedDB
    
    * Improve Raiden.log private property usage
    
    * Revert matrix monkey-patch of loglevel.methodFactory
    
    * Remove logstore's serialization functions from code coverage
    
    Co-authored-by: Thomas Pischulski <thomas.pischulski@gmail.com>

--- raiden-ts/src/polyfills.ts
+++ raiden-ts/src/polyfills.ts
@@ -1,3 +1,10 @@
 import 'symbol-observable';
 import 'isomorphic-fetch';
 import 'abort-controller/polyfill';
+
+// matrix-js-sdk monkey-patch root methodFactory
+import logging from 'loglevel';
+const methodFactory = logging.methodFactory;
+import { logger as matrixLogger } from 'matrix-js-sdk/lib/logger';
+Object.assign(logging, { methodFactory }); // revert
+matrixLogger.setLevel(logging.levels.DEBUG); // apply
