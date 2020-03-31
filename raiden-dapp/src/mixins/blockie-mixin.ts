fb5d1fce7f661eea8c1f214403678d2bdf2e5353
taleldayekh <mail@talel.se>
2020-03-31 13:22:19 +0200

    Enables black and white identicon

--- raiden-dapp/src/mixins/blockie-mixin.ts
+++ raiden-dapp/src/mixins/blockie-mixin.ts
@@ -2,7 +2,11 @@ import { Component, Vue } from 'vue-property-decorator';
 
 @Component
 export default class BlockieMixin extends Vue {
-  $blockie(address: string) {
-    return this.$identicon.getIdenticon(address);
+  $blockie(address?: string) {
+    if (address) {
+      return this.$identicon.getIdenticon(address);
+    } else {
+      return this.$identicon.getIdenticon('0x000');
+    }
   }
 }
