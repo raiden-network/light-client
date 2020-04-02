9a88bd968e81ca27ee9455701fef0f343952fd00
taleldayekh <mail@talel.se>
2020-04-02 15:07:32 +0200

    Updating App tests

--- raiden-dapp/tests/unit/app.spec.ts
+++ raiden-dapp/tests/unit/app.spec.ts
@@ -1,74 +1,41 @@
 jest.mock('@/services/raiden-service');
-
 import { shallowMount, Wrapper } from '@vue/test-utils';
-import { DeniedReason } from '@/model/types';
-import App from '@/App.vue';
-import RaidenService from '@/services/raiden-service';
 import Vue from 'vue';
-import Vuex from 'vuex';
-import Vuetify from 'vuetify';
 import VueRouter from 'vue-router';
-import flushPromises from 'flush-promises';
 import store from '@/store/index';
+import Vuetify from 'vuetify';
+import RaidenService from '@/services/raiden-service';
+import App from '@/App.vue';
 
-Vue.use(Vuex);
-Vue.use(Vuetify);
 Vue.use(VueRouter);
+Vue.use(Vuetify);
 
 describe('App.vue', () => {
+  let wrapper: Wrapper<App>;
+  let vuetify: typeof Vuetify;
   let $raiden: RaidenService;
 
   beforeEach(() => {
     $raiden = new RaidenService(store);
-    $raiden.connect = jest.fn();
     $raiden.disconnect = jest.fn();
-  });
-
-  describe('call connect on component creation and disconnect on destruction', async () => {
-    let wrapper: Wrapper<App>;
-
-    beforeEach(() => {
-      wrapper = shallowMount(App, {
-        store,
-        mocks: {
-          $raiden: $raiden,
-          $t: (msg: string) => msg
-        }
-      });
-    });
-
-    test('call without subkey', async () => {
-      await (wrapper.vm as any).connect();
-      await flushPromises();
-      wrapper.vm.$destroy();
-
-      expect($raiden.connect).toHaveBeenCalledTimes(1);
-      expect($raiden.disconnect).toHaveBeenCalledTimes(1);
-    });
-
-    test('call with subkey', async () => {
-      await (wrapper.vm as any).connect(true);
-      await flushPromises();
-      wrapper.vm.$destroy();
-
-      expect($raiden.connect).toHaveBeenCalledTimes(1);
-      expect($raiden.disconnect).toHaveBeenCalledTimes(1);
-    });
-  });
-
-  test('connect can be called without error after failing initially', async () => {
-    store.commit('accessDenied', DeniedReason.NO_ACCOUNT);
-    const wrapper = shallowMount(App, {
-      store,
+    wrapper = shallowMount(App, {
+      vuetify,
       mocks: {
         $raiden: $raiden,
         $t: (msg: string) => msg
       }
     });
+  });
 
-    await (wrapper.vm as any).connect();
+  test('disconnects on destruction', () => {
+    wrapper.vm.$destroy();
 
-    await flushPromises();
-    expect(store.state.accessDenied).toEqual(DeniedReason.UNDEFINED);
+    expect($raiden.disconnect).toHaveBeenCalledTimes(1);
+  });
+
+  test('displays privacy policy', () => {
+    const privacyPolicy = wrapper.find('.policy');
+
+    expect(privacyPolicy.text()).toBe('application.privacy-policy');
   });
 });
