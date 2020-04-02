01b9a117cc9bb56d1627c4aea08ce8f39698fce9
Konstantinos Paparas <kelsos86@gmail.com>
2020-04-02 15:36:08 +0200

    Fixes rm failing on missing pathspec (#1288)

--- raiden-dapp/deploy.sh
+++ raiden-dapp/deploy.sh
@@ -49,8 +49,7 @@ git remote add --fetch origin "$remote"
 if git rev-parse --verify origin/gh-pages >/dev/null 2>&1; then
   git checkout gh-pages
   # delete any old site as we are going to replace it
-  # Note: this explodes if there aren't any, so moving it here for now
-  git rm -rf ./${DEPLOY_BASE}
+  git rm -rf ./${DEPLOY_BASE} --ignore-unmatch
 else
   git checkout --orphan gh-pages
 fi
