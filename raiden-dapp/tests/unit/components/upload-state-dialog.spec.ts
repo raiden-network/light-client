bc60797aa8809c731d871e8c1a612b22ea32e72a
taleldayekh <mail@talel.se>
2020-04-14 15:27:08 +0200

    Increasing test coverage

--- raiden-dapp/tests/unit/components/upload-state-dialog.spec.ts
+++ raiden-dapp/tests/unit/components/upload-state-dialog.spec.ts
@@ -9,9 +9,13 @@ Vue.use(Vuetify);
 describe('UploadStateDialog.vue', () => {
   let wrapper: Wrapper<UploadStateDialog>;
   let vuetify: typeof Vuetify;
+  let mockStateFile: File;
 
   beforeEach(() => {
     vuetify = new Vuetify();
+    mockStateFile = new File([''], 'raiden-state-file.json', {
+      type: 'application/json'
+    });
 
     wrapper = mount(UploadStateDialog, {
       vuetify,
@@ -95,6 +99,33 @@ describe('UploadStateDialog.vue', () => {
 
     expect(wrapper.vm.$data.dropzoneErrorMessage).toBe(true);
     jest.advanceTimersByTime(2000);
+    expect(wrapper.vm.$data.dropzoneErrorMessage).toBe(false);
+  });
+
+  test('method for uploading state displays dropzone error message if > 1 file is uploaded', () => {
+    expect(wrapper.vm.$data.dropzoneErrorMessage).toBe(false);
+
+    const fileList = {
+      0: mockStateFile,
+      1: mockStateFile,
+      length: 2
+    };
+    // @ts-ignore
+    wrapper.vm.uploadState(fileList);
+
+    expect(wrapper.vm.$data.dropzoneErrorMessage).toBe(true);
+  });
+
+  test('method for uploading state does not display error message if only 1 file is uploaded', () => {
+    expect(wrapper.vm.$data.dropzoneErrorMessage).toBe(false);
+
+    const fileList = {
+      0: mockStateFile,
+      length: 1
+    };
+    // @ts-ignore
+    wrapper.vm.uploadState(fileList);
+
     expect(wrapper.vm.$data.dropzoneErrorMessage).toBe(false);
   });
 });
