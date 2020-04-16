bc16164e9678aa7be4fead398d0591076c75b992
Continuous <no-reply@raiden.network>
2020-04-16 15:54:21 +0000

    Automated deployment to GitHub Pages: 9a603e5edf2cb80fcf3241e9e74298c2487ab861 [skip ci]

---
+++ staging/precache-manifest.64434ee7bfe0692481a1eea464e59a06.js
@@ -0,0 +1,210 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c12af454a2cc54a9007e9d7f5b5dffa1",
+    "url": "/staging/404.html"
+  },
+  {
+    "revision": "3324a04ed67a0d4cb473",
+    "url": "/staging/css/app.45ddce8d.css"
+  },
+  {
+    "revision": "86783ace8ff9763049d9",
+    "url": "/staging/css/chunk-08151fa2.b6ac451a.css"
+  },
+  {
+    "revision": "d8ac9b570732b761443b",
+    "url": "/staging/css/chunk-15f0b7af.1d5a726d.css"
+  },
+  {
+    "revision": "03905200fe26e3dde993",
+    "url": "/staging/css/chunk-27f3174b.4454c863.css"
+  },
+  {
+    "revision": "34606788d433cc07d131",
+    "url": "/staging/css/chunk-2a7c1c36.2e0dc378.css"
+  },
+  {
+    "revision": "ebd9d7c08674e512c876",
+    "url": "/staging/css/chunk-488fc1f2.6d6e40eb.css"
+  },
+  {
+    "revision": "1c505f403f721ac28ea5",
+    "url": "/staging/css/chunk-49ba97a0.c1c4c74f.css"
+  },
+  {
+    "revision": "0efb3185851e5c394aab",
+    "url": "/staging/css/chunk-4c34ba5c.39ba85ce.css"
+  },
+  {
+    "revision": "f5fefba1ee19b810725f",
+    "url": "/staging/css/chunk-501c040c.43563bf9.css"
+  },
+  {
+    "revision": "1979707cc9279ed2d983",
+    "url": "/staging/css/chunk-552b1730.b6b89419.css"
+  },
+  {
+    "revision": "dc6a5db094bc6ec7746c",
+    "url": "/staging/css/chunk-5e7189c2.295d09cf.css"
+  },
+  {
+    "revision": "88264a35fe85dfc8fb3d",
+    "url": "/staging/css/chunk-b14359ce.59b8ab97.css"
+  },
+  {
+    "revision": "331f1a071f63da271e4f",
+    "url": "/staging/css/chunk-d3b2fbc4.8da4d130.css"
+  },
+  {
+    "revision": "67dec83c9d8b1921c003",
+    "url": "/staging/css/chunk-e17a21de.1740c2a4.css"
+  },
+  {
+    "revision": "316ed6724cf577abf5f1",
+    "url": "/staging/css/chunk-vendors.122b2a61.css"
+  },
+  {
+    "revision": "2dcce27160495d68abf4945acd282448",
+    "url": "/staging/fonts/materialdesignicons-webfont.2dcce271.woff"
+  },
+  {
+    "revision": "3e2c1c7919fb45d5dee5b0703fe52931",
+    "url": "/staging/fonts/materialdesignicons-webfont.3e2c1c79.eot"
+  },
+  {
+    "revision": "a323c28ecd42189e70efca274f5b7089",
+    "url": "/staging/fonts/materialdesignicons-webfont.a323c28e.woff2"
+  },
+  {
+    "revision": "e7dec9c5e1bd830c084f2d2fb94fa1e7",
+    "url": "/staging/fonts/materialdesignicons-webfont.e7dec9c5.ttf"
+  },
+  {
+    "revision": "268247fb18f0a1f0c4b01bf77750afb5",
+    "url": "/staging/img/back_arrow.268247fb.svg"
+  },
+  {
+    "revision": "4e8b8f1b476fdd5f3f52326d6540ef57",
+    "url": "/staging/img/bug.4e8b8f1b.svg"
+  },
+  {
+    "revision": "51f3a9701270d46b903f820a4d16ffde",
+    "url": "/staging/img/done.51f3a970.svg"
+  },
+  {
+    "revision": "d7fcc637123a0365361fb4e0c3c8684c",
+    "url": "/staging/img/down_arrow.d7fcc637.svg"
+  },
+  {
+    "revision": "55c56a33855dfaa41bdd5db277c2b321",
+    "url": "/staging/img/generic.55c56a33.svg"
+  },
+  {
+    "revision": "4e8b2468b175efdc06e6e6e88ca22e39",
+    "url": "/staging/img/input_invalid.4e8b2468.svg"
+  },
+  {
+    "revision": "762cb0ae3a76693f6d02685fdbf3b6c7",
+    "url": "/staging/img/input_valid.762cb0ae.svg"
+  },
+  {
+    "revision": "5c4b022de72e407cd650f0c4bcde5973",
+    "url": "/staging/img/logo.5c4b022d.svg"
+  },
+  {
+    "revision": "c879a582a94e772b8ed27295ae83145d",
+    "url": "/staging/img/metamask.c879a582.svg"
+  },
+  {
+    "revision": "ff4426c1d8501bdcb341df92f438c828",
+    "url": "/staging/img/state.ff4426c1.svg"
+  },
+  {
+    "revision": "fc1f0473592702c61bcb0137f1002bd3",
+    "url": "/staging/img/state_download.fc1f0473.png"
+  },
+  {
+    "revision": "dea399eb4ace55f4a13007018aaaf15e",
+    "url": "/staging/img/state_upload.dea399eb.png"
+  },
+  {
+    "revision": "49bc56a9fd562747445fe4222c8e21be",
+    "url": "/staging/img/warning.49bc56a9.svg"
+  },
+  {
+    "revision": "424606a38072c148f1cc19de1ee1c461",
+    "url": "/staging/index.html"
+  },
+  {
+    "revision": "3324a04ed67a0d4cb473",
+    "url": "/staging/js/app.b6ae0659.js"
+  },
+  {
+    "revision": "86783ace8ff9763049d9",
+    "url": "/staging/js/chunk-08151fa2.248f1e1e.js"
+  },
+  {
+    "revision": "d8ac9b570732b761443b",
+    "url": "/staging/js/chunk-15f0b7af.cc04dab4.js"
+  },
+  {
+    "revision": "03905200fe26e3dde993",
+    "url": "/staging/js/chunk-27f3174b.95929e9c.js"
+  },
+  {
+    "revision": "34606788d433cc07d131",
+    "url": "/staging/js/chunk-2a7c1c36.60b461a9.js"
+  },
+  {
+    "revision": "ebd9d7c08674e512c876",
+    "url": "/staging/js/chunk-488fc1f2.45a13c93.js"
+  },
+  {
+    "revision": "1c505f403f721ac28ea5",
+    "url": "/staging/js/chunk-49ba97a0.cc934ea0.js"
+  },
+  {
+    "revision": "0efb3185851e5c394aab",
+    "url": "/staging/js/chunk-4c34ba5c.8262af52.js"
+  },
+  {
+    "revision": "f5fefba1ee19b810725f",
+    "url": "/staging/js/chunk-501c040c.14041124.js"
+  },
+  {
+    "revision": "1979707cc9279ed2d983",
+    "url": "/staging/js/chunk-552b1730.a8860cbe.js"
+  },
+  {
+    "revision": "dc6a5db094bc6ec7746c",
+    "url": "/staging/js/chunk-5e7189c2.f3476e97.js"
+  },
+  {
+    "revision": "88264a35fe85dfc8fb3d",
+    "url": "/staging/js/chunk-b14359ce.14b0b025.js"
+  },
+  {
+    "revision": "331f1a071f63da271e4f",
+    "url": "/staging/js/chunk-d3b2fbc4.2e55ee34.js"
+  },
+  {
+    "revision": "67dec83c9d8b1921c003",
+    "url": "/staging/js/chunk-e17a21de.7cd29f16.js"
+  },
+  {
+    "revision": "316ed6724cf577abf5f1",
+    "url": "/staging/js/chunk-vendors.8c738e87.js"
+  },
+  {
+    "revision": "364061b3f4d46ef296d8e6e026c3a6a6",
+    "url": "/staging/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/staging/robots.txt"
+  },
+  {
+    "revision": "cc7630759f15a5ca9f6a716d0af3bba2",
+    "url": "/staging/wizard.json.example"
+  }
+]);
--- staging/service-worker.js
+++ staging/service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/staging/precache-manifest.4cc13113968cf6c2d8174a7249312b7c.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/staging/precache-manifest.64434ee7bfe0692481a1eea464e59a06.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 self.addEventListener('message', e => {
   if (!e.data) {
