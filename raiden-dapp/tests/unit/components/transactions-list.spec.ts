bae329ebea6f05de86808c42d16310db3f5e6064
taleldayekh <mail@talel.se>
2020-05-07 16:43:59 +0200

    Small test for the transaction list component

---
+++ raiden-dapp/tests/unit/components/transactions-list.spec.ts
@@ -0,0 +1,28 @@
+import { mount, Wrapper } from '@vue/test-utils';
+import Vue from 'vue';
+import store from '@/store/index';
+import Vuetify from 'vuetify';
+import TransactionsList from '@/components/transaction-history/TransactionsList.vue';
+
+Vue.use(Vuetify);
+
+describe('TransactionsList.vue', () => {
+  let wrapper: Wrapper<TransactionsList>;
+  let vuetify: typeof Vuetify;
+
+  beforeEach(() => {
+    vuetify = new Vuetify();
+    wrapper = mount(TransactionsList, {
+      vuetify,
+      store,
+      mocks: {
+        $t: (msg: string) => msg
+      }
+    });
+  });
+
+  test('displays transaction history title', () => {
+    const transactionHistoryTitle = wrapper.find('.transaction-history__title');
+    expect(transactionHistoryTitle.text()).toBe('transaction-history.title');
+  });
+});
