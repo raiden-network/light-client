2c756095655171c512766f16014cc6525797ce0f
Konstantinos Paparas <kelsos86@gmail.com>
2020-05-07 18:26:21 +0200

    Adds tests for the components

---
+++ raiden-dapp/tests/unit/components/account/withdrawal.spec.ts
@@ -0,0 +1,140 @@
+import RaidenDialog from '@/components/dialogs/RaidenDialog.vue';
+
+jest.mock('@/services/raiden-service');
+
+import { mount, Wrapper } from '@vue/test-utils';
+import Vue from 'vue';
+import Vuetify from 'vuetify';
+import store from '@/store';
+import { $identicon } from '../../utils/mocks';
+import Withdrawal from '@/components/account/Withdrawal.vue';
+import RaidenService from '@/services/raiden-service';
+import Mocked = jest.Mocked;
+import { parseEther, parseUnits } from 'ethers/utils';
+import { Zero } from 'ethers/constants';
+
+Vue.use(Vuetify);
+
+describe('Withdrawal.vue', () => {
+  let wrapper: Wrapper<Withdrawal>;
+  let vuetify: typeof Vuetify;
+  let $raiden: Mocked<RaidenService>;
+
+  function createWrapper() {
+    return mount(Withdrawal, {
+      vuetify,
+      store,
+      stubs: ['v-dialog', 'i18n'],
+      mocks: {
+        $identicon: $identicon(),
+        $t: (msg: string) => msg,
+        $raiden
+      }
+    });
+  }
+
+  beforeEach(() => {
+    vuetify = new Vuetify();
+    $raiden = new RaidenService(store) as Mocked<RaidenService>;
+  });
+
+  describe('no tokens', () => {
+    beforeEach(() => {
+      $raiden.getUpdatedBalances.mockResolvedValue([]);
+      wrapper = createWrapper();
+    });
+
+    test('render component', () => {
+      expect(wrapper.is(Withdrawal)).toBe(true);
+    });
+
+    test('show an empty screen', () => {
+      expect(wrapper.find('.withdrawal__empty').exists()).toBe(true);
+    });
+  });
+
+  describe('with tokens', () => {
+    beforeEach(() => {
+      $raiden.getUpdatedBalances.mockResolvedValue([
+        {
+          address: '0xtoken',
+          decimals: 5,
+          balance: parseUnits('1.2', 5),
+          name: 'TestToken',
+          symbol: 'TTT'
+        },
+        {
+          address: '0xtoken2',
+          decimals: 5,
+          balance: Zero,
+          name: 'TestToken2',
+          symbol: 'TTT2'
+        }
+      ]);
+      wrapper = createWrapper();
+    });
+
+    test('display only tokens with balance', () => {
+      expect(wrapper.findAll('.withdrawal__tokens__name')).toHaveLength(1);
+    });
+
+    test('open a dialog when clicking withdraw for a token', async () => {
+      wrapper
+        .find('.withdrawal__tokens')
+        .find('button')
+        .trigger('click');
+
+      await wrapper.vm.$nextTick();
+
+      expect(wrapper.find(RaidenDialog).exists()).toBe(true);
+    });
+
+    test('disables withdrawal if the Raiden account has no funds', async () => {
+      wrapper
+        .find('.withdrawal__tokens')
+        .find('button')
+        .trigger('click');
+
+      await wrapper.vm.$nextTick();
+
+      expect(
+        wrapper
+          .find('.withdrawal-dialog__action')
+          .find('button')
+          .attributes()['disabled']
+      ).toBe('disabled');
+    });
+
+    test('withdraws tokens when the user clicks on the action button', async () => {
+      store.commit('raidenAccountBalance', parseEther('1.2'));
+      wrapper
+        .find('.withdrawal__tokens')
+        .find('button')
+        .trigger('click');
+
+      await wrapper.vm.$nextTick();
+
+      expect(
+        wrapper
+          .find('.withdrawal-dialog__action')
+          .find('button')
+          .attributes()['disabled']
+      ).toBeUndefined();
+
+      wrapper
+        .find('.withdrawal-dialog__action')
+        .find('button')
+        .trigger('click');
+
+      await wrapper.vm.$nextTick();
+      expect($raiden.transferOnChainTokens).toHaveBeenCalledTimes(1);
+      await wrapper.vm.$nextTick();
+      expect(wrapper.find('.withdrawal__empty').exists()).toBe(true);
+    });
+
+    test('withdraw is no-op without a selection', async () => {
+      await (wrapper.vm as any).withdrawTokens();
+      expect($raiden.transferOnChainTokens).toHaveBeenCalledTimes(0);
+    });
+  });
+});
