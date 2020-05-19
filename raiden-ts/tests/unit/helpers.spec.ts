9cf4a9eb086325765b1fee5aa2270cca6c9e7a9e
Thomas Pischulski <thomas.pischulski@gmail.com>
2020-05-19 13:12:00 +0200

    Add unit test

--- raiden-ts/tests/unit/helpers.spec.ts
+++ raiden-ts/tests/unit/helpers.spec.ts
@@ -22,9 +22,14 @@ describe('getContracts', () => {
     expect(getContracts(goerliNetwork)).toHaveProperty('UserDeposit');
   });
 
+  test('supports mainnet', async () => {
+    const mainNetwork = { name: 'homestead', chainId: 1 } as Network;
+    expect(() => getContracts(mainNetwork)).not.toThrow();
+  });
+
   test('throw if network is not supported', async () => {
-    const mainNetwork = { name: 'private-chain', chainId: 666 } as Network;
-    expect(() => getContracts(mainNetwork)).toThrow();
+    const privateNetwork = { name: 'private-chain', chainId: 666 } as Network;
+    expect(() => getContracts(privateNetwork)).toThrow();
   });
 });
 
