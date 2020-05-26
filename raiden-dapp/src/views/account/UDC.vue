7bbbd9892ffc74b6bc605eb391df452a62348565
Thomas Pischulski <thomas.pischulski@gmail.com>
2020-05-26 10:55:33 +0200

    Added mint & deposit functionality from dialog

--- raiden-dapp/src/views/account/UDC.vue
+++ raiden-dapp/src/views/account/UDC.vue
@@ -1,5 +1,17 @@
 <template>
   <div>
+    <v-row justify="center">
+      <v-col cols="10">
+        <p>
+          {{ $t('udc.description') }}
+        </p>
+      </v-col>
+    </v-row>
+    <v-row justify="center">
+      <v-col cols="10">
+        <h3 class="udc__sub-head">{{ $t('udc.mint-deposit') }}</h3>
+      </v-col>
+    </v-row>
     <v-row v-if="!loading && !error" justify="center" no-gutters>
       <v-col cols="6">
         <v-text-field
@@ -7,7 +19,7 @@
           autofocus
           type="text"
           :suffix="udcToken.symbol || 'SVT'"
-          class="mint-deposit-dialog__amount"
+          class="udc__mint-deposit--amount"
         />
       </v-col>
     </v-row>
@@ -16,7 +28,7 @@
         <error-message :error="error" />
       </v-col>
     </v-row>
-    <v-row v-else class="mint-deposit-dialog--progress" justify="center">
+    <v-row v-else class="udc__progress" justify="center">
       <v-col cols="10">
         <v-row no-gutters align="center" justify="center">
           <v-progress-circular
@@ -56,16 +68,18 @@
       class="mint-deposit-dialog__available"
       justify="center"
     >
-      <p>
-        {{
-          $t('mint-deposit-dialog.available', {
-            balance: accountBalance,
-            currency: $t('app-header.currency')
-          })
-        }}
-      </p>
+      <v-col cols="10" class="text-center">
+        <p>
+          {{
+            $t('mint-deposit-dialog.available', {
+              balance: accountBalance,
+              currency: $t('app-header.currency')
+            })
+          }}
+        </p>
+      </v-col>
     </v-row>
-    <v-row justify="center">
+    <v-row v-if="!loading && !error" justify="center">
       <action-button
         arrow
         :enabled="valid && !loading"
@@ -81,15 +95,21 @@
     </v-row>
     <v-row justify="center">
       <v-col cols="10">
-        <p>
-          UDC Balance:
+        <p
+          :class="{
+            'udc__low-balance': !hasEnoughServiceTokens
+          }"
+        >
+          {{ $t('udc.balance') }}
           <amount-display inline :amount="udcCapacity" :token="udcToken" />
         </p>
-        <p>
-          Your SVT balance is too low to pay for the Monitoring Service.
-          Receiving transfers has been temporarily disabled. Use the button aove
-          to mint an deposit more tokens. A minimum amount of 5 SVT is needed to
-          enable receiving.
+        <p
+          v-if="!hasEnoughServiceTokens"
+          :class="{
+            'udc__low-balance': !hasEnoughServiceTokens
+          }"
+        >
+          {{ $t('udc.balance-too-low') }}
         </p>
       </v-col>
     </v-row>
@@ -99,16 +119,18 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import { mapState } from 'vuex';
-
-import { Token } from '@/model/types';
-import ActionButton from '@/components/ActionButton';
-import AmountDisplay from '@/components/AmountDisplay';
-import MintDepositDialog from '@/components/dialogs/MintDepositDialog';
 import { RaidenError } from 'raiden-ts';
 import { Zero } from 'ethers/constants';
 
+import { Token } from '@/model/types';
+import { BalanceUtils } from '@/utils/balance-utils';
+import ActionButton from '@/components/ActionButton.vue';
+import AmountDisplay from '@/components/AmountDisplay.vue';
+import ErrorMessage from '@/components/ErrorMessage.vue';
+import MintDepositDialog from '@/components/dialogs/MintDepositDialog.vue';
+
 @Component({
-  components: { ActionButton, MintDepositDialog, AmountDisplay },
+  components: { ActionButton, MintDepositDialog, AmountDisplay, ErrorMessage },
   computed: {
     ...mapState(['accountBalance'])
   }
@@ -130,6 +152,38 @@ export default class UDC extends Vue {
     await this.updateUDCCapacity();
   }
 
+  get valid(): boolean {
+    return /^[1-9]\d*$/.test(this.amount);
+  }
+
+  async mintDeposit() {
+    this.error = null;
+    this.loading = true;
+
+    const tokenAddress = this.$raiden.userDepositTokenAddress;
+    const token: Token = this.$store.state.tokens[tokenAddress];
+    const depositAmount = BalanceUtils.parse(this.amount, token.decimals!);
+
+    try {
+      if (depositAmount.gt(token.balance!)) {
+        this.step = 'mint';
+        await this.$raiden.mint(tokenAddress, depositAmount);
+      }
+
+      this.step = 'approve';
+      await this.$raiden.depositToUDC(depositAmount, () => {
+        this.step = 'deposit';
+      });
+      await this.updateUDCCapacity();
+      this.$emit('done');
+    } catch (e) {
+      this.error = e;
+    }
+
+    this.step = '';
+    this.loading = false;
+  }
+
   private async updateUDCCapacity() {
     const { userDepositTokenAddress, monitoringReward } = this.$raiden;
     await this.$raiden.fetchTokenData([userDepositTokenAddress]);
@@ -146,6 +200,70 @@ export default class UDC extends Vue {
 <style scoped lang="scss">
 @import '@/scss/colors';
 
+hr {
+  border: 0.5px solid #707070;
+  margin-top: 5px;
+}
+
 .udc {
+  &__sub-head {
+    font-size: 20px;
+    text-align: center;
+  }
+
+  &__low-balance {
+    color: $error-color;
+  }
+
+  &__progress {
+    margin-top: 20px;
+    span {
+      margin-top: 22px;
+    }
+  }
+
+  &__mint-deposit {
+    &--amount {
+      font-size: 24px;
+      font-weight: bold;
+      color: $color-white;
+
+      ::v-deep {
+        input {
+          text-align: center;
+          max-width: 40px;
+          max-height: none;
+        }
+
+        .v-input {
+          &__slot {
+            &::before {
+              border: none !important;
+            }
+
+            &::after {
+              border: none !important;
+            }
+          }
+        }
+
+        .v-text-field {
+          &__details {
+            display: none;
+          }
+
+          &__slot {
+            justify-content: center;
+          }
+
+          &__suffix {
+            padding-left: 8px;
+            color: white;
+            padding-right: 18px;
+          }
+        }
+      }
+    }
+  }
 }
 </style>
