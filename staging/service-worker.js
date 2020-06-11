5524151daf4621dec0a8df84d49dc782ccc3ee5f
Continuous <no-reply@raiden.network>
2020-06-11 11:54:58 +0000

    Automated deployment to GitHub Pages: 1afc961c43985ef8eb9f5b8aec87b7be90dad543 [skip ci]

--- staging/service-worker.js
+++ staging/service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/staging/precache-manifest.0e9c22e13af85fd1504a625ae29f3d2d.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/staging/precache-manifest.b1adfdc31e3644b542e4ab733e48effc.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 self.addEventListener('message', e => {
   if (!e.data) {
