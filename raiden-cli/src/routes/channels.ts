57dbd1a0e3fefcf4f33ea62b15b2d6536d8120a0
Thore Weilbier <thore@weilbier.net>
2020-06-15 11:44:14 +0200

    cli: implement endpoint to create new channel
    This is the first endpoint that handles error messages by the SDK.
    Therefore the approach can be summarized the following:
      - forward/fill-in request parameter into SDK interface method
      - search for created object if call was successful
      - evaluate error in case call has failed
        - group errors by their messages/codes
        - map error groups to a HTTP return code
    
    Some error groups that are likely to be common enough to become re-used
    among multiple endpoints have been already exported to the validation
    module.

--- raiden-cli/src/routes/channels.ts
+++ raiden-cli/src/routes/channels.ts
@@ -1,12 +1,24 @@
-import { Router, Request, Response } from 'express';
+import { Router, Request, Response, NextFunction } from 'express';
 import { first } from 'rxjs/operators';
+import { ErrorCodes, RaidenError } from 'raiden-ts';
 import { Cli } from '../types';
-import { validateAddressParameter } from '../utils/validation';
+import {
+  validateAddressParameter,
+  isInvalidParameterError,
+  isTransactionWouldFailError,
+} from '../utils/validation';
 import {
   flattenChannelDictionary,
   transformSdkChannelFormatToApi,
   filterChannels,
 } from '../utils/channels';
+
+function isConflictError(error: RaidenError): boolean {
+  return (
+    [ErrorCodes.RDN_UNKNOWN_TOKEN_NETWORK, ErrorCodes.CNL_INVALID_STATE].includes(error.message) ||
+    isTransactionWouldFailError(error)
+  );
+}
 
 export function makeChannelsRouter(this: Cli): Router {
   const router = Router();
@@ -52,8 +64,30 @@ export function makeChannelsRouter(this: Cli): Router {
     },
   );
 
-  router.put('/', (_request: Request, response: Response) => {
-    response.status(404).send('Not implemented yet');
+  router.put('/', async (request: Request, response: Response, next: NextFunction) => {
+    try {
+      // TODO: We ignore the provided `reveal_timeout` until #1656 provides
+      // a better solution.
+      await this.raiden.openChannel(request.body.token_address, request.body.partner_address, {
+        settleTimeout: request.body.settle_timeout,
+        deposit: request.body.total_deposit,
+      });
+      const channelDictionary = await this.raiden.channels$.pipe(first()).toPromise();
+      const newChannel =
+        channelDictionary[request.body.token_address][request.body.partner_address];
+      response.status(201).json(newChannel);
+    } catch (error) {
+      console.log(error.code);
+      if (isInvalidParameterError(error)) {
+        response.status(400).send(error.message);
+      } else if (isConflictError(error)) {
+        response.status(409).send(error.message);
+      } else {
+        next(error);
+      }
+
+      // FIXME: Based on #1698 there must be a case with status code 402
+    }
   });
 
   router.patch('/:tokenAddress/:partnerAddress', (_request: Request, response: Response) => {
