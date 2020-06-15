6bb2500fb4fc68a3f472066b896bb04f9b692f4c
Thore Weilbier <thore@weilbier.net>
2020-06-15 16:14:39 +0200

    cli: reduce code complexity on channels router
    This was triggered by a report of codeclimate. Due to the design
    decision that the routes should export per default a function
    explicitly so that is can become bound to a context, the channel router
    (as currently only implemented router) become to big. So the number of
    lines that act as a complexity parameter was too big.
    Therefore the router middleware functions have become exported to
    standalone functions instead of arrow functions. They again get bound to
    the global context. An additionally gained advantage is that the router
    definitions reads more nice now.

--- raiden-cli/src/routes/channels.ts
+++ raiden-cli/src/routes/channels.ts
@@ -20,75 +20,82 @@ function isConflictError(error: RaidenError): boolean {
   );
 }
 
+async function getAllChannels(this: Cli, _request: Request, response: Response) {
+  const channelDictionary = await this.raiden.channels$.pipe(first()).toPromise();
+  const channelList = flattenChannelDictionary(channelDictionary);
+  const formattedChannels = transformSdkChannelFormatToApi(channelList);
+  response.json(formattedChannels);
+}
+
+async function getChannelsForToken(this: Cli, request: Request, response: Response) {
+  const channelDictionary = await this.raiden.channels$.pipe(first()).toPromise();
+  const channelList = flattenChannelDictionary(channelDictionary);
+  const filteredChannels = filterChannels(channelList, request.params.tokenAddress);
+  const formattedChannels = transformSdkChannelFormatToApi(filteredChannels);
+  response.json(formattedChannels);
+}
+
+async function getChannelsForTokenAndPartner(this: Cli, request: Request, response: Response) {
+  const channelDictionary = await this.raiden.channels$.pipe(first()).toPromise();
+  const channelList = flattenChannelDictionary(channelDictionary);
+  const filteredChannels = filterChannels(
+    channelList,
+    request.params.tokenAddress,
+    request.params.partnerAddress,
+  );
+  const formattedChannels = transformSdkChannelFormatToApi(filteredChannels);
+
+  if (formattedChannels.length) {
+    response.json(formattedChannels[0]);
+  } else {
+    response.status(404).send('The channel does not exist');
+  }
+}
+
+async function openChannel(this: Cli, request: Request, response: Response, next: NextFunction) {
+  try {
+    // TODO: We ignore the provided `reveal_timeout` until #1656 provides
+    // a better solution.
+    await this.raiden.openChannel(request.body.token_address, request.body.partner_address, {
+      settleTimeout: request.body.settle_timeout,
+      deposit: request.body.total_deposit,
+    });
+    const channelDictionary = await this.raiden.channels$.pipe(first()).toPromise();
+    const newChannel = channelDictionary[request.body.token_address][request.body.partner_address];
+    response.status(201).json(newChannel);
+  } catch (error) {
+    console.log(error.code);
+    if (isInvalidParameterError(error)) {
+      response.status(400).send(error.message);
+    } else if (isConflictError(error)) {
+      response.status(409).send(error.message);
+    } else {
+      next(error);
+    }
+
+    // FIXME: Based on #1698 there must be a case with status code 402
+  }
+}
+
 export function makeChannelsRouter(this: Cli): Router {
   const router = Router();
 
-  router.get('/', async (_request: Request, response: Response) => {
-    const channelDictionary = await this.raiden.channels$.pipe(first()).toPromise();
-    const channelList = flattenChannelDictionary(channelDictionary);
-    const formattedChannels = transformSdkChannelFormatToApi(channelList);
-    response.json(formattedChannels);
-  });
+  router.get('/', getAllChannels.bind(this));
 
   router.get(
     '/:tokenAddress',
     validateAddressParameter.bind('tokenAddress'),
-    async (request: Request, response: Response) => {
-      const channelDictionary = await this.raiden.channels$.pipe(first()).toPromise();
-      const channelList = flattenChannelDictionary(channelDictionary);
-      const filteredChannels = filterChannels(channelList, request.params.tokenAddress);
-      const formattedChannels = transformSdkChannelFormatToApi(filteredChannels);
-      response.json(formattedChannels);
-    },
+    getChannelsForToken.bind(this),
   );
 
   router.get(
     '/:tokenAddress/:partnerAddress',
     validateAddressParameter.bind('tokenAddress'),
     validateAddressParameter.bind('partnerAddress'),
-    async (request: Request, response: Response) => {
-      const channelDictionary = await this.raiden.channels$.pipe(first()).toPromise();
-      const channelList = flattenChannelDictionary(channelDictionary);
-      const filteredChannels = filterChannels(
-        channelList,
-        request.params.tokenAddress,
-        request.params.partnerAddress,
-      );
-      const formattedChannels = transformSdkChannelFormatToApi(filteredChannels);
-
-      if (formattedChannels.length) {
-        response.json(formattedChannels[0]);
-      } else {
-        response.status(404).send('The channel does not exist');
-      }
-    },
+    getChannelsForTokenAndPartner.bind(this),
   );
 
-  router.put('/', async (request: Request, response: Response, next: NextFunction) => {
-    try {
-      // TODO: We ignore the provided `reveal_timeout` until #1656 provides
-      // a better solution.
-      await this.raiden.openChannel(request.body.token_address, request.body.partner_address, {
-        settleTimeout: request.body.settle_timeout,
-        deposit: request.body.total_deposit,
-      });
-      const channelDictionary = await this.raiden.channels$.pipe(first()).toPromise();
-      const newChannel =
-        channelDictionary[request.body.token_address][request.body.partner_address];
-      response.status(201).json(newChannel);
-    } catch (error) {
-      console.log(error.code);
-      if (isInvalidParameterError(error)) {
-        response.status(400).send(error.message);
-      } else if (isConflictError(error)) {
-        response.status(409).send(error.message);
-      } else {
-        next(error);
-      }
-
-      // FIXME: Based on #1698 there must be a case with status code 402
-    }
-  });
+  router.put('/', openChannel.bind(this));
 
   router.patch('/:tokenAddress/:partnerAddress', (_request: Request, response: Response) => {
     response.status(404).send('Not implemented yet');
