ffc8e11d18b0acfe12749e601d01d6aa6ca7da47
André Vitor de Lima Matos <andre.vmatos@gmail.com>
2020-06-17 17:45:38 -0300

    cli,dApp: Change optional operators

--- raiden-cli/src/utils/channels.ts
+++ raiden-cli/src/utils/channels.ts
@@ -26,17 +26,16 @@ export function filterChannels(
   return filteredChannels;
 }
 
-/* eslint-disable @typescript-eslint/camelcase */
 export function transformSdkChannelFormatToApi(channel: RaidenChannel): ApiChannel {
   return {
-    channel_identifier: channel.id ?? 0, // FIXME: old "bug" in the SDK
+    channel_identifier: channel.id,
     token_network_address: channel.tokenNetwork,
     partner_address: channel.partner,
     token_address: channel.token,
     balance: channel.balance.toString(),
     total_deposit: channel.ownDeposit.toString(),
     state: channel.state,
-    settle_timeout: channel.settleTimeout ?? 0,
+    settle_timeout: channel.settleTimeout,
     reveal_timeout: 0, // FIXME: Not defined here. Python client handles reveal timeout differently,
   };
 }
