96eed07530bc51025dfa70818435aff471960bea
André Vitor de Lima Matos <andre.vmatos@gmail.com>
2020-06-22 10:50:58 -0300

    Fix multiple workflows and add .gitignore

--- serve_pr/ci_dapp.sh
+++ serve_pr/ci_dapp.sh
@@ -12,19 +12,24 @@ BASE="https://circleci.com/api/v2"
 PIPELINES=(`$CURL "$BASE/project/${CIRCLE_SLUG}/pipeline?${QUERY}" | grep -B1 -A0 '"errors" : \[\s*\]' | grep -oP '(?<="id" : ")[^"]+'`)
 for PIPELINE in $PIPELINES; do
   echo "Pipeline: ${PIPELINE}" >&2
-  WORKFLOW=`$CURL "$BASE/pipeline/${PIPELINE}/workflow" | grep -oP '(?<="id" : ")[^"]+'`
-  echo "Workflow: ${WORKFLOW}" >&2
-  JOB=`$CURL "$BASE/workflow/${WORKFLOW}/job" | grep -B5 -A0 '"status" : "success"' | grep -B3 -A0 '"name" : "build_dapp"' | grep -oP '(?<="job_number" : )\d+' | head -1`
-  URL=`$CURL "$BASE/project/${CIRCLE_SLUG}/${JOB}/artifacts" | grep -oP '(?<="url" : ")[^"]+' | head -1`
-  PULL=`curl "https://circleci.com/api/v1.1/project/${CIRCLE_SLUG}/${JOB}?circle-token=${CIRCLE_TOKEN}" | grep -B0 -A2 '"pull_requests"' | grep -oP '(?<=/pull/)\d+' | head -1`
-  PULLDIR="${PWD}/${PULL}"
-  mkdir -p "${PULLDIR}"
-  OLDFILE="$PULLDIR/dapp.prev.tar.gz"
-  OUTFILE="$PULLDIR/dapp.tar.gz"
-  $CURL -L -o "$OUTFILE" -z "$OLDFILE" "$URL" && break
+  WORKFLOWS=(`$CURL "$BASE/pipeline/${PIPELINE}/workflow" | grep -oP '(?<="id" : ")[^"]+'`)
+  for WORKFLOW in $WORKFLOWS; do
+    echo "Workflow: ${WORKFLOW}" >&2
+    JOB=`$CURL "$BASE/workflow/${WORKFLOW}/job" | grep -B5 -A0 '"status" : "success"' | grep -B3 -A0 '"name" : "build_dapp"' | grep -oP '(?<="job_number" : )\d+' | head -1`
+    URL=`$CURL "$BASE/project/${CIRCLE_SLUG}/${JOB}/artifacts" | grep -oP '(?<="url" : ")[^"]+' | head -1`
+    PULL=`curl "https://circleci.com/api/v1.1/project/${CIRCLE_SLUG}/${JOB}?circle-token=${CIRCLE_TOKEN}" | grep -B0 -A2 '"pull_requests"' | grep -oP '(?<=/pull/)\d+' | head -1`
+    [[ -n "$PULL" ]] || continue
+    PULLDIR="${PWD}/${PULL}"
+    mkdir -p "${PULLDIR}"
+    OLDFILE="$PULLDIR/dapp.prev.tar.gz"
+    OUTFILE="$PULLDIR/dapp.tar.gz"
+    $CURL -L -o "$OUTFILE" -z "$OLDFILE" "$URL" && SUCCESS=1
+    [[ "$SUCCESS" == 1 ]] && break
+  done
+  [[ "$SUCCESS" == 1 ]] && break
 done
 
-[[ -d "$PULLDIR" ]] || exit 1
+[[ "$SUCCESS" == 1 && -d "$PULLDIR" ]] || exit 1
 if [[ -e "$OUTFILE" ]]; then
   rm -rf "${PULLDIR}/dist"
   tar -xvf "$OUTFILE" -C "$PULLDIR" >&2
