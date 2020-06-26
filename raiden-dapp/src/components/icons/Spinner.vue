0d36a42a719722eaf8df45b7abfe0eaed70b22ab
Thore Weilbier <thore@weilbier.net>
2020-06-26 15:12:17 +0200

    dapp: rework spinner component
    The progress components by Vuetify can cause heavy hardware issues for
    certain computers. Since this is a serious issues that is apparently
    hard to get under control, it has been decided to use a custom progress
    component. Such has proven to cause nearly no performance issues at all.
    Therefore the old Spinner component has been revived. It was still
    partially used, but was mainly referenced by dead code. It has been
    reworked to become a proper component working for all locations where
    getting used. This allowed to make the templates where a progress
    components more slim, letting the component do the work.
    
    Related to: #1773

--- raiden-dapp/src/components/icons/Spinner.vue
+++ raiden-dapp/src/components/icons/Spinner.vue
@@ -1,16 +1,61 @@
 <template>
-  <v-progress-circular
-    :size="110"
-    :width="7"
-    class="spinner"
-    color="primary"
-    indeterminate
-  ></v-progress-circular>
+  <div :class="{ 'spinner--blocking': !inline }">
+    <div class="spinner__circle" :style="style" />
+  </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+import { Component, Vue, Prop } from 'vue-property-decorator';
 
 @Component({})
-export default class Spinner extends Vue {}
+export default class Spinner extends Vue {
+  @Prop({ type: Boolean, default: false })
+  inline!: boolean;
+
+  @Prop({ type: Number, default: 120 })
+  size!: number;
+
+  @Prop({ type: Number, default: 7 })
+  width!: number;
+
+  get style() {
+    return {
+      width: `${this.size}px`,
+      height: `${this.size}px`,
+      borderWidth: `${this.width}px`
+    };
+  }
+}
 </script>
+
+<style scoped lang="scss">
+@import '@/scss/colors';
+
+.spinner {
+  &--blocking {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  &__circle {
+    border: solid transparent;
+    border-top: solid $primary-color;
+    border-radius: 50%;
+    animation: spinning 1s linear infinite;
+  }
+}
+
+@keyframes spinning {
+  0% {
+    transform: rotate(0deg);
+  }
+  60% {
+    transform: rotate(270deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
