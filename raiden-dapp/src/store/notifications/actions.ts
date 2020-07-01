34033f81587b370e516aba5a3b20752087c5c505
Konstantinos Paparas <kelsos86@gmail.com>
2020-07-01 08:03:39 +0200

    dapp: replaces warning with notifications

--- raiden-dapp/src/store/notifications/actions.ts
+++ raiden-dapp/src/store/notifications/actions.ts
@@ -1,16 +1,18 @@
 import { ActionTree } from 'vuex';
 import { RootState } from '@/types';
-import { NotificationsState } from '@/store/notifications/types';
+import { Notification, NotificationsState } from '@/store/notifications/types';
 
 export const actions: ActionTree<NotificationsState, RootState> = {
   notify: (
     { commit, getters: { nextNotificationId } },
     notification: Notification
   ) => {
-    commit('notificationAdd', {
+    const payload: Notification = {
       ...notification,
       id: nextNotificationId,
       received: new Date(),
-    });
+    };
+    commit('notificationAdd', payload);
+    commit('notificationShow', payload);
   },
 };
