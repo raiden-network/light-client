91bd982b6bbac2e3bbb25c707843275355b9922b
taleldayekh <mail@talel.se>
2020-07-06 13:36:01 +0200

    Refactored AmountInput component

--- raiden-dapp/src/components/AmountInput.vue
+++ raiden-dapp/src/components/AmountInput.vue
@@ -6,12 +6,12 @@
       ref="input"
       :class="{ invalid: !valid }"
       :disabled="disabled"
-      :rules="rules"
       :value="amount"
       :placeholder="placeholder"
+      :error-messages="!hideErrorLabel ? errorMessages : []"
       autocomplete="off"
-      @paste="onPaste($event)"
       @input="onInput($event)"
+      @paste="onPaste($event)"
     >
       <div slot="append" class="amount-input__token-symbol">
         {{ token.symbol || 'TKN' }}
@@ -21,7 +21,7 @@
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue, Watch } from 'vue-property-decorator';
+import { Component, Vue, Prop, Watch, Emit } from 'vue-property-decorator';
 import { Token } from '@/model/types';
 import { BalanceUtils } from '@/utils/balance-utils';
 import { BigNumber } from 'ethers/utils';
@@ -31,73 +31,25 @@ import { Zero } from 'ethers/constants';
 export default class AmountInput extends Vue {
   @Prop({ required: false })
   label?: string;
-  @Prop({})
-  disabled!: boolean;
-  @Prop({ required: true })
-  value!: string;
+  @Prop({ required: false, default: false, type: Boolean })
+  hideErrorLabel!: boolean;
   @Prop()
   token?: Token;
   @Prop({ default: false, type: Boolean })
   limit!: boolean;
-  @Prop({ default: '', type: String })
-  placeholder!: string;
+  @Prop({ required: true })
+  value!: string;
   @Prop({ required: false, default: () => Zero })
   max!: BigNumber;
+  @Prop({})
+  disabled!: boolean;
+  @Prop({ default: '', type: String })
+  placeholder!: string;
 
   valid: boolean = true;
   amount: string = '';
-  private static numericRegex = /^\d*[.]?\d*$/;
-
-  readonly rules = [
-    (v: string) => !!v || this.$parent.$t('amount-input.error.empty'),
-    (v: string) =>
-      !Number.isNaN(Number(v)) || this.$parent.$t('amount-input.error.invalid'),
-    (v: string) =>
-      !this.limit ||
-      (v && this.noDecimalOverflow(v)) ||
-      this.$parent.$t('amount-input.error.too-many-decimals', {
-        decimals: this.token!.decimals
-      }),
-    (v: string) => {
-      let parsedAmount;
-      try {
-        parsedAmount = BalanceUtils.parse(v, this.token!.decimals!);
-      } catch (e) {}
-      return (
-        !this.limit ||
-        (v && parsedAmount && !parsedAmount.isZero()) ||
-        this.$parent.$t('amount-input.error.zero')
-      );
-    },
-    (v: string) =>
-      !this.limit ||
-      (v && this.hasEnoughBalance(v, this.max)) ||
-      this.$parent.$t('amount-input.error.not-enough-funds', {
-        funds: BalanceUtils.toUnits(this.max, this.token!.decimals ?? 18),
-        symbol: this.token!.symbol
-      })
-  ];
-
-  private noDecimalOverflow(v: string) {
-    return (
-      AmountInput.numericRegex.test(v) &&
-      !BalanceUtils.decimalsOverflow(v, this.token!.decimals ?? 18)
-    );
-  }
-
-  private hasEnoughBalance(v: string, max: BigNumber) {
-    return (
-      !Number.isNaN(Number(v)) &&
-      !BalanceUtils.decimalsOverflow(v, this.token!.decimals!) &&
-      BalanceUtils.parse(v, this.token!.decimals!).lte(max)
-    );
-  }
-
-  private updateIfValid(value: string) {
-    if (value !== this.amount && !Number.isNaN(Number(value))) {
-      this.amount = value;
-    }
-  }
+  errorMessages: string[] = [];
+  numericRegEx = /^\d*[.]?\d*$/;
 
   @Watch('value', { immediate: true })
   onChange(value: string | undefined) {
@@ -113,24 +65,13 @@ export default class AmountInput extends Vue {
     (this.$refs.input as any).validate();
   }
 
-  mounted() {
-    this.updateIfValid(this.value);
+  @Emit()
+  inputError(errorMessage: string) {
+    return errorMessage;
   }
 
-  onPaste(event: ClipboardEvent) {
-    const clipboardData = event.clipboardData;
-
-    if (!clipboardData) {
-      return;
-    }
-
-    const value = clipboardData.getData('text');
-    if (!AmountInput.numericRegex.test(value)) {
-      event.preventDefault();
-    } else {
-      const input = event.target as HTMLInputElement;
-      input.setSelectionRange(0, input.value.length);
-    }
+  mounted() {
+    this.updateIfValid(this.value);
   }
 
   onInput(value: string) {
@@ -141,6 +82,98 @@ export default class AmountInput extends Vue {
     }
 
     this.$emit('input', value);
+    this.inputError(this.errorMessages[0]);
+  }
+
+  onPaste(event: ClipboardEvent) {
+    const clipboardData = event.clipboardData;
+
+    if (!clipboardData) {
+      return;
+    }
+
+    const value = clipboardData.getData('text');
+
+    if (!this.numericRegEx.test(value)) {
+      event.preventDefault();
+    } else {
+      const input = event.target as HTMLInputElement;
+      input.setSelectionRange(0, input.value.length);
+    }
+  }
+
+  updateIfValid(value: string) {
+    this.errorMessages.pop();
+
+    switch (false) {
+      case this.noEmptyValue(value):
+        this.errorMessages.push(this.$t('amount-input.error.empty') as string);
+        break;
+      case this.noValidInput(value):
+        this.errorMessages.push(
+          this.$t('amount-input.error.invalid') as string
+        );
+        break;
+      case this.noDecimalOverflow(value):
+        this.errorMessages.push(
+          this.$t('amount-input.error.too-many-decimals', {
+            decimals: this.token!.decimals
+          }) as string
+        );
+        break;
+      case this.parsedAmount(value):
+        this.errorMessages.push(this.$t('amount-input.error.zero') as string);
+        break;
+      case this.hasEnoughBalance(value, this.max):
+        this.errorMessages.push(
+          this.$t('amount-input.error.not-enough-funds', {
+            funds: BalanceUtils.toUnits(this.max, this.token!.decimals ?? 18),
+            symbol: this.token!.symbol
+          }) as string
+        );
+        break;
+      default:
+        if (value !== this.amount && !Number.isNaN(Number(value))) {
+          this.amount = value;
+        }
+    }
+  }
+
+  noEmptyValue(value: string): boolean {
+    return !!value;
+  }
+
+  noValidInput(value: string): boolean {
+    return !Number.isNaN(Number(value));
+  }
+
+  noDecimalOverflow(value: string): boolean {
+    const validDecimalsCount =
+      value &&
+      !BalanceUtils.decimalsOverflow(value, this.token!.decimals ?? 18);
+
+    return !this.limit || validDecimalsCount;
+  }
+
+  parsedAmount(value: string): boolean {
+    let parsedAmount;
+
+    try {
+      parsedAmount = BalanceUtils.parse(value, this.token!.decimals!);
+    } catch (e) {}
+
+    const validParsedAmount = value && parsedAmount && !parsedAmount.isZero();
+    return !this.limit || validParsedAmount;
+  }
+
+  hasEnoughBalance(value: string, max: BigNumber): boolean {
+    const validBalance =
+      value &&
+      !Number.isNaN(Number(value)) &&
+      !BalanceUtils.decimalsOverflow(value, this.token!.decimals!) &&
+      BalanceUtils.parse(value, this.token!.decimals!).lte(max);
+
+    return !this.limit || validBalance;
   }
 }
 </script>
@@ -149,40 +182,33 @@ export default class AmountInput extends Vue {
 @import '../scss/mixins';
 @import '../scss/colors';
 @import '../scss/fonts';
-
 $header-vertical-margin: 5rem;
 $header-vertical-margin-mobile: 2rem;
-
 .amount-input {
   display: flex;
   align-items: center;
   justify-content: center;
   flex-direction: column;
   border: 0;
-
   @include respond-to(handhelds) {
     padding-top: 30px;
     padding-bottom: 30px;
     border: 0;
   }
-
   ::v-deep {
     .v-input {
       width: 100%;
-
       &__slot {
         border-radius: 10px;
         background-color: $input-background !important;
         padding: 8px 16px;
         max-height: 49px;
         border: 1.5px solid transparent;
-
         &::before,
         &::after {
           border-width: 0 0 0 0;
         }
       }
-
       &--is-focused {
         .v-input {
           &__slot {
@@ -191,24 +217,20 @@ $header-vertical-margin-mobile: 2rem;
         }
       }
     }
-
     input {
       font-family: $main-font;
       font-size: 16px;
       line-height: 20px;
       caret-color: white !important;
-
       &:focus {
         outline: 0;
       }
     }
-
     .v-messages {
       border: 1px solid transparent;
       font-family: $main-font;
       font-size: 14px;
       line-height: 16px;
-
       &__wrapper {
         display: flex;
         flex-direction: column;
@@ -216,19 +238,16 @@ $header-vertical-margin-mobile: 2rem;
         padding-left: 20px;
         justify-content: center;
         color: white;
-
         @include respond-to(handhelds) {
           padding-left: 10px;
         }
       }
-
       &__message {
         color: $secondary-text-color;
         line-height: 1.1;
       }
     }
   }
-
   &__token-symbol {
     font-family: $main-font;
     color: $text-color;
@@ -237,7 +256,6 @@ $header-vertical-margin-mobile: 2rem;
     line-height: 21px;
     text-align: center;
   }
-
   &__label {
     color: $secondary-color;
     font-size: 13px;
