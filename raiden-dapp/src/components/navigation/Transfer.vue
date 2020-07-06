2699d5d04f1937af03cd756ab0a1b3c95fec90a5
taleldayekh <mail@talel.se>
2020-07-06 15:56:08 +0200

    Reworked transfer component

--- raiden-dapp/src/components/navigation/Transfer.vue
+++ raiden-dapp/src/components/navigation/Transfer.vue
@@ -1,55 +1,74 @@
 <template>
   <v-container fluid class="transfer">
-    <v-row justify="center" no-gutters>
-      <amount-display
-        class="transfer__token-network-amount"
-        exact-amount
-        :amount="capacity"
-        :token="token"
-      />
+    <div class="transfer__menus">
+      <v-row no-gutters class="transfer__menus__items">
+        <v-col cols="11">
+          <div class="transfer__menus__items__token-select">
+            <span @click="showTokenNetworks = true">
+              {{ $t('transfer.change-token-title') }}
+              <v-icon>mdi-chevron-down</v-icon>
+            </span>
+          </div>
+        </v-col>
+        <v-col cols="1">
+          <v-menu transition="scale-transition">
+            <template #activator="{ on }">
+              <v-btn icon v-on="on">
+                <v-icon large>mdi-dots-vertical</v-icon>
+              </v-btn>
+            </template>
+            <div class="transfer__menus__items__channels-deposit">
+              <v-row justify="center">
+                <span
+                  class="transfer__menus__items__channels-deposit--deposit"
+                  @click="depositing = true"
+                >
+                  {{ $t('transfer.deposit-button') }}
+                </span>
+              </v-row>
+              <v-row justify="center">
+                <span
+                  class="transfer__menus__items__channels-deposit--channels"
+                  @click="navigateToChannels(token.address)"
+                >
+                  {{ $t('transfer.channel-button') }}
+                </span>
+              </v-row>
+            </div>
+          </v-menu>
+        </v-col>
+      </v-row>
+      <v-row class="transfer__selected-token" no-gutters justify="center">
+        <v-row no-gutters justify="center">
+          <amount-display
+            class="transfer__selected-token__amount"
+            exact-amount
+            :amount="capacity"
+            :token="token"
+          />
+        </v-row>
+        <v-row
+          no-gutters
+          class="transfer__selected-token__symbol"
+          justify="center"
+        >
+          {{ token.name | truncate(22) }}
+        </v-row>
+      </v-row>
       <token-overlay
         :show="showTokenNetworks"
         @cancel="showTokenNetworks = false"
       />
-    </v-row>
-    <v-row class="transfer__actions" justify="center" no-gutters>
-      <v-col class="transfer__actions__channel-button">
-        <action-button
-          :text="$t('transfer.channel-button')"
-          ghost
-          enabled
-          full-width
-          @click="navigateToChannels(token.address)"
-        />
-      </v-col>
-      <v-col class="transfer__actions__token-button">
-        <action-button
-          :text="token.name | truncate(22)"
-          ghost
-          enabled
-          full-width
-          @click="showTokenNetworks = true"
-        />
-      </v-col>
-      <v-col class="transfer__actions__deposit-button">
-        <action-button
-          :text="$t('transfer.deposit-button')"
-          ghost
-          full-width
-          enabled
-          @click="depositing = true"
-        />
-        <channel-deposit-dialog
-          :loading="loading"
-          :done="done"
-          :token="token"
-          :visible="depositing"
-          identifier="0"
-          @cancel="depositing = false"
-          @depositTokens="deposit($event)"
-        />
-      </v-col>
-    </v-row>
+      <channel-deposit-dialog
+        :loading="loading"
+        :done="done"
+        :token="token"
+        :visible="depositing"
+        identifier="0"
+        @cancel="depositing = false"
+        @depositTokens="deposit($event)"
+      />
+    </div>
     <div class="transfer__form-container">
       <v-form
         ref="transfer"
@@ -58,37 +77,54 @@
         novalidate
         @submit.prevent="navigateToTransferSteps(target, amount)"
       >
-        <v-row class="transfer__form-container__title" no-gutters>
-          {{ $t('transfer.transfer-title') }}
-        </v-row>
         <div class="transfer__form-container__form">
-          <v-row class="transfer__form-container__form__inputs" no-gutters>
-            <v-col>
-              <address-input
-                v-model="target"
-                class="transfer__form-container__form__inputs__address-input"
-                :exclude="[token.address, defaultAccount]"
-                :block="blockedHubs"
-              />
+          <v-row class="transfer__form-container__form__heading" no-gutters>
+            <v-col
+              class="transfer__form-container__form__heading--title"
+              cols="2"
+            >
+              {{ $t('transfer.transfer-title') }}
             </v-col>
-            <v-col>
-              <amount-input
-                v-model="amount"
-                class="transfer__form-container__form__inputs__amount-input"
-                :token="token"
-                :placeholder="$t('transfer.amount-placeholder')"
-                :max="capacity"
-                limit
-              />
+            <v-col
+              class="transfer__form-container__form__heading--error"
+              cols="10"
+            >
+              <span>
+                {{ addressError }}
+              </span>
+              <span>
+                {{ amountError }}
+              </span>
             </v-col>
           </v-row>
-          <v-row no-gutters>
+          <v-row class="transfer__form-container__form__address" no-gutters>
+            <address-input
+              v-model="target"
+              class="transfer__form-container__form__address--input"
+              :exclude="[token.address, defaultAccount]"
+              :block="blockedHubs"
+              hide-error-label
+              @input-error="addressError = $event"
+            />
+          </v-row>
+          <v-row
+            class="transfer__form-container__form__amount-button"
+            no-gutters
+          >
+            <amount-input
+              v-model="amount"
+              class="transfer__form-container__form__amount-button--amount-input"
+              :placeholder="$t('transfer.amount-placeholder')"
+              hide-error-label
+              limit
+              :token="token"
+              :max="capacity"
+              @input-error="amountError = $event"
+            />
             <action-button
-              class="transfer__form-container__form__transfer-button"
-              full-width
+              class="transfer__form-container__form__amount-button--button"
               :enabled="valid"
               :text="$t('general.buttons.continue')"
-              arrow
             />
           </v-row>
         </div>
@@ -152,6 +188,8 @@ export default class Transfer extends Mixins(BlockieMixin, NavigationMixin) {
   depositing: boolean = false;
 
   error: Error | RaidenError | null = null;
+  addressError: string = '';
+  amountError: string = '';
 
   channels!: (tokenAddress: string) => RaidenChannel[];
 
@@ -251,129 +289,149 @@ export default class Transfer extends Mixins(BlockieMixin, NavigationMixin) {
     display: none;
   }
 
-  &__token-network-amount {
-    color: $color-white;
-    font-size: 24px;
-    font-weight: bold;
-    line-height: 19px;
-    margin: 20px 0 10px 0;
-    text-align: center;
+  &__menus {
+    background-color: $transfer-screen-color;
+    border-radius: 15px;
+    height: 175px;
+    margin: 0 auto;
+    width: 511px;
+    @include respond-to(handhelds) {
+      width: 100%;
+    }
+
+    &__items {
+      padding-top: 18px;
+      margin-left: 36px;
+
+      &__token-select {
+        display: flex;
+        justify-content: center;
+        align-items: flex-end;
+        height: 85%;
+        margin: 0 auto;
+        border-bottom: solid 1px $color-white;
+        width: 130px;
+      }
+
+      &__token-select span {
+        cursor: pointer;
+      }
+
+      &__channels-deposit {
+        background-color: $notification-card-background;
+        border-radius: 8px;
+        display: flex;
+        flex-direction: column;
+        height: 80px;
+        justify-content: center;
+        width: 145px;
+
+        &--deposit,
+        &--channels {
+          align-items: center;
+          display: flex;
+          cursor: pointer;
+        }
+      }
+    }
   }
 
-  &__actions {
-    margin-top: 10px;
-    display: flex;
+  &__selected-token {
+    flex-direction: column;
+    margin-top: 12px;
 
-    &__channel-button {
-      flex: 0 0 80px;
-      margin-left: 34px;
-      @include respond-to(handhelds) {
-        flex: 0 0 60;
-        margin: 0;
-      }
+    &__amount {
+      font-size: 36px;
     }
 
-    &__token-button {
-      @include respond-to(handhelds) {
-        margin: 0;
-        flex: 1;
-      }
-    }
-
-    &__deposit-button {
-      flex: 0 0 80px;
-      margin-right: 34px;
-      @include respond-to(handhelds) {
-        flex: 0 0 60px;
-        margin: 0;
-      }
+    &__symbol {
+      font-size: 18px;
+      padding-top: 10px;
     }
   }
 
   &__form-container {
-    margin-top: 50px;
-
-    &__title {
-      color: $secondary-text-color;
-      font-weight: bold;
-      margin-left: 45px;
-      padding-bottom: 20px;
-      @include respond-to(handhelds) {
-        margin: 0;
-      }
-    }
+    margin-top: 27px;
 
     &__form {
-      background-color: $transfer-form-color;
+      background-color: $transfer-screen-color;
       border-radius: 15px;
       margin: 0 auto;
       width: 511px;
-      padding-bottom: 16px;
-
       @include respond-to(handhelds) {
         width: 100%;
-        display: flex;
-        flex-direction: column;
-        padding-bottom: 6px;
       }
 
-      &__inputs {
+      &__address {
+        margin: 0 20px 0 20px;
+
+        &--input {
+          width: 100%;
+          @include respond-to(handhelds) {
+            padding: 0;
+          }
+        }
+      }
+
+      &__heading {
+        margin: 0 20px 0 20px;
+        padding-top: 16px;
         @include respond-to(handhelds) {
-          display: flex;
           flex-direction: column;
         }
-        &__address-input {
-          margin-left: 23px;
-          margin-top: 5px;
-          width: 226px;
-          @include respond-to(handhelds) {
-            flex: none;
-            margin: 0 auto;
-            padding: 0;
-            width: 90%;
-          }
+
+        &--title {
+          color: $secondary-text-color;
+          font-weight: bold;
         }
 
-        &__amount-input {
-          margin-left: auto;
-          margin-right: 23px;
-          margin-top: 5px;
-          width: 189px;
-          height: 140px;
-
-          ::v-deep {
-            .v-text-field {
-              &__details {
-                padding-top: 8px;
-              }
-            }
-          }
-
+        &--error {
+          display: flex;
+          flex-direction: column;
+          text-align: right;
+          color: var(--v-pending-base);
           @include respond-to(handhelds) {
-            flex: none;
-            margin: 0 auto;
-            padding: 0;
-            width: 90%;
+            justify-content: flex-start;
+            margin-top: 10px;
           }
         }
       }
 
-      &__transfer-button {
-        margin: 10px 23px 0 23px;
+      &__amount-button {
+        margin: 0 5px 0 20px;
         @include respond-to(handhelds) {
-          margin: 0 0 10px 0;
+          flex-direction: column;
+          margin: 0 20px 0 20px;
         }
 
-        ::v-deep {
-          .col-10 {
-            flex: 1;
-            max-width: 100%;
-            @include respond-to(handhelds) {
-              max-width: 90%;
-            }
+        &--amount-input {
+          flex: 0 0 300px;
+          @include respond-to(handhelds) {
+            flex: none;
+            padding: 0;
           }
-          .v-btn {
-            border-radius: 8px;
+        }
+
+        &--button {
+          ::v-deep {
+            .col-10 {
+              display: flex;
+              justify-content: flex-end;
+              padding-bottom: 8px;
+              @include respond-to(handhelds) {
+                flex: 1;
+                max-width: 100%;
+              }
+            }
+
+            .v-btn {
+              border-radius: 8px;
+              min-height: 48px;
+              max-width: 150px;
+              @include respond-to(handhelds) {
+                min-width: 100%;
+              }
+            }
           }
         }
       }
