49ab9d4e297dd53600192e15b78aa92cf301b370
André Vitor de Lima Matos <andre.vmatos@gmail.com>
2020-07-20 18:51:02 -0300

    dApp: hide new block actions, split logs per account in logs

--- raiden-dapp/src/utils/logstore.ts
+++ raiden-dapp/src/utils/logstore.ts
@@ -1,14 +1,16 @@
 import logging from 'loglevel';
 import { openDB, DBSchema, IDBPDatabase } from 'idb';
 
-const collectionName = 'logs';
+const storeName = 'logs';
+let lastBlockNumber: number | undefined = undefined;
 
 interface RaidenDB extends DBSchema {
-  [collectionName]: {
+  [storeName]: {
     value: {
       logger: string;
       level: string;
       message: any[];
+      block?: number;
     };
     key: number;
     indexes: {
@@ -37,7 +39,13 @@ function serializeError(e: Error): string {
 /* istanbul ignore next */
 function filterMessage(message: any[]) {
   if (message[0] === '%c prev state') return;
+  if (message[0]?.startsWith?.('action %c')) return;
   if (message[0] === '—— log end ——') return;
+  const blockNumber: number | undefined = message[2]?.payload?.blockNumber;
+  if (blockNumber) {
+    lastBlockNumber = blockNumber; // set lastBlockNumber and skip newBlock action
+    return;
+  }
   message = message.map((e) =>
     e instanceof Error
       ? serializeError(e)
@@ -64,19 +72,29 @@ function serialize(e: any): string {
   }
 }
 
-/* istanbul ignore next */
-export async function setupLogStore(
-  dbName = 'raiden',
-  additionalLoggers: string[] = ['matrix']
-): Promise<void> {
-  if (typeof db !== 'undefined') return;
+async function setupDb(dbName: string) {
+  if (db?.name === dbName) return;
   db = await openDB<RaidenDB>(dbName, 1, {
     upgrade(db) {
-      const logsStore = db.createObjectStore(collectionName);
+      const logsStore = db.createObjectStore(storeName);
       logsStore.createIndex('by-logger', 'logger');
       logsStore.createIndex('by-level', 'level');
     },
   });
+}
+
+const loggerRe = /^raiden:(0x[0-9a-f]{40})$/i;
+async function setDbForLogger(loggerName: string) {
+  if (!loggerRe.test(loggerName)) return;
+  await setupDb(loggerName);
+}
+
+/* istanbul ignore next */
+export async function setupLogStore(
+  additionalLoggers: string[] = ['matrix']
+): Promise<void> {
+  if (typeof db !== 'undefined') return;
+  await setupDb('raiden'); // init database, for startup logs
 
   for (const log of [logging, ...additionalLoggers.map(logging.getLogger)]) {
     const origFactory = log.methodFactory;
@@ -86,25 +104,28 @@ export async function setupLogStore(
       loggerName: string
     ): logging.LoggingMethod => {
       const rawMethod = origFactory(methodName, level, loggerName);
+      setDbForLogger(loggerName);
       return (...message: any[]): void => {
         rawMethod(...message);
         const filtered = filterMessage(message);
         if (!filtered) return;
         db.put(
-          collectionName,
+          storeName,
           {
             logger: loggerName,
             level: methodName,
             message: filtered,
+            ...(lastBlockNumber ? { block: lastBlockNumber } : {}),
           },
           Date.now()
         ).catch(() =>
           db.put(
-            collectionName,
+            storeName,
             {
               logger: loggerName,
               level: methodName,
               message: message.map(serialize),
+              ...(lastBlockNumber ? { block: lastBlockNumber } : {}),
             },
             Date.now()
           )
@@ -116,16 +137,26 @@ export async function setupLogStore(
 
 export async function getLogsFromStore(): Promise<[number, string]> {
   let content = '';
-  let cursor = await db.transaction(collectionName).store.openCursor();
+  let cursor = await db.transaction(storeName).store.openCursor();
   let lastTime = Date.now();
+  let first = true;
   while (cursor) {
-    const { logger, level, message } = cursor.value;
+    const { logger, level, message, block } = cursor.value;
     const line = message
       .map((m) => (typeof m === 'string' ? m : JSON.stringify(m)))
       .join(' ');
     lastTime = +cursor.key;
     const time = new Date(cursor.key).toISOString();
-    content += `${time} @ ${logger} [${level}] \t=> ${line}\n`;
+    const blockStr = block ? ` #${block}` : ''; // don't print block if there's none
+    // most (usually all) entries have loggerName == dbName == `raiden:<address>`
+    // in this case, we print the loggerName only once and omit later to save space
+    let loggerStr;
+    if (logger === db.name && !first) loggerStr = '';
+    else {
+      loggerStr = ` @ ${logger}`;
+      first = false;
+    }
+    content += `${time}${blockStr}${loggerStr} [${level}] \t=> ${line}\n`;
     cursor = await cursor.continue();
   }
   return [lastTime, content];
