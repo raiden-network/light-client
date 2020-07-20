221de9b1368c5ee3d58ce59f90eb4a3a9388b7a5
Continuous <no-reply@raiden.network>
2020-07-20 16:21:10 +0000

    Automated deployment to GitHub Pages: ff83ce1cc9319e8113e200d4f121380cec230629 [skip ci]

--- staging/precache-manifest.4b31f21eb30f7d139eef594bce019086.js
+++ staging/precache-manifest.4b31f21eb30f7d139eef594bce019086.js
@@ -12,7 +12,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/staging/config.json.example"
   },
   {
-    "revision": "f110e4baffd1a3ade6a5",
+    "revision": "d301d0dcaddb3f9e111a",
     "url": "/staging/css/app.eb0b879b.css"
   },
   {
@@ -316,12 +316,12 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/staging/img/withdrawal.c80c3337.svg"
   },
   {
-    "revision": "855bffab0f621b6ee35e2b366e8ad695",
+    "revision": "77dd8534cad2380bb9a7e842a951f320",
     "url": "/staging/index.html"
   },
   {
-    "revision": "f110e4baffd1a3ade6a5",
-    "url": "/staging/js/app.adc134e9.js"
+    "revision": "d301d0dcaddb3f9e111a",
+    "url": "/staging/js/app.ec7975b9.js"
   },
   {
     "revision": "bfee43e91bcf52a26a64",
--- staging/service-worker.js
+++ staging/service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/staging/precache-manifest.d87dcb1c159020de4f5d3d79d62be73d.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/staging/precache-manifest.4b31f21eb30f7d139eef594bce019086.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 self.addEventListener('message', (e) => {
   if (!e.data) {
