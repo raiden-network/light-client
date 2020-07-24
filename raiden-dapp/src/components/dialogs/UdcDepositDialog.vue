3d19eff47eb8481f1cdf5b570063cb6605c2a1aa
Talel Dayekh <43838780+taleldayekh@users.noreply.github.com>
2020-07-24 16:06:58 +0200

    dApp: Updated testnet UDC deposit dialog (#1956)
    * Updated testnet UDC deposit dialog
    
    * mapState no longer used

--- raiden-dapp/src/components/dialogs/UdcDepositDialog.vue
+++ raiden-dapp/src/components/dialogs/UdcDepositDialog.vue
@@ -1,25 +1,15 @@
 <template>
   <raiden-dialog :visible="visible" @close="cancel">
     <v-card-title v-if="!error">
-      {{
-        $t(
-          mainnet
-            ? 'udc-deposit-dialog.button-main'
-            : 'udc-deposit-dialog.button'
-        )
-      }}
+      {{ $t('udc-deposit-dialog.title') }}
     </v-card-title>
     <v-card-text>
       <v-row v-if="!loading && !error" justify="center" no-gutters>
-        <v-col cols="6">
-          <v-text-field
-            v-model="amount"
-            autofocus
-            type="text"
-            :suffix="serviceToken"
-            class="udc-deposit-dialog__amount"
-          />
-        </v-col>
+        <amount-input
+          v-model="amount"
+          :token="udcToken"
+          :placeholder="$t('transfer.amount-placeholder')"
+        />
       </v-row>
       <v-row v-else-if="error">
         <error-message :error="error" />
@@ -33,34 +23,26 @@
             <span v-if="step === 'mint'">
               {{
                 $t('udc-deposit-dialog.progress.mint', {
-                  currency: serviceToken,
+                  currency: udcToken.symbol,
                 })
               }}
             </span>
             <span v-else-if="step === 'approve'">
               {{
                 $t('udc-deposit-dialog.progress.approve', {
-                  currency: serviceToken,
+                  currency: udcToken.symbol,
                 })
               }}
             </span>
             <span v-else-if="step === 'deposit'">
               {{
                 $t('udc-deposit-dialog.progress.deposit', {
-                  currency: serviceToken,
+                  currency: udcToken.symbol,
                 })
               }}
             </span>
           </v-row>
         </v-col>
-      </v-row>
-      <v-row v-if="!loading && !error" class="udc-deposit-dialog__available">
-        {{
-          $t('udc-deposit-dialog.available', {
-            balance: accountBalance,
-            currency: $t('app-header.currency'),
-          })
-        }}
       </v-row>
     </v-card-text>
     <v-card-actions v-if="!error">
@@ -85,8 +67,8 @@
 
 <script lang="ts">
 import { Component, Vue, Emit, Prop } from 'vue-property-decorator';
-import { mapGetters, mapState } from 'vuex';
-
+import { mapGetters } from 'vuex';
+import AmountInput from '@/components/AmountInput.vue';
 import ActionButton from '@/components/ActionButton.vue';
 import { BalanceUtils } from '@/utils/balance-utils';
 import { Token } from '@/model/types';
@@ -96,9 +78,14 @@ import Spinner from '@/components/icons/Spinner.vue';
 import { RaidenError } from 'raiden-ts';
 
 @Component({
-  components: { ActionButton, RaidenDialog, ErrorMessage, Spinner },
+  components: {
+    ActionButton,
+    RaidenDialog,
+    ErrorMessage,
+    Spinner,
+    AmountInput,
+  },
   computed: {
-    ...mapState(['accountBalance']),
     ...mapGetters(['mainnet', 'udcToken']),
   },
 })
@@ -114,10 +101,6 @@ export default class UdcDepositDialog extends Vue {
 
   mainnet!: boolean;
   udcToken!: Token;
-
-  get serviceToken(): string {
-    return this.udcToken.symbol ?? (this.mainnet ? 'RDN' : 'SVT');
-  }
 
   get valid(): boolean {
     return /^[1-9]\d*$/.test(this.amount);
@@ -162,42 +145,6 @@ export default class UdcDepositDialog extends Vue {
   background-color: #141414;
   border-radius: 10px !important;
   padding: 20px 20px 55px 20px;
-
-  &__amount {
-    font-size: 24px;
-    font-weight: bold;
-    color: $color-white;
-
-    ::v-deep {
-      input {
-        text-align: right;
-      }
-
-      .v-input {
-        &__slot {
-          &::before {
-            border: none !important;
-          }
-
-          &::after {
-            border: none !important;
-          }
-        }
-      }
-
-      .v-text-field {
-        &__details {
-          display: none;
-        }
-
-        &__suffix {
-          padding-left: 8px;
-          color: white;
-          padding-right: 18px;
-        }
-      }
-    }
-  }
 
   &__available {
     text-align: center;
