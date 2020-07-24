d734c6b860be43a1d3614289ea49d1435f76b0e3
André Vitor de Lima Matos <andre.vmatos@gmail.com>
2020-07-24 16:55:11 -0300

    cli: pr review comments

--- raiden-cli/src/cli.ts
+++ raiden-cli/src/cli.ts
@@ -5,18 +5,18 @@ import { Cli } from './types';
 
 export function makeCli(
   raiden: Raiden,
-  endpoint?: number | string,
+  endpoint?: readonly [string, number],
   log?: Logger,
   corsOrigin?: string,
 ): Cli {
   log = log ?? getLogger(`cli:${raiden.address}`);
   const cli: Cli = { log, raiden };
   if (endpoint) {
+    const [, port] = endpoint;
+    let [host] = endpoint;
+    host = host || '127.0.0.1';
     cli.app = makeApp.call(cli, corsOrigin);
-    let host: string, port: number | string;
-    if (typeof endpoint === 'number') [host, port] = ['127.0.0.1', endpoint];
-    else [host, port] = endpoint.split(':');
-    cli.server = cli.app.listen(+port, host, () =>
+    cli.server = cli.app.listen(port, host, () =>
       log!.info(`Server started at: http://${host}:${port}`),
     );
     cli.server.setTimeout(3.6e6); // 1h
