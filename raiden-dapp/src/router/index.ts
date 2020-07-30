9e2e7ddf07a936861f1525f235a977ed2841cac4
Thore Weilbier <thore@weilbier.net>
2020-07-30 13:18:30 +0200

    dapp: add unit test suite for router
    Because the router does not include bare configuration anymore, but has
    been extended with some logic for the navigation guards, it was
    necessary to add a test suite for it.
    Therefore the router module needed a little tweak to expose its internal
    part to make them testable.

--- raiden-dapp/src/router/index.ts
+++ raiden-dapp/src/router/index.ts
@@ -1,175 +1,173 @@
 import Vue from 'vue';
-import Router from 'vue-router';
+import Router, { Route, RouteConfig, NavigationGuardNext } from 'vue-router';
 
 import DisclaimerRoute from '../views/DisclaimerRoute.vue';
 import Home from '../views/Home.vue';
 import { RouteNames } from '@/router/route-names';
 import store from '../store';
 
-Vue.use(Router);
-
 /* istanbul ignore next */
-const router = new Router({
-  mode: 'hash',
-  base: process.env.BASE_URL,
-  routes: [
-    {
-      path: '/',
-      name: RouteNames.DISCLAIMER,
-      meta: {
-        title: 'Disclaimer',
-      },
-      component: DisclaimerRoute,
+export const routes: RouteConfig[] = [
+  {
+    path: '/',
+    name: RouteNames.DISCLAIMER,
+    meta: {
+      title: 'Disclaimer',
     },
-    {
-      path: '*',
-      redirect: '/',
+    component: DisclaimerRoute,
+  },
+  {
+    path: '*',
+    redirect: '/',
+  },
+  {
+    path: '/home',
+    name: RouteNames.HOME,
+    meta: {
+      title: 'Raiden dApp',
     },
-    {
-      path: '/home',
-      name: RouteNames.HOME,
-      meta: {
-        title: 'Raiden dApp',
-      },
-      component: Home,
+    component: Home,
+  },
+  {
+    path: '/transfer/:token',
+    name: RouteNames.TRANSFER,
+    meta: {
+      title: 'Transfer',
     },
-    {
-      path: '/transfer/:token',
-      name: RouteNames.TRANSFER,
-      meta: {
-        title: 'Transfer',
-      },
-      component: () => import('../views/TransferRoute.vue'),
+    component: () => import('../views/TransferRoute.vue'),
+  },
+  {
+    path: '/transfer/:token/:target',
+    name: RouteNames.TRANSFER_STEPS,
+    meta: {
+      title: 'Transfer',
     },
-    {
-      path: '/transfer/:token/:target',
-      name: RouteNames.TRANSFER_STEPS,
-      meta: {
-        title: 'Transfer',
-      },
-      component: () => import('../views/TransferStepsRoute.vue'),
+    component: () => import('../views/TransferStepsRoute.vue'),
+  },
+  {
+    path: '/connect',
+    name: RouteNames.SELECT_TOKEN,
+    meta: {
+      title: 'Select Token',
     },
-    {
-      path: '/connect',
-      name: RouteNames.SELECT_TOKEN,
-      meta: {
-        title: 'Select Token',
-      },
-      component: () => import('../views/SelectTokenRoute.vue'),
+    component: () => import('../views/SelectTokenRoute.vue'),
+  },
+  {
+    path: '/connect/:token',
+    name: RouteNames.SELECT_HUB,
+    meta: {
+      title: 'Select Hub',
     },
-    {
-      path: '/connect/:token',
-      name: RouteNames.SELECT_HUB,
-      meta: {
-        title: 'Select Hub',
-      },
-      component: () => import('../views/SelectHubRoute.vue'),
+    component: () => import('../views/SelectHubRoute.vue'),
+  },
+  {
+    path: '/connect/:token/:partner',
+    name: RouteNames.OPEN_CHANNEL,
+    meta: {
+      title: 'Open Channel',
     },
-    {
-      path: '/connect/:token/:partner',
-      name: RouteNames.OPEN_CHANNEL,
-      meta: {
-        title: 'Open Channel',
-      },
-      component: () => import('../views/OpenChannelRoute.vue'),
+    component: () => import('../views/OpenChannelRoute.vue'),
+  },
+  {
+    path: '/channels/:token',
+    name: RouteNames.CHANNELS,
+    meta: {
+      title: 'Channels',
     },
-    {
-      path: '/channels/:token',
-      name: RouteNames.CHANNELS,
-      meta: {
-        title: 'Channels',
-      },
-      component: () => import('../views/ChannelsRoute.vue'),
+    component: () => import('../views/ChannelsRoute.vue'),
+  },
+  {
+    path: '/notifications',
+    name: RouteNames.NOTIFICATIONS,
+    beforeEnter: (to, from, next) => {
+      if (from.name === null) {
+        next({
+          name: RouteNames.HOME,
+        });
+      } else if (to.matched.length) {
+        to.matched[0].components.default = from.matched[0].components.default;
+        to.matched[0].components.notifications = () =>
+          import('../views/NotificationPanel.vue');
+      }
+      next();
     },
-    {
-      path: '/notifications',
-      name: RouteNames.NOTIFICATIONS,
-      beforeEnter: (to, from, next) => {
-        if (from.name === null) {
-          next({
-            name: RouteNames.HOME,
-          });
-        } else if (to.matched.length) {
-          to.matched[0].components.default = from.matched[0].components.default;
-          to.matched[0].components.notifications = () =>
-            import('../views/NotificationPanel.vue');
-        }
-        next();
-      },
+  },
+  {
+    path: '/account',
+    name: RouteNames.ACCOUNT,
+    beforeEnter: (to, from, next) => {
+      // Remembers the route that was visited just before the General view is opened and
+      // then loads the General view in a separate <router-view>. The last visited route
+      // is loaded when clicking out of the General view.
+      if (from.name === null) {
+        next({
+          name: RouteNames.HOME,
+        });
+      } else if (to.matched.length) {
+        to.matched[0].components.default = from.matched[0].components.default;
+        to.matched[0].components.modal = () =>
+          import('../views/AccountRoute.vue');
+      }
+      next();
     },
-    {
-      path: '/account',
-      name: RouteNames.ACCOUNT,
-      beforeEnter: (to, from, next) => {
-        // Remembers the route that was visited just before the General view is opened and
-        // then loads the General view in a separate <router-view>. The last visited route
-        // is loaded when clicking out of the General view.
-        if (from.name === null) {
-          next({
-            name: RouteNames.HOME,
-          });
-        } else if (to.matched.length) {
-          to.matched[0].components.default = from.matched[0].components.default;
-          to.matched[0].components.modal = () =>
-            import('../views/AccountRoute.vue');
-        }
-        next();
-      },
-      children: [
-        {
-          path: '/',
-          name: RouteNames.ACCOUNT_ROOT,
-          meta: {
-            title: 'Account',
-          },
-          component: () => import('../views/account/AccountRoot.vue'),
+    children: [
+      {
+        path: '/',
+        name: RouteNames.ACCOUNT_ROOT,
+        meta: {
+          title: 'Account',
         },
-        {
-          path: 'backup',
-          name: RouteNames.ACCOUNT_BACKUP,
-          meta: {
-            title: 'Backup State',
-          },
-          component: () => import('../views/account/BackupState.vue'),
+        component: () => import('../views/account/AccountRoot.vue'),
+      },
+      {
+        path: 'backup',
+        name: RouteNames.ACCOUNT_BACKUP,
+        meta: {
+          title: 'Backup State',
         },
-        {
-          path: 'raiden',
-          name: RouteNames.ACCOUNT_RAIDEN,
-          meta: {
-            title: 'Raiden Account',
-          },
-          component: () => import('../views/account/RaidenAccount.vue'),
+        component: () => import('../views/account/BackupState.vue'),
+      },
+      {
+        path: 'raiden',
+        name: RouteNames.ACCOUNT_RAIDEN,
+        meta: {
+          title: 'Raiden Account',
         },
-        {
-          path: 'settings',
-          name: RouteNames.ACCOUNT_SETTINGS,
-          meta: {
-            title: 'Settings',
-          },
-          component: () => import('../views/account/Settings.vue'),
+        component: () => import('../views/account/RaidenAccount.vue'),
+      },
+      {
+        path: 'settings',
+        name: RouteNames.ACCOUNT_SETTINGS,
+        meta: {
+          title: 'Settings',
         },
-        {
-          path: 'withdrawal',
-          name: RouteNames.ACCOUNT_WITHDRAWAL,
-          meta: {
-            title: 'Withdrawal',
-          },
-          component: () => import('../views/account/WithdrawalRoute.vue'),
+        component: () => import('../views/account/Settings.vue'),
+      },
+      {
+        path: 'withdrawal',
+        name: RouteNames.ACCOUNT_WITHDRAWAL,
+        meta: {
+          title: 'Withdrawal',
         },
-        {
-          path: 'udc',
-          name: RouteNames.ACCOUNT_UDC,
-          meta: {
-            title: 'UDC',
-          },
-          component: () => import('../views/account/UDC.vue'),
+        component: () => import('../views/account/WithdrawalRoute.vue'),
+      },
+      {
+        path: 'udc',
+        name: RouteNames.ACCOUNT_UDC,
+        meta: {
+          title: 'UDC',
         },
-      ],
-    },
-  ],
-});
+        component: () => import('../views/account/UDC.vue'),
+      },
+    ],
+  },
+];
 
-router.beforeEach((to, _from, next) => {
+export function globalNavigationGuard(
+  to: Route,
+  _from: Route,
+  next: NavigationGuardNext
+) {
   const routeToDisclaimer = to.name === RouteNames.DISCLAIMER;
 
   if (store.state.disclaimerAccepted) {
@@ -186,6 +184,16 @@ router.beforeEach((to, _from, next) => {
   } else {
     next(); // Avoid infinite loop of disclaimer redirects
   }
+}
+
+Vue.use(Router);
+
+const router = new Router({
+  mode: process.env.NODE_ENV === 'production' ? 'history' : 'hash',
+  base: process.env.BASE_URL,
+  routes,
 });
+
+router.beforeEach(globalNavigationGuard);
 
 export default router;
