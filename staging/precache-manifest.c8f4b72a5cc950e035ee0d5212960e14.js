4ae9d335125cea2f60a3c96d25798ccab6b19773
Continuous <no-reply@raiden.network>
2020-08-05 15:43:19 +0000

    Automated deployment to GitHub Pages: 0f87aee4c1fd5f36bdc52944f055f6272445738d [skip ci]

---
+++ staging/precache-manifest.c8f4b72a5cc950e035ee0d5212960e14.js
@@ -0,0 +1,410 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c12af454a2cc54a9007e9d7f5b5dffa1",
+    "url": "/staging/404.html"
+  },
+  {
+    "revision": "dc6a01b2d18fa87f6c1b71e81e9d9553",
+    "url": "/staging/config.json"
+  },
+  {
+    "revision": "d313a8dc0e5625caf4424b8b902121f6",
+    "url": "/staging/config.json.example"
+  },
+  {
+    "revision": "c8771c28bf4a0678ef96",
+    "url": "/staging/css/app.a7684e42.css"
+  },
+  {
+    "revision": "c6b2013cc6202357db43",
+    "url": "/staging/css/chunk-11468e22.27c85d4e.css"
+  },
+  {
+    "revision": "6a34f05bd74f30d637f5",
+    "url": "/staging/css/chunk-1ad720d1.a9f4933e.css"
+  },
+  {
+    "revision": "7085e4b050d58795ceaf",
+    "url": "/staging/css/chunk-2caf36c7.284343a3.css"
+  },
+  {
+    "revision": "d001430141d5c117dd91",
+    "url": "/staging/css/chunk-2dce5709.688988c3.css"
+  },
+  {
+    "revision": "e42360aa202a57ae6371",
+    "url": "/staging/css/chunk-3a6e243c.26e6eef1.css"
+  },
+  {
+    "revision": "70864b31b987c8be49b6",
+    "url": "/staging/css/chunk-42bc4fc2.4c2a0c88.css"
+  },
+  {
+    "revision": "d6b8c8c6466e8c8c29d6",
+    "url": "/staging/css/chunk-48646ba8.09416e52.css"
+  },
+  {
+    "revision": "42bb834f1440965022cd",
+    "url": "/staging/css/chunk-4d883632.5b2edc41.css"
+  },
+  {
+    "revision": "2cce4b9579788b4389b8",
+    "url": "/staging/css/chunk-5173cd44.c2e1e777.css"
+  },
+  {
+    "revision": "098c78374c08557135eb",
+    "url": "/staging/css/chunk-52abfed0.9ddd6f20.css"
+  },
+  {
+    "revision": "75b540bf25665c357ae0",
+    "url": "/staging/css/chunk-5d3b2a50.afe9b41e.css"
+  },
+  {
+    "revision": "0d45d63f73cf717ce943",
+    "url": "/staging/css/chunk-618b5a2b.4641a76f.css"
+  },
+  {
+    "revision": "ecbcaf7d9cddf7c3720c",
+    "url": "/staging/css/chunk-662b1de2.1747eb60.css"
+  },
+  {
+    "revision": "42d62fda0e7a877ee731",
+    "url": "/staging/css/chunk-67561f66.42e3e78c.css"
+  },
+  {
+    "revision": "75ed850e1670b3d3695c",
+    "url": "/staging/css/chunk-6d6cffbd.fda03424.css"
+  },
+  {
+    "revision": "c96dda7832ad21b38459",
+    "url": "/staging/css/chunk-73bb0efe.3eb41cc2.css"
+  },
+  {
+    "revision": "87fcb4d07ca520b88ef2",
+    "url": "/staging/css/chunk-a76b7520.f0caf6ea.css"
+  },
+  {
+    "revision": "12610059e15476b4c671",
+    "url": "/staging/css/chunk-b0964c66.423bfba4.css"
+  },
+  {
+    "revision": "b86999b37806620ed0ef",
+    "url": "/staging/css/chunk-vendors.2503676e.css"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "/staging/fonts/MaterialIcons-Regular.0509ab09.woff2"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "/staging/fonts/MaterialIcons-Regular.29b882f0.woff"
+  },
+  {
+    "revision": "96c476804d7a788cc1c05351b287ee41",
+    "url": "/staging/fonts/MaterialIcons-Regular.96c47680.eot"
+  },
+  {
+    "revision": "da4ea5cdfca6b3baab285741f5ccb59f",
+    "url": "/staging/fonts/MaterialIcons-Regular.da4ea5cd.ttf"
+  },
+  {
+    "revision": "66dc854e8e6da281f8beee081d633d70",
+    "url": "/staging/fonts/materialdesignicons-webfont.66dc854e.eot"
+  },
+  {
+    "revision": "8ac434c848d34646f73544f35a903a67",
+    "url": "/staging/fonts/materialdesignicons-webfont.8ac434c8.woff"
+  },
+  {
+    "revision": "8ed8f0719def123d77bf6ea8919b8d8a",
+    "url": "/staging/fonts/materialdesignicons-webfont.8ed8f071.ttf"
+  },
+  {
+    "revision": "c0f7978a13704c522aaf355a80a5d0b0",
+    "url": "/staging/fonts/materialdesignicons-webfont.c0f7978a.woff2"
+  },
+  {
+    "revision": "5cb7edfceb233100075dc9a1e12e8da3",
+    "url": "/staging/fonts/roboto-latin-100.5cb7edfc.woff"
+  },
+  {
+    "revision": "7370c3679472e9560965ff48a4399d0b",
+    "url": "/staging/fonts/roboto-latin-100.7370c367.woff2"
+  },
+  {
+    "revision": "f8b1df51ba843179fa1cc9b53d58127a",
+    "url": "/staging/fonts/roboto-latin-100italic.f8b1df51.woff2"
+  },
+  {
+    "revision": "f9e8e590b4e0f1ff83469bb2a55b8488",
+    "url": "/staging/fonts/roboto-latin-100italic.f9e8e590.woff"
+  },
+  {
+    "revision": "b00849e00f4c2331cddd8ffb44a6720b",
+    "url": "/staging/fonts/roboto-latin-300.b00849e0.woff"
+  },
+  {
+    "revision": "ef7c6637c68f269a882e73bcb57a7f6a",
+    "url": "/staging/fonts/roboto-latin-300.ef7c6637.woff2"
+  },
+  {
+    "revision": "14286f3ba79c6627433572dfa925202e",
+    "url": "/staging/fonts/roboto-latin-300italic.14286f3b.woff2"
+  },
+  {
+    "revision": "4df32891a5f2f98a363314f595482e08",
+    "url": "/staging/fonts/roboto-latin-300italic.4df32891.woff"
+  },
+  {
+    "revision": "479970ffb74f2117317f9d24d9e317fe",
+    "url": "/staging/fonts/roboto-latin-400.479970ff.woff2"
+  },
+  {
+    "revision": "60fa3c0614b8fb2f394fa29944c21540",
+    "url": "/staging/fonts/roboto-latin-400.60fa3c06.woff"
+  },
+  {
+    "revision": "51521a2a8da71e50d871ac6fd2187e87",
+    "url": "/staging/fonts/roboto-latin-400italic.51521a2a.woff2"
+  },
+  {
+    "revision": "fe65b8335ee19dd944289f9ed3178c78",
+    "url": "/staging/fonts/roboto-latin-400italic.fe65b833.woff"
+  },
+  {
+    "revision": "020c97dc8e0463259c2f9df929bb0c69",
+    "url": "/staging/fonts/roboto-latin-500.020c97dc.woff2"
+  },
+  {
+    "revision": "87284894879f5b1c229cb49c8ff6decc",
+    "url": "/staging/fonts/roboto-latin-500.87284894.woff"
+  },
+  {
+    "revision": "288ad9c6e8b43cf02443a1f499bdf67e",
+    "url": "/staging/fonts/roboto-latin-500italic.288ad9c6.woff"
+  },
+  {
+    "revision": "db4a2a231f52e497c0191e8966b0ee58",
+    "url": "/staging/fonts/roboto-latin-500italic.db4a2a23.woff2"
+  },
+  {
+    "revision": "2735a3a69b509faf3577afd25bdf552e",
+    "url": "/staging/fonts/roboto-latin-700.2735a3a6.woff2"
+  },
+  {
+    "revision": "adcde98f1d584de52060ad7b16373da3",
+    "url": "/staging/fonts/roboto-latin-700.adcde98f.woff"
+  },
+  {
+    "revision": "81f57861ed4ac74741f5671e1dff2fd9",
+    "url": "/staging/fonts/roboto-latin-700italic.81f57861.woff"
+  },
+  {
+    "revision": "da0e717829e033a69dec97f1e155ae42",
+    "url": "/staging/fonts/roboto-latin-700italic.da0e7178.woff2"
+  },
+  {
+    "revision": "9b3766ef4a402ad3fdeef7501a456512",
+    "url": "/staging/fonts/roboto-latin-900.9b3766ef.woff2"
+  },
+  {
+    "revision": "bb1e4dc6333675d11ada2e857e7f95d7",
+    "url": "/staging/fonts/roboto-latin-900.bb1e4dc6.woff"
+  },
+  {
+    "revision": "28f9151055c950874d2c6803a39b425b",
+    "url": "/staging/fonts/roboto-latin-900italic.28f91510.woff"
+  },
+  {
+    "revision": "ebf6d1640ccddb99fb49f73c052c55a8",
+    "url": "/staging/fonts/roboto-latin-900italic.ebf6d164.woff2"
+  },
+  {
+    "revision": "268247fb18f0a1f0c4b01bf77750afb5",
+    "url": "/staging/img/back_arrow.268247fb.svg"
+  },
+  {
+    "revision": "4e8b8f1b476fdd5f3f52326d6540ef57",
+    "url": "/staging/img/bug.4e8b8f1b.svg"
+  },
+  {
+    "revision": "d1f000b77627513d8999bbce2ca89078",
+    "url": "/staging/img/deposit.d1f000b7.svg"
+  },
+  {
+    "revision": "51f3a9701270d46b903f820a4d16ffde",
+    "url": "/staging/img/done.51f3a970.svg"
+  },
+  {
+    "revision": "d7fcc637123a0365361fb4e0c3c8684c",
+    "url": "/staging/img/down_arrow.d7fcc637.svg"
+  },
+  {
+    "revision": "f52d506970badd4fc705078c4655459d",
+    "url": "/staging/img/error.f52d5069.svg"
+  },
+  {
+    "revision": "640d53f2a597d56cfd02aae4ade2ea91",
+    "url": "/staging/img/eth.640d53f2.svg"
+  },
+  {
+    "revision": "cdc92be662122b87a155e508137bf71d",
+    "url": "/staging/img/eth_transfer_arrow.cdc92be6.svg"
+  },
+  {
+    "revision": "263a7ddf6c592b10d57c22f7f0d3a43e",
+    "url": "/staging/img/gear.263a7ddf.svg"
+  },
+  {
+    "revision": "55c56a33855dfaa41bdd5db277c2b321",
+    "url": "/staging/img/generic.55c56a33.svg"
+  },
+  {
+    "revision": "c9b975f769a4af42c0bd10c56015f090",
+    "url": "/staging/img/info.c9b975f7.svg"
+  },
+  {
+    "revision": "4e8b2468b175efdc06e6e6e88ca22e39",
+    "url": "/staging/img/input_invalid.4e8b2468.svg"
+  },
+  {
+    "revision": "762cb0ae3a76693f6d02685fdbf3b6c7",
+    "url": "/staging/img/input_valid.762cb0ae.svg"
+  },
+  {
+    "revision": "5c4b022de72e407cd650f0c4bcde5973",
+    "url": "/staging/img/logo.5c4b022d.svg"
+  },
+  {
+    "revision": "c879a582a94e772b8ed27295ae83145d",
+    "url": "/staging/img/metamask.c879a582.svg"
+  },
+  {
+    "revision": "9b9982fe0de7c1b434448156144bb982",
+    "url": "/staging/img/notifications.9b9982fe.svg"
+  },
+  {
+    "revision": "ef6596ccea36d221ca4404d115252a06",
+    "url": "/staging/img/received_transfer.ef6596cc.svg"
+  },
+  {
+    "revision": "087bd40407e3e2d10d2c27c109395989",
+    "url": "/staging/img/sent_transfer.087bd404.svg"
+  },
+  {
+    "revision": "ff4426c1d8501bdcb341df92f438c828",
+    "url": "/staging/img/state.ff4426c1.svg"
+  },
+  {
+    "revision": "fc1f0473592702c61bcb0137f1002bd3",
+    "url": "/staging/img/state_download.fc1f0473.png"
+  },
+  {
+    "revision": "dea399eb4ace55f4a13007018aaaf15e",
+    "url": "/staging/img/state_upload.dea399eb.png"
+  },
+  {
+    "revision": "a4ddd71af6022bf4c6fff8e0e95507d8",
+    "url": "/staging/img/udc.a4ddd71a.svg"
+  },
+  {
+    "revision": "49bc56a9fd562747445fe4222c8e21be",
+    "url": "/staging/img/warning.49bc56a9.svg"
+  },
+  {
+    "revision": "c80c3337a3e02164468f3b4a364b8bab",
+    "url": "/staging/img/withdrawal.c80c3337.svg"
+  },
+  {
+    "revision": "bf10aee9c7d37bdd61387ef544b3b13b",
+    "url": "/staging/index.html"
+  },
+  {
+    "revision": "c8771c28bf4a0678ef96",
+    "url": "/staging/js/app.6752239e.js"
+  },
+  {
+    "revision": "c6b2013cc6202357db43",
+    "url": "/staging/js/chunk-11468e22.a39bc2f1.js"
+  },
+  {
+    "revision": "6a34f05bd74f30d637f5",
+    "url": "/staging/js/chunk-1ad720d1.f293c618.js"
+  },
+  {
+    "revision": "7085e4b050d58795ceaf",
+    "url": "/staging/js/chunk-2caf36c7.ade791e1.js"
+  },
+  {
+    "revision": "d001430141d5c117dd91",
+    "url": "/staging/js/chunk-2dce5709.f53d1d1b.js"
+  },
+  {
+    "revision": "e42360aa202a57ae6371",
+    "url": "/staging/js/chunk-3a6e243c.066ffa41.js"
+  },
+  {
+    "revision": "70864b31b987c8be49b6",
+    "url": "/staging/js/chunk-42bc4fc2.f80eb4f8.js"
+  },
+  {
+    "revision": "d6b8c8c6466e8c8c29d6",
+    "url": "/staging/js/chunk-48646ba8.e2629804.js"
+  },
+  {
+    "revision": "42bb834f1440965022cd",
+    "url": "/staging/js/chunk-4d883632.4b16b090.js"
+  },
+  {
+    "revision": "2cce4b9579788b4389b8",
+    "url": "/staging/js/chunk-5173cd44.f8d90101.js"
+  },
+  {
+    "revision": "098c78374c08557135eb",
+    "url": "/staging/js/chunk-52abfed0.591a4cbe.js"
+  },
+  {
+    "revision": "75b540bf25665c357ae0",
+    "url": "/staging/js/chunk-5d3b2a50.9b864f5b.js"
+  },
+  {
+    "revision": "0d45d63f73cf717ce943",
+    "url": "/staging/js/chunk-618b5a2b.ed40a94c.js"
+  },
+  {
+    "revision": "ecbcaf7d9cddf7c3720c",
+    "url": "/staging/js/chunk-662b1de2.6b7130fd.js"
+  },
+  {
+    "revision": "42d62fda0e7a877ee731",
+    "url": "/staging/js/chunk-67561f66.f1efc8eb.js"
+  },
+  {
+    "revision": "75ed850e1670b3d3695c",
+    "url": "/staging/js/chunk-6d6cffbd.1b4d18b6.js"
+  },
+  {
+    "revision": "c96dda7832ad21b38459",
+    "url": "/staging/js/chunk-73bb0efe.1d7f19aa.js"
+  },
+  {
+    "revision": "87fcb4d07ca520b88ef2",
+    "url": "/staging/js/chunk-a76b7520.b72b2bb5.js"
+  },
+  {
+    "revision": "12610059e15476b4c671",
+    "url": "/staging/js/chunk-b0964c66.ac19fed2.js"
+  },
+  {
+    "revision": "b86999b37806620ed0ef",
+    "url": "/staging/js/chunk-vendors.0972185c.js"
+  },
+  {
+    "revision": "364061b3f4d46ef296d8e6e026c3a6a6",
+    "url": "/staging/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/staging/robots.txt"
+  }
+]);
---
+++ staging/service-worker.js
@@ -0,0 +1,16 @@
+importScripts("/staging/precache-manifest.c8f4b72a5cc950e035ee0d5212960e14.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+self.addEventListener('message', (e) => {
+  if (!e.data) {
+    return;
+  }
+
+  switch (e.data) {
+    case 'skipWaiting':
+      self.skipWaiting();
+      break;
+    default:
+      break;
+  }
+});
+
