855c52214ccfa2ff0e740ea54816b72453a9780f
Thore Weilbier <thore@weilbier.net>
2020-08-05 11:39:48 +0200

    dapp: add unit test for transaction list component
    There were no unit tests for this list so far. Since the functionality
    has been recently extended, it was necessary to finally add those tests.
    They include cases for the old/already existing functionality, as well
    as the new filter feature.
    
    Note that there is something awkward on components using the lazy
    listing feature of Vuetify. For some reason does the 'mount' wrapper
    function not render the list entries. But the 'shallowMount' wrapper
    does. This is unexpected behavior and got therefore also documented into
    the code.

---
+++ raiden-dapp/tests/unit/components/transaction-history/transaction-list.spec.ts
@@ -0,0 +1,78 @@
+import { shallowMount, Wrapper } from '@vue/test-utils';
+import Vue from 'vue';
+import Vuetify from 'vuetify';
+import { RaidenTransfer } from 'raiden-ts';
+import Transaction from '@/components/transaction-history/Transaction.vue';
+import TransactionList from '@/components/transaction-history/TransactionList.vue';
+import { Token } from '@/model/types';
+import { Transfers } from '@/types';
+import { generateToken, generateTransfer } from '../../utils/data-generator';
+
+Vue.use(Vuetify);
+
+describe('TransactionList.vue', () => {
+  const vuetify = new Vuetify();
+  const token = generateToken();
+  const transfers = [generateTransfer({}, token), generateTransfer({}, token)];
+
+  const createWrapper = (
+    tokenProp?: Token,
+    transferList: RaidenTransfer[] = transfers
+  ): Wrapper<TransactionList> => {
+    const transfersState: Transfers = {};
+    transferList.forEach(
+      (transfer) => (transfersState[transfer.key] = transfer)
+    );
+
+    /*
+     * For some reason does the plain 'mount' not work with with lazy list of
+     * Vuetify. Also it looks wrong, the 'shallowMount' fixes this issue and
+     * actually displays the transaction components in the list.
+     */
+    return shallowMount(TransactionList, {
+      vuetify,
+      mocks: {
+        $t: (msg: string) => msg,
+        $store: { state: { transfers: transfersState } },
+      },
+      propsData: {
+        token: tokenProp,
+      },
+    });
+  };
+
+  test('transaction list without filter includes all transfers', () => {
+    const wrapper = createWrapper();
+    const transactionEntries = wrapper.findAllComponents(Transaction);
+
+    expect(transactionEntries.length).toEqual(2);
+  });
+
+  test('transaction list applies token filter', () => {
+    const newToken = generateToken();
+    const transfer = generateTransfer({}, newToken);
+    const wrapper = createWrapper(newToken, [transfer]);
+    const transactionEntries = wrapper.findAllComponents(Transaction);
+
+    expect(transactionEntries.length).toEqual(1);
+  });
+
+  test('transaction list is empty for token filter without transactions', () => {
+    const tokenWithoutTransfers = generateToken();
+    const wrapper = createWrapper(tokenWithoutTransfers);
+    const transactionEntries = wrapper.findAllComponents(Transaction);
+
+    expect(transactionEntries.length).toEqual(0);
+  });
+
+  test('transaction list is stored by date', () => {
+    const wrapper = createWrapper();
+    const transactionEntries = wrapper.findAllComponents(Transaction);
+    const orderAsIs = transactionEntries.wrappers.map(
+      (entry) => entry.props().transfer.changedAt
+    );
+    const correctOrder = orderAsIs.sort();
+
+    expect(orderAsIs).toEqual(correctOrder);
+  });
+});
