690bbd3c37babc247212bb6eff8276f628fd4018
Sohan <sohan.agate@gmail.com>
2020-08-12 19:09:29 +0530

    removing old tests from monitor.spec.ts

--- raiden-ts/tests/unit/epics/monitor.spec.ts
+++ raiden-ts/tests/unit/epics/monitor.spec.ts
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import {
-  raidenEpicDeps,
   makeLog,
   makeAddress,
   makeHash,
@@ -12,7 +11,6 @@ import {
   makeRaiden,
 } from '../mocks';
 import {
-  epicFixtures,
   tokenNetwork,
   token,
   deposit,
@@ -25,32 +23,19 @@ import {
   id,
 } from '../fixtures';
 
-import { bigNumberify, BigNumberish, defaultAbiCoder } from 'ethers/utils';
+import { bigNumberify, defaultAbiCoder } from 'ethers/utils';
 import { Zero, WeiPerEther, Two, MaxUint256 } from 'ethers/constants';
-import { of } from 'rxjs';
 import { first, pluck } from 'rxjs/operators';
 
 import { Capabilities } from 'raiden-ts/constants';
 import { raidenConfigUpdate, raidenShutdown } from 'raiden-ts/actions';
-import { MessageType, LockedTransfer } from 'raiden-ts/messages/types';
-import { signMessage, createBalanceHash } from 'raiden-ts/messages/utils';
-import { tokenMonitored, channelOpen, channelDeposit } from 'raiden-ts/channels/actions';
-import { messageReceived, messageGlobalSend } from 'raiden-ts/messages/actions';
-import { transferGenerateAndSignEnvelopeMessageEpic } from 'raiden-ts/transfers/epics';
-import { UInt, decode, Hash, last } from 'raiden-ts/utils/types';
-import {
-  makeMessageId,
-  makeSecret,
-  getSecrethash,
-  makePaymentId,
-  getLocksroot,
-} from 'raiden-ts/transfers/utils';
-import { monitorRequestEpic } from 'raiden-ts/services/epics';
+import { MessageType } from 'raiden-ts/messages/types';
+import { createBalanceHash } from 'raiden-ts/messages/utils';
+import { messageGlobalSend } from 'raiden-ts/messages/actions';
+import { UInt, Hash } from 'raiden-ts/utils/types';
+
 import { udcDeposit, msBalanceProofSent } from 'raiden-ts/services/actions';
-import { transferProcessed, transferSecretRegister } from 'raiden-ts/transfers/actions';
-import { channelKey } from 'raiden-ts/channels/utils';
-import { Direction } from 'raiden-ts/transfers/state';
-import { RaidenError, ErrorCodes } from 'raiden-ts/utils/error';
+import { ErrorCodes } from 'raiden-ts/utils/error';
 
 test('monitorUdcBalanceEpic', async () => {
   expect.assertions(5);
@@ -79,297 +64,6 @@ test('monitorUdcBalanceEpic', async () => {
 });
 
 describe('monitorRequestEpic', () => {
-  let depsMock: ReturnType<typeof raidenEpicDeps>;
-  let token: ReturnType<typeof epicFixtures>['token'],
-    tokenNetwork: ReturnType<typeof epicFixtures>['tokenNetwork'],
-    channelId: ReturnType<typeof epicFixtures>['channelId'],
-    partner: ReturnType<typeof epicFixtures>['partner'],
-    settleTimeout: ReturnType<typeof epicFixtures>['settleTimeout'],
-    isFirstParticipant: ReturnType<typeof epicFixtures>['isFirstParticipant'],
-    txHash: ReturnType<typeof epicFixtures>['txHash'],
-    partnerSigner: ReturnType<typeof epicFixtures>['partnerSigner'],
-    key: ReturnType<typeof epicFixtures>['key'],
-    action$: ReturnType<typeof epicFixtures>['action$'],
-    state$: ReturnType<typeof epicFixtures>['state$'];
-
-  const monitoringReward = bigNumberify(5) as UInt<32>;
-
-  beforeEach(async () => {
-    depsMock = raidenEpicDeps();
-    ({
-      token,
-      tokenNetwork,
-      channelId,
-      partner,
-      settleTimeout,
-      isFirstParticipant,
-      txHash,
-      partnerSigner,
-      key,
-      action$,
-      state$,
-    } = epicFixtures(depsMock));
-
-    [
-      raidenConfigUpdate({
-        httpTimeout: 30,
-        monitoringReward,
-        caps: {
-          [Capabilities.NO_DELIVERY]: true,
-          [Capabilities.NO_MEDIATE]: true,
-          // 'noReceive' should be auto-disabled by 'getCaps$'
-        },
-        // rate=WeiPerEther == 1:1 to SVT
-        rateToSvt: { [token]: WeiPerEther as UInt<32> },
-      }),
-      tokenMonitored({ token, tokenNetwork }),
-      channelOpen.success(
-        {
-          id: channelId,
-          settleTimeout,
-          isFirstParticipant,
-          token,
-          txHash,
-          txBlock: 121,
-          confirmed: true,
-        },
-        { tokenNetwork, partner },
-      ),
-      channelDeposit.success(
-        {
-          id: channelId,
-          participant: depsMock.address,
-          totalDeposit: bigNumberify(500) as UInt<32>,
-          txHash,
-          txBlock: 122,
-          confirmed: true,
-        },
-        { tokenNetwork, partner },
-      ),
-      channelDeposit.success(
-        {
-          id: channelId,
-          participant: partner,
-          totalDeposit: bigNumberify(500) as UInt<32>,
-          txHash,
-          txBlock: 122,
-          confirmed: true,
-        },
-        { tokenNetwork, partner },
-      ),
-    ].forEach((a) => action$.next(a));
-  });
-
-  async function receiveTransfer(value: BigNumberish, unlock = true) {
-    const amount = decode(UInt(32), value);
-    const secret = makeSecret();
-    const secrethash = getSecrethash(secret);
-
-    const { state, config } = await depsMock.latest$.pipe(first()).toPromise();
-
-    const expiration = bigNumberify(state.blockNumber + config.revealTimeout * 2) as UInt<32>;
-    const lock = {
-      secrethash,
-      amount,
-      expiration,
-    };
-    const unsigned: LockedTransfer = {
-      type: MessageType.LOCKED_TRANSFER,
-      payment_identifier: makePaymentId(),
-      message_identifier: makeMessageId(),
-      chain_id: bigNumberify(depsMock.network.chainId) as UInt<32>,
-      token,
-      token_network_address: tokenNetwork,
-      recipient: depsMock.address,
-      target: partner, // lol
-      initiator: partner,
-      channel_identifier: bigNumberify(channelId) as UInt<32>,
-      metadata: { routes: [{ route: [depsMock.address, partner] }] },
-      lock,
-      locksroot: getLocksroot([...state.channels[key].partner.locks, lock]),
-      nonce: state.channels[key].partner.balanceProof.nonce.add(1) as UInt<8>,
-      transferred_amount: Zero as UInt<32>,
-      locked_amount: state.channels[key].partner.balanceProof.lockedAmount.add(
-        lock.amount,
-      ) as UInt<32>,
-    };
-
-    const transf = await signMessage(partnerSigner, unsigned, depsMock);
-    const received = action$.pipe(first(transferProcessed.is)).toPromise();
-
-    transferGenerateAndSignEnvelopeMessageEpic(
-      of(messageReceived({ text: '', message: transf, ts: Date.now() }, { address: partner })),
-      state$,
-      depsMock,
-    ).subscribe((a) => action$.next(a));
-
-    await received;
-    if (unlock) {
-      // register secret on-chain
-      action$.next(
-        transferSecretRegister.success(
-          { secret, txHash, txBlock: state.blockNumber + 1, confirmed: true },
-          { secrethash, direction: Direction.RECEIVED },
-        ),
-      );
-    }
-    // return channel state
-    return depsMock.latest$
-      .pipe(pluck('state', 'channels', channelKey({ tokenNetwork, partner })), first())
-      .toPromise();
-  }
-
-  afterEach(() => {
-    jest.clearAllMocks();
-    action$.complete();
-    state$.complete();
-    depsMock.latest$.complete();
-  });
-
-  test('success: receiving a transfer triggers monitoring', async () => {
-    expect.assertions(2);
-
-    const signerSpy = jest.spyOn(depsMock.signer, 'signMessage');
-
-    const promise = monitorRequestEpic(action$, state$, depsMock).toPromise();
-    action$.next(
-      udcDeposit.success(undefined, { totalDeposit: monitoringReward.mul(2) as UInt<32> }),
-    );
-    const channelState = await receiveTransfer(20);
-    const partnerBP = channelState.partner.balanceProof;
-    setTimeout(() => action$.complete(), 100);
-
-    await expect(promise).resolves.toEqual(
-      messageGlobalSend(
-        {
-          message: {
-            type: MessageType.MONITOR_REQUEST,
-            balance_proof: {
-              chain_id: partnerBP.chainId,
-              token_network_address: tokenNetwork,
-              channel_identifier: partnerBP.channelId,
-              nonce: partnerBP.nonce,
-              balance_hash: createBalanceHash(partnerBP),
-              additional_hash: partnerBP.additionalHash,
-              signature: partnerBP.signature,
-            },
-            non_closing_participant: depsMock.address,
-            non_closing_signature: expect.any(String),
-            monitoring_service_contract_address: expect.any(String),
-            reward_amount: monitoringReward,
-            signature: expect.any(String),
-          },
-        },
-        { roomName: expect.stringMatching(/_monitoring$/) },
-      ),
-    );
-
-    expect(signerSpy).toHaveBeenCalledTimes(3);
-    signerSpy.mockRestore();
-  });
-
-  test('success: token without known rateToSvt gets monitored', async () => {
-    expect.assertions(1);
-
-    action$.next(raidenConfigUpdate({ rateToSvt: {} }));
-
-    const promise = monitorRequestEpic(action$, state$, depsMock).toPromise();
-    action$.next(
-      udcDeposit.success(undefined, { totalDeposit: monitoringReward.mul(2) as UInt<32> }),
-    );
-    await receiveTransfer(20);
-    setTimeout(() => action$.complete(), 50);
-
-    await expect(promise).resolves.toBeDefined();
-  });
-
-  test('ignore: not enough udcBalance', async () => {
-    expect.assertions(1);
-
-    const promise = monitorRequestEpic(action$, state$, depsMock).toPromise();
-    action$.next(
-      udcDeposit.success(undefined, { totalDeposit: monitoringReward.sub(1) as UInt<32> }),
-    );
-
-    await receiveTransfer(10);
-    setTimeout(() => action$.complete(), 50);
-
-    await expect(promise).resolves.toBeUndefined();
-  });
-
-  test('ignore: config.monitoringReward unset', async () => {
-    expect.assertions(1);
-    action$.next(raidenConfigUpdate({ monitoringReward: null }));
-
-    const promise = monitorRequestEpic(action$, state$, depsMock).toPromise();
-    action$.next(
-      udcDeposit.success(undefined, { totalDeposit: monitoringReward.mul(2) as UInt<32> }),
-    );
-
-    await receiveTransfer(10);
-    setTimeout(() => action$.complete(), 50);
-
-    await expect(promise).resolves.toBeUndefined();
-  });
-
-  test('ignore: signing rejected not fatal', async () => {
-    expect.assertions(2);
-
-    const promise = monitorRequestEpic(action$, state$, depsMock).toPromise();
-    action$.next(
-      udcDeposit.success(undefined, { totalDeposit: monitoringReward.mul(2) as UInt<32> }),
-    );
-
-    // fails only after transfer's signatures
-    const originalSign = depsMock.signer.signMessage;
-    const signerSpy = jest
-      .spyOn(depsMock.signer, 'signMessage')
-      .mockImplementation(async (message) => {
-        if (signerSpy.mock.calls.length > 1) throw new Error('Signature rejected');
-        return originalSign.call(depsMock.signer, message);
-      });
-
-    await receiveTransfer(10);
-    setTimeout(() => action$.complete(), 100);
-
-    await expect(promise).resolves.toBeUndefined();
-
-    expect(signerSpy).toHaveBeenCalledTimes(2);
-    signerSpy.mockRestore();
-  });
-
-  test('ignore: non economically viable channels', async () => {
-    expect.assertions(1);
-
-    const promise = monitorRequestEpic(action$, state$, depsMock).toPromise();
-    action$.next(
-      udcDeposit.success(undefined, { totalDeposit: monitoringReward.mul(2) as UInt<32> }),
-    );
-
-    // transfer <= monitoringReward isn't worth to be monitored
-    await receiveTransfer(monitoringReward.toNumber());
-    setTimeout(() => action$.complete(), 50);
-
-    await expect(promise).resolves.toBeUndefined();
-  });
-
-  test('ignore: non unlocked amount', async () => {
-    expect.assertions(1);
-
-    const promise = monitorRequestEpic(action$, state$, depsMock).toPromise();
-    action$.next(
-      udcDeposit.success(undefined, { totalDeposit: monitoringReward.mul(2) as UInt<32> }),
-    );
-
-    // transfer <= monitoringReward isn't worth to be monitored
-    await receiveTransfer(20, false);
-    setTimeout(() => action$.complete(), 50);
-
-    await expect(promise).resolves.toBeUndefined();
-  });
-});
-
-describe('monitorRequestEpic', () => {
   test('success: receiving a transfer triggers monitoring', async () => {
     expect.assertions(1);
     const [raiden, partner] = await makeRaidens(2);
@@ -392,7 +86,7 @@ describe('monitorRequestEpic', () => {
     await ensureChannelIsDeposited([partner, raiden], deposit);
     await ensureTransferUnlocked([partner, raiden], amount);
 
-    await waitBlock(2);
+    await waitBlock();
     const partnerBP = getChannel(raiden, partner).partner.balanceProof;
 
     expect(raiden.output).toContainEqual(
@@ -527,7 +221,6 @@ describe('monitorRequestEpic', () => {
 
     const [raiden, partner] = await makeRaidens(2);
     const monitoringReward = bigNumberify(5) as UInt<32>;
-    const error = new RaidenError(ErrorCodes.RDN_GENERAL_ERROR);
     raiden.store.dispatch(
       raidenConfigUpdate({
         httpTimeout: 30,
@@ -562,7 +255,7 @@ describe('monitorRequestEpic', () => {
         expect.anything(),
       ),
     );
-    expect(last(raiden.output)).not.toEqual(raidenShutdown({ reason: error }));
+    expect(raiden.output).not.toContainEqual(raidenShutdown(expect.anything()));
     /* We expect 4 times because in the ensureTransferUnlocked after the transfer/success action
      we have MonitorRequest, SecretReveal, PFSCapacityUpdate which fail*/
     expect(signerSpy).toHaveBeenCalledTimes(4);
