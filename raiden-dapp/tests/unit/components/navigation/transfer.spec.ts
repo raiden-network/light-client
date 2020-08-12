fc22c84b4ade6150b060ffdebe0fb5f5e20ecd06
taleldayekh <mail@talel.se>
2020-08-12 11:33:51 +0200

    Getting open channel and token from mock data

--- raiden-dapp/tests/unit/components/navigation/transfer.spec.ts
+++ raiden-dapp/tests/unit/components/navigation/transfer.spec.ts
@@ -7,41 +7,22 @@ import VueRouter from 'vue-router';
 import Vuetify from 'vuetify';
 import Transfer from '@/components/navigation/Transfer.vue';
 import { TestData } from '../../data/mock-data';
-import { One, Zero } from 'ethers/constants';
+import { One } from 'ethers/constants';
 import { Token } from '@/model/types';
-import { ChannelState } from 'raiden-ts';
 
 Vue.use(Vuetify);
 
 describe('Transfer.vue', () => {
   const vuetify = new Vuetify();
   const router = new VueRouter() as Mocked<VueRouter>;
-  const token: Token = {
-    address: '0xtoken',
-    balance: One,
-    decimals: 18,
-    symbol: 'TTT',
-    name: 'Test Token',
-  };
+  const token: Token = TestData.token;
+  const openChannel = TestData.openChannel;
+
+  openChannel.capacity = One;
 
   store.commit('updateTokens', { '0xtoken': token });
   store.commit('updateChannels', {
-    '0xtoken': {
-      '0xaddr': {
-        capacity: One,
-        balance: Zero,
-        ownDeposit: One,
-        partnerDeposit: Zero,
-        partner: '0xaddr' as any,
-        token: '0xtoken' as any,
-        state: ChannelState.open,
-        settleTimeout: 500,
-        tokenNetwork: '0xtokennetwork' as any,
-        closeBlock: undefined,
-        openBlock: 12346,
-        id: 1,
-      },
-    },
+    '0xtoken': { '0xaddr': openChannel },
   });
 
   router.push = jest.fn().mockImplementation(() => Promise.resolve());
