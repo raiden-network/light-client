d84372ca8caa4637d1717574ff62d4374550cf7d
taleldayekh <mail@talel.se>
2020-08-27 16:46:05 +0200

    Updated notification card design
    The notification card now has a "proper" design. For the description, the text is split into an array of words which is gets iterated over in the template. Any word that happens to be an address gets wrapped in the address display component.

--- raiden-dapp/src/components/notification-panel/NotificationCard.vue
+++ raiden-dapp/src/components/notification-panel/NotificationCard.vue
@@ -1,39 +1,33 @@
 <template>
-  <v-card class="notification-card">
+  <v-card class="notification-card" flat>
     <v-row class="notification-card__content" no-gutters>
-      <v-col cols="3">
-        <v-avatar
-          class="notification-card__content__icon"
-          tile
-          size="80"
-          color="grey"
-        />
-      </v-col>
-      <v-col class="notification-card__content__details">
-        <div class="notification-card__content__details__header">
-          <div
-            class="notification-card__content__details__header--title text--primary title"
-          >
-            {{ notification.title }}
-          </div>
-          <v-btn
-            icon
-            x-small
-            class="notification-card__dismiss"
-            @click="notificationDelete(notification.id)"
-          >
-            <v-icon icon>mdi-close</v-icon>
-          </v-btn>
+      <v-avatar class="notification-card__content__icon" size="44" rounded>
+        <img :src="require(`@/assets/${notification.icon}`)" />
+      </v-avatar>
+      <div class="notification-card__content__details">
+        <span class="notification-card__content__details__title">
+          {{ notification.title }}
+        </span>
+        <div class="notification-card__content__details__description">
+          <p v-for="(word, index) in splitDescription" :key="index">
+            <template v-if="isAddress(word)">
+              <address-display
+                class="notification-card__content__details__description__address"
+                :address="word"
+              />
+            </template>
+            <template v-else>
+              {{ word }}
+            </template>
+          </p>
         </div>
-        <div
-          class="notification-card__content__details__description text--secondary"
-        >
-          <span>{{ notification.description }}</span>
-        </div>
-        <span class="notification-card__content__details--received">
+        <span class="notification-card__content__details__date">
           {{ notification.received | formatDate }}
         </span>
-      </v-col>
+      </div>
+      <v-btn x-small icon @click="notificationDelete(notification.id)">
+        <img :src="require('@/assets/trash.svg')" />
+      </v-btn>
     </v-row>
   </v-card>
 </template>
@@ -42,19 +36,32 @@
 import { Component, Prop, Vue } from 'vue-property-decorator';
 import { NotificationPayload } from '@/store/notifications/types';
 import { createNamespacedHelpers } from 'vuex';
+import AddressDisplay from '@/components/AddressDisplay.vue';
 
 const { mapMutations } = createNamespacedHelpers('notifications');
 
 @Component({
+  components: {
+    AddressDisplay,
+  },
   methods: {
     ...mapMutations(['notificationDelete']),
   },
 })
 export default class NotificationCard extends Vue {
+  splitDescription: string[] = [];
+  notificationDelete!: (id: number) => void;
+
   @Prop({ required: true })
   notification!: NotificationPayload;
 
-  notificationDelete!: (id: number) => void;
+  isAddress(address: string): boolean {
+    return /^0x.+/.test(address);
+  }
+
+  mounted() {
+    this.splitDescription = this.notification.description.split(/\s+/);
+  }
 }
 </script>
 
@@ -63,54 +70,50 @@ export default class NotificationCard extends Vue {
 @import '@/scss/scroll';
 
 .notification-card {
-  background-color: $notification-card-background;
-  border-radius: 20px !important;
-  height: 200px;
-
-  &__dismiss {
-    padding-left: 14px;
-  }
+  background-color: $notification-card-bg;
+  border-radius: 16px !important;
+  height: 88px;
 
   &__content {
     height: 100%;
-    padding: 30px 30px 0 30px;
+    padding: 16px;
+
+    &__icon {
+      background-color: $notification-icon-bg;
+      padding: 4px;
+    }
 
     &__details {
       display: flex;
+      flex: 1;
       flex-direction: column;
-      max-width: 350px;
+      margin-left: 16px;
 
-      &__header {
-        display: flex;
-
-        &--title {
-          overflow-x: hidden;
-          text-overflow: ellipsis;
-          display: flex;
-          flex: 1;
-        }
+      &__title {
+        color: $primary-color;
       }
 
       &__description {
-        margin-top: 10px;
-        max-height: 100px;
-        overflow-y: scroll;
-        padding-bottom: 10px;
-        @extend .themed-scrollbar;
-      }
-
-      &--received {
         color: $secondary-text-color;
         display: flex;
-        align-items: flex-end;
-        flex: 1;
-        padding-bottom: 30px;
+        font-size: 14px;
+
+        > p {
+          padding-right: 4px;
+          margin: 0;
+        }
+
+        &__address {
+          color: $secondary-text-color;
+          font-size: 14px;
+          padding-top: 1px;
+        }
+      }
+
+      &__date {
+        color: $secondary-text-color;
         font-size: 12px;
       }
-    }
-
-    &__icon {
-      margin-top: 5px;
     }
   }
 }
