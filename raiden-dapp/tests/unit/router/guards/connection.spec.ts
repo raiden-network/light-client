9af9eb960af5390756edce5692db144908c211e3
Thore Weilbier <thore@weilbier.net>
2020-09-24 12:34:04 +0200

    dapp: fix routing to account if not connected
    If the user is not connected with the dApp, he should be still able to
    access the account route (and its children). Due to the recent switch to
    use a global navigation guard to deny access without being connected,
    the account routes have become protected as well.
    Therefore the guard for the connection state has been simply extended to
    allow navigations to any account route, no matter the connection state.
    
    While extending the tests, it has shown that those are not properly
    mocked. So their order was important to make them complete. This has
    been solved by tweaking with the store. But it should be target of
    future improvements to mock the store completely for unit tests, not
    only here. Unfortunately are the getters of the store read-only
    protected. So this improvement will need some more attention to find the
    best solution.

--- raiden-dapp/tests/unit/router/guards/connection.spec.ts
+++ raiden-dapp/tests/unit/router/guards/connection.spec.ts
@@ -5,19 +5,48 @@ import { transformRouteConfigsToRoutes } from '../../utils/router-utils';
 
 const {
   [RouteNames.HOME]: homeRoute,
-  ...routesWithoutHome
+  [RouteNames.ACCOUNT_ROOT]: accountRootRoute,
+  [RouteNames.ACCOUNT_BACKUP]: accountBackupRoute,
+  [RouteNames.ACCOUNT_RAIDEN]: accountRaidenRoute,
+  [RouteNames.ACCOUNT_SETTINGS]: accountSettingsRoute,
+  [RouteNames.ACCOUNT_WITHDRAWAL]: accountWithdrawalRoute,
+  [RouteNames.ACCOUNT_UDC]: accountUdcRoute,
+  ...protectedRoutes
 } = transformRouteConfigsToRoutes();
 
-function connect() {
-  store.commit('loadComplete');
-  store.commit('account', 'not-empty');
-}
+const accountRoutes = [
+  accountRootRoute,
+  accountBackupRoute,
+  accountRaidenRoute,
+  accountSettingsRoute,
+  accountWithdrawalRoute,
+  accountUdcRoute,
+];
 
 describe('redirectIfNotConnected()', () => {
+  // TODO: Properly mock the global store object (getters are read-only protected)
+  //
+  // The idea here is that the `isConnected` getter depends on the loading state
+  // and the account address. So it sets loading completed and then toggles the
+  // account to be either empty or not. As result, the connection state
+  // switches.
+  // An issue is, that the loading state is not re-settable with mutations.
+  beforeAll(() => {
+    store.commit('loadComplete');
+  });
+
+  afterEach(() => {
+    store.commit('account', '');
+  });
+
+  function connect(): void {
+    store.commit('account', 'not-empty');
+  }
+
   test('redirect to home route if not connected', () => {
     expect(store.getters.isConnected).toBe(false);
 
-    Object.values(routesWithoutHome).forEach((route) => {
+    Object.values(protectedRoutes).forEach((route) => {
       expect(redirectIfNotConnected(route)).toEqual(
         expect.objectContaining({ name: RouteNames.HOME })
       );
@@ -25,9 +54,9 @@ describe('redirectIfNotConnected()', () => {
   });
 
   test('add redirect query parameter to original target when redirecting to home route', () => {
-    expect(store.state.disclaimerAccepted).toBe(false);
+    expect(store.getters.isConnected).toBe(false);
 
-    Object.values(routesWithoutHome).forEach((route) => {
+    Object.values(protectedRoutes).forEach((route) => {
       expect(redirectIfNotConnected(route)).toEqual(
         expect.objectContaining({ query: { redirectTo: route.fullPath } })
       );
@@ -42,16 +71,26 @@ describe('redirectIfNotConnected()', () => {
 
   test('skip home route if already connected', () => {
     connect();
+    expect(store.getters.isConnected).toBe(true);
 
     expect(redirectIfNotConnected(homeRoute)).toEqual({
       name: RouteNames.TRANSFER,
     });
   });
 
-  test('do nothing if disclaimer is accepted and not navigating to disclaimer route', () => {
+  test('do nothing if connected and not navigating to home route', () => {
     connect();
+    expect(store.getters.isConnected).toBe(true);
 
-    Object.values(routesWithoutHome).forEach((route) => {
+    Object.values(protectedRoutes).forEach((route) => {
+      expect(redirectIfNotConnected(route)).toBeUndefined();
+    });
+  });
+
+  test('do nothing if not connected and navigating to any account route', () => {
+    expect(store.getters.isConnected).toBe(false);
+
+    Object.values(accountRoutes).forEach((route) => {
       expect(redirectIfNotConnected(route)).toBeUndefined();
     });
   });
