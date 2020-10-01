78b29f0792c0f1d9b2b389be47f683b8eecd1a1a
taleldayekh <mail@talel.se>
2020-10-01 14:41:22 +0200

    Introducing fixtures
    This is a first attempt to make sense about how to split the tests into reusable chunks. Here accessing the disclaimer route has been split into a separate function while the assertions happens in the test for the disclaimer.

--- raiden-dapp/tests/e2e/specs/accept-disclaimer.spec.ts
+++ raiden-dapp/tests/e2e/specs/accept-disclaimer.spec.ts
@@ -1,11 +1,12 @@
-describe('disclaimer', () => {
-  it('accepts disclaimer and redirects to home route', () => {
-    cy.viewport('macbook-13');
-    cy.visit('/');
-    cy.contains('Disclaimer').should('be.visible');
-    cy.get('.disclaimer__accept-checkbox').click();
-    cy.get('.disclaimer__persist-checkbox').click();
-    cy.get('.disclaimer__accept-button').click();
-    cy.url().should('include', '/#/home');
-  });
+import { disclaimerRoute } from '../fixtures';
+
+it('accepts disclaimer and redirects to home route', () => {
+  disclaimerRoute();
+
+  cy.contains('Disclaimer').should('be.visible');
+  cy.get('.disclaimer__accept-checkbox').click();
+  cy.get('.disclaimer__persist-checkbox').click();
+  cy.get('.disclaimer__accept-button').click();
+
+  cy.url().should('include', '/#/home');
 });
