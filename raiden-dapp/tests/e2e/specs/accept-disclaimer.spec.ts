5d01a7fc8874546dd87c34dc04ff5d4f3ccb2596
Thore Weilbier <thore@weilbier.net>
2020-10-06 12:15:23 +0200

    dapp: use expect style assertions for e2e tests

--- raiden-dapp/tests/e2e/specs/accept-disclaimer.spec.ts
+++ raiden-dapp/tests/e2e/specs/accept-disclaimer.spec.ts
@@ -2,11 +2,11 @@ import { navigateToDisclaimerRoute } from '../utils';
 
 it('accepts disclaimer and redirects to home route', () => {
   navigateToDisclaimerRoute();
+  cy.contains('Disclaimer').should((div) => expect(div).toBeVisible());
 
-  cy.contains('Disclaimer').should('be.visible');
   cy.get('.disclaimer__accept-checkbox').click();
   cy.get('.disclaimer__persist-checkbox').click();
   cy.get('.disclaimer__accept-button').click();
 
-  cy.url().should('include', '/#/home');
+  cy.url().should((url) => expect(url).toContain('/#/home'));
 });
