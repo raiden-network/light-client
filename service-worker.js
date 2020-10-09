4d2bb224710303bd027fbdb7f4730b22c96cac63
Thore Weilbier <thore@weilbier.net>
2020-10-09 13:08:23 +0200

    Hotfix deployment of latest v0.11.1 without mainnet
    Unfortunately our CI pipeline is broken and has built the latest release
    in development mode. This for examples allows the user to connect to the
    main network which is not allowed yet.

---
+++ service-worker.js
@@ -0,0 +1,16 @@
+importScripts("/precache-manifest.65ce2cdf6d6927814e0f94b236d55d65.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+self.addEventListener('message', (e) => {
+  if (!e.data) {
+    return;
+  }
+
+  switch (e.data) {
+    case 'skipWaiting':
+      self.skipWaiting();
+      break;
+    default:
+      break;
+  }
+});
+
