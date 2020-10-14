f460eaa4644d63c04c9b64ef3ee20e06c9347f6a
taleldayekh <mail@talel.se>
2020-10-14 10:57:30 +0200

    Hook to delete indexedDBs before a test run
    Cypress supports hooks that runs before and after each test and the documentation provides examples on how to add them. Therefore I added the code for deleting indexedDBs in a before hook. The documentation also suggests to use the hooks inside the scope of a describe block.

--- raiden-dapp/tests/e2e/specs/scenario.spec.ts
+++ raiden-dapp/tests/e2e/specs/scenario.spec.ts
@@ -1,73 +1,18 @@
-import {
-  navigateToDisclaimer,
-  navigateToSelectHub,
-  navigateToNotificationPanel,
-  navigateToAccountMenu,
-  navigateToBackupState,
-  navigateToTokenSelect,
-  navigateToConnectNewToken,
-  navigateBackToTransferScreen,
-  navigateToTokenDeposit,
-  navigateToChannelsList,
-  navigateToRaidenAccount,
-  navigateToWithdrawal,
-  navigateToUDC,
-  navigateToDownloadLogs
-} from '../utils/navigation-utils';
-import {
-  acceptDisclaimer,
-  connectToDApp,
-  enterAndSelectHub,
-  enterChannelDepositAmount,
-  deleteTopNotification,
-  closeNotificationPanel,
-  enterTransferAddress,
-  enterTransferAmount,
-  makeDirectTransfer,
-  downloadState,
-  depositTokensToOpenedChannel,
-  withdrawTokens,
-  withdrawUDCTokens
-} from '../utils/user-interaction-utils';
-import {
-  mintAndDepositUtilityToken,
-  mintSelectedToken,
-  openChannel,
-  transferETHToRaidenAccount,
-  withdrawTokensBackToMainAccount
-} from '../utils/blockchain-interaction-utils';
+import { navigateToDisclaimer } from '../utils/navigation';
+import { acceptDisclaimer, connectToDapp } from '../utils/user-interaction';
 
-it('', () => {
-  navigateToDisclaimer();
-  acceptDisclaimer();
-  connectToDApp();
-  navigateToSelectHub();
-  mintAndDepositUtilityToken();
-  mintSelectedToken();
-  enterAndSelectHub();
-  enterChannelDepositAmount();
-  openChannel();
-  navigateToNotificationPanel();
-  deleteTopNotification();
-  closeNotificationPanel();
-  enterTransferAddress();
-  enterTransferAmount();
-  makeDirectTransfer();
-  navigateToAccountMenu();
-  navigateToBackupState();
-  downloadState();
-  navigateToTokenSelect();
-  navigateToConnectNewToken();
-  navigateBackToTransferScreen();
-  navigateToTokenDeposit();
-  depositTokensToOpenedChannel();
-  navigateToChannelsList()
-  withdrawTokens();
-  navigateToRaidenAccount();
-  transferETHToRaidenAccount();
-  navigateToWithdrawal();
-  withdrawTokensBackToMainAccount();
-  navigateToUDC();
-  withdrawUDCTokens();
-  navigateToDownloadLogs();
+describe('dApp e2e tests', () => {
+  before(async () => {
+    const allIndexedDBs = await window.indexedDB.databases();
+    allIndexedDBs.forEach((indexedDB) => {
+      window.indexedDB.deleteDatabase(indexedDB.name);
+    });
+  });
+
+  it('should run all dApp e2e scenarios', () => {
+    cy.viewport('macbook-13');
+    navigateToDisclaimer();
+    acceptDisclaimer();
+    connectToDapp();
+  });
 });
