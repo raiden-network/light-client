14a627a3425789607c20d2f6b43172e8d5245e53
taleldayekh <mail@talel.se>
2020-10-15 14:25:28 +0200

    Remaining tests refactoring

--- raiden-dapp/tests/e2e/utils/blockchain-interaction.ts
+++ raiden-dapp/tests/e2e/utils/blockchain-interaction.ts
@@ -20,3 +20,36 @@ export function openChannel() {
   cy.getWithCustomTimeout('.open-channel-dialog').should('not.exist');
   cy.getWithCustomTimeout('.open-channel').should('not.exist');
 }
+
+export function depositTokensToOpenedChannel() {
+  cy.get('.channel-deposit').should('exist');
+  cy.get('.channel-deposit__button').click();
+  cy.getWithCustomTimeout('.channel-deposit').should('not.exist');
+}
+
+export function withdrawTokens() {
+  cy.get('.channel-withdraw').should('exist');
+  cy.get('.channel-withdraw__button').click();
+  cy.getWithCustomTimeout('.channel-withdraw').should('not.exist');
+}
+
+export function transferETHToRaidenAccount() {
+  cy.get('.raiden-account__transfer-button__button').click();
+  cy.getWithCustomTimeout('.raiden-account__progress-wrapper').should(
+    'not.exist'
+  );
+}
+
+export function withdrawTokensBackToMainAccount() {
+  cy.get('.withdrawal__tokens').should('exist');
+  cy.get('.withdrawal__tokens__button').eq(0).click();
+  cy.getWithCustomTimeout('.withdrawal-dialog__action').should('exist');
+  cy.get('.withdrawal-dialog__action').click();
+  cy.getWithCustomTimeout('.withdrawal__empty').should('exist');
+}
+
+export function withdrawUDCTokens() {
+  cy.get('.udc-withdrawal-dialog').should('exist');
+  cy.get('.udc-withdrawal-dialog__button').click();
+  cy.getWithCustomTimeout('.udc-withdrawal-dialog').should('not.exist');
+}
--- raiden-dapp/tests/e2e/utils/user-interaction.ts
+++ raiden-dapp/tests/e2e/utils/user-interaction.ts
@@ -6,7 +6,7 @@ export function acceptDisclaimer() {
   cy.getWithCustomTimeout('.disclaimer').should('not.exist');
 }
 
-export function connectToDapp(firstTimeConnect = true) {
+export function connectToDApp(firstTimeConnect = true) {
   cy.get('.home').should('exist');
   cy.get('.home__connect-button').click();
 
@@ -30,5 +30,78 @@ export function enterChannelDepositAmount(uiTimeout) {
   cy.get('.open-channel').should('exist');
   cy.contains('Open Channel');
   cy.get('.amount-input').type('0.5');
+  cy.wait(uiTimeout);
+}
+
+export function deleteTopNotification() {
+  cy.get('#notification-panel').should('exist');
+  cy.getWithCustomTimeout('.notification-card__delete-button')
+    .eq(0)
+    .should('exist');
+  cy.get('.notification-card__delete-button').eq(0).click();
+  cy.getWithCustomTimeout('.notification-card').should('not.exist');
+}
+
+export function closeNotificationPanel() {
+  cy.get('.notification-panel-content__close__button').should('exist');
+  cy.get('.notification-panel-content__close__button').click();
+  cy.getWithCustomTimeout('#notification-panel').should('not.exist');
+}
+
+export function enterTransferAddress(uiTimeout, partnerAddress) {
+  cy.get('.transfer-inputs').should('exist');
+  cy.get('.address-input').type(partnerAddress);
+  cy.wait(uiTimeout);
+}
+
+export function enterTransferAmount(uiTimeout) {
+  cy.get('.transfer-inputs').should('exist');
+  cy.get('.amount-input').type('0.0001');
+  cy.wait(uiTimeout);
+}
+
+export function makeDirectTransfer(uiTimeout) {
+  cy.get('.transfer-inputs').should('exist');
+  cy.get('.transfer-inputs__form__button').click();
+  cy.wait(uiTimeout);
+  cy.getWithCustomTimeout('.transfer__button').should('exist');
+  cy.get('.transfer__button').click();
+  cy.getWithCustomTimeout('.transfer-inputs').should('exist');
+}
+
+export function downloadState() {
+  cy.get('.backup-state').should('exist');
+  cy.get('.backup-state__buttons__download-state').click();
+  cy.getWithCustomTimeout('.download-state__button').should('exist');
+  cy.get('.download-state__button').click();
+  cy.getWithCustomTimeout('.backup-state').should('not.exist');
+}
+
+export function enterDepositTokenAmountForOpenedChannel(uiTimeout) {
+  cy.get('.channel-deposit').should('exist');
+  cy.get('.channel-deposit__input').type('001');
+  cy.wait(uiTimeout);
+}
+
+export function enterTokenWithdrawalAmoutFromChannelsList(uiTimeout) {
+  cy.get('.channel-list').should('exist');
+  cy.get('.channel-action-button').eq(1).click();
+  cy.getWithCustomTimeout('.channel-withdraw').should('exist');
+  cy.get('.channel-withdraw__input').type('001');
+  cy.wait(uiTimeout);
+}
+
+export function enterETHAmountToTransferFromRaidenAccount(uiTimeout) {
+  cy.get('.raiden-account').should('exist');
+  cy.get('input').clear();
+  cy.get('.raiden-account__amount-input__field').type('100');
+  cy.wait(uiTimeout);
+}
+
+export function enterUDCWithdrawalAmount(uiTimeout) {
+  cy.get('.udc').should('exist');
+  cy.get('.udc__actions__button').eq(1).click();
+  cy.getWithCustomTimeout('.udc-withdrawal-dialog').should('exist');
+  cy.get('.udc-withdrawal-dialog__amount').type('1');
   cy.wait(uiTimeout);
 }
