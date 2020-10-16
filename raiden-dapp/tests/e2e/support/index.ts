4f57e11e9ba2737094e680f3093b8c767d28332b
taleldayekh <mail@talel.se>
2020-10-16 13:11:10 +0200

    Naming anonymous function
    This makes it easier to understand what the function for ingoring uncought exceptions actually does.

--- raiden-dapp/tests/e2e/support/index.ts
+++ raiden-dapp/tests/e2e/support/index.ts
@@ -2,6 +2,6 @@ import '@cypress/code-coverage/support';
 import './commands';
 import 'cypress-jest-adapter';
 
-Cypress.on('uncaught:exception', () => {
+Cypress.on('uncaught:exception', function ignore() {
   return false;
 });
