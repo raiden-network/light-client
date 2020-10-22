2383f66ab59dabf7edfcea5b6982adf54ae501fa
Thore Weilbier <thore@weilbier.net>
2020-10-22 19:58:41 +0200

    Manual deployment to GitHub Pages: v0.12.0

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.65ce2cdf6d6927814e0f94b236d55d65.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/precache-manifest.73daf8fd7c05ff9e9a1abc81a4cce0cc.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 self.addEventListener('message', (e) => {
   if (!e.data) {
