87cb95240aade3a3582378f03d58d41bd897dc5d
André Vitor de Lima Matos <andre.vmatos@gmail.com>
2020-11-10 13:21:20 -0300

    v0.12.0 -> 0.13.0

--- prepare-release.sh
+++ prepare-release.sh
@@ -1,49 +1,26 @@
 #!/usr/bin/env bash
 
+set -e
+
 if [[ "$#" -ne 1 ]]; then
-  VERSION_PARAMS=$(pnpm version --help | grep -oPm1 'npm version (.*)' | sed 's/npm version/''/g')
-  echo "USAGE: $0 $VERSION_PARAMS"
+  echo "Usage: $0 [--major|--minor|--patch]"
   exit 1
 fi
 
-cd raiden-ts
+cd "$( dirname $0 )"
 
-PACKAGE_VERSION=$(cat package.json |
-  grep version |
-  head -1 |
-  awk -F: '{ print $2 }' |
-  sed 's/[",]//g' |
-  tr -d '[[:space:]]')
+OLD_VERSION=`yarn versions --json | jq -er '.data."light-client"'`
+yarn version $1 --no-git-tag-version
+NEW_VERSION=`yarn versions --json | jq -er '.data."light-client"'`
+MESSAGE="v$OLD_VERSION -> $NEW_VERSION"
 
-# Using `--no-git-tag-version` in January 2020 serves no purpose due to `npm version`
-# expecting the command to run from the same directory that also hosts the `.git` folder
-# You can find more information on https://github.com/npm/npm/issues/9111.
-# We disable the whole functionality because there no option exists to disable only automatic tagging.
-VERSION=$(pnpm version $1 --no-git-tag-version)
-echo "sdk version update updated to $VERSION"
-MESSAGE="v$PACKAGE_VERSION -> $VERSION"
+yarn workspace raiden-ts version --no-git-tag-version --new-version "${NEW_VERSION}"
+yarn workspace raiden-dapp version --no-git-tag-version --new-version "${NEW_VERSION}"
+yarn workspace raiden-cli version --no-git-tag-version --new-version "${NEW_VERSION}"
 
-cd ../raiden-dapp/
+yarn install
 
-DAPP_VERSION=$(pnpm version $1 --no-git-tag-version)
-echo "dApp version update updated to $DAPP_VERSION"
-
-cd ../raiden-cli/
-
-CLI_VERSION=$(pnpm version $1 --no-git-tag-version)
-echo "cli version update updated to $CLI_VERSION"
-
-cd ../
-ROOT_VERSION=$(pnpm version $1 --no-git-tag-version)
-echo "light client version update updated to $ROOT_VERSION"
-
-pnpm install
-
-git add package.json
-git add raiden-ts/package.json
-git add raiden-dapp/package.json
-git add raiden-cli/package.json
-git add pnpm-lock.yaml
+git add package.json raiden-{ts,dapp,cli}/package.json yarn.lock
 
 echo "Preparing to commit version update $MESSAGE"
 git commit -m "$MESSAGE"
