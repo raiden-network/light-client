e7322b301d7026cfcea2ec885dc46660d383bdd5
taleldayekh <mail@talel.se>
2020-11-12 10:51:36 +0100

    dApp: Customizable privacy policy

--- raiden-dapp/tests/unit/app.spec.ts
+++ raiden-dapp/tests/unit/app.spec.ts
@@ -6,47 +6,63 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Vuex from 'vuex';
 import Vuetify from 'vuetify';
-import Mocked = jest.Mocked;
 import store from '@/store/index';
 import RaidenService from '@/services/raiden-service';
 import App from '@/App.vue';
+import Mocked = jest.Mocked;
 
 Vue.use(VueRouter);
 Vue.use(Vuex);
 Vue.use(Vuetify);
 
-describe('App.vue', () => {
-  let wrapper: Wrapper<App>;
-  let vuetify: Vuetify;
-  let router: Mocked<VueRouter>;
-  let $raiden: RaidenService;
+let vuetify: Vuetify;
+let router: Mocked<VueRouter>;
+let $raiden: RaidenService;
 
-  beforeEach(() => {
-    vuetify = new Vuetify();
-    router = new VueRouter() as Mocked<VueRouter>;
-    router.push = jest.fn().mockResolvedValue(null);
-    $raiden = new RaidenService(store);
-    $raiden.disconnect = jest.fn();
+const createWrapper = (imprint: string): Wrapper<App> => {
+  if (imprint) {
+    process.env.VUE_APP_IMPRINT = imprint;
+  } else {
+    delete process.env.VUE_APP_IMPRINT;
+  }
 
-    wrapper = shallowMount(App, {
-      vuetify,
-      store,
-      stubs: ['router-view', 'v-dialog'],
-      mocks: {
-        $router: router,
-        $raiden: $raiden,
-        $t: (msg: string) => msg,
-      },
-    });
+  vuetify = new Vuetify();
+  router = new VueRouter() as Mocked<VueRouter>;
+  router.push = jest.fn().mockResolvedValue(null);
+  $raiden = new RaidenService(store);
+  $raiden.disconnect = jest.fn();
+
+  return shallowMount(App, {
+    vuetify,
+    store,
+    stubs: ['router-view', 'v-dialog'],
+    mocks: {
+      $router: router,
+      $raiden: $raiden,
+      $t: (msg: string) => msg,
+    },
   });
+};
 
-  test('displays privacy policy', () => {
+describe('App.vue', () => {
+  test('displays privacy policy if imprint env variable is set', () => {
+    const wrapper = createWrapper('https://custom-imprint.test');
     const privacyPolicy = wrapper.find('.policy');
+    const privacyPolicyUrl = privacyPolicy.find('a').attributes().href;
 
     expect(privacyPolicy.text()).toBe('application.privacy-policy');
+    expect(privacyPolicyUrl).toBe('https://custom-imprint.test');
+  });
+
+  test('does not display privacy policy if imprint env variable is not set', () => {
+    const wrapper = createWrapper('');
+    const privacyPolicy = wrapper.find('.policy');
+
+    expect(privacyPolicy.exists()).toBe(false);
   });
 
   test('disconnects on destruction', () => {
+    const wrapper = createWrapper('');
     wrapper.vm.$destroy();
 
     expect($raiden.disconnect).toHaveBeenCalledTimes(1);
