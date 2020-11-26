afd265bad9a5b55396f7863f762b57221f637b68
Thore Weilbier <thore@weilbier.net>
2020-11-26 17:36:17 +0100

    dapp: refactor notification card unit tests
    Follow some of the agreed rules in preparation to add new ones.
    Use a create wrapper function and fully mock the store.

--- raiden-dapp/tests/unit/components/notification-panel/notification-card.spec.ts
+++ raiden-dapp/tests/unit/components/notification-panel/notification-card.spec.ts
@@ -1,46 +1,65 @@
+jest.mock('vue-router');
+
 import { mount, Wrapper } from '@vue/test-utils';
 import Vue from 'vue';
 import Vuetify from 'vuetify';
 import VueRouter from 'vue-router';
+import Vuex  from 'vuex';
 import { TestData } from '../../data/mock-data';
 import Mocked = jest.Mocked;
 import { RouteNames } from '@/router/route-names';
 import Filters from '@/filters';
 import NotificationCard from '@/components/notification-panel/NotificationCard.vue';
-import store from '@/store';
 
 Vue.use(Vuetify);
+Vue.use(Vuex);
 Vue.filter('formatDate', Filters.formatDate);
 
-describe('NotificationCard.vue', () => {
-  let wrapper: Wrapper<NotificationCard>;
-  let vuetify: Vuetify;
-  let router: Mocked<VueRouter>;
+const vuetify = new Vuetify();
+const $router = new VueRouter() as Mocked<VueRouter>;
+const $raiden = { fetchAndUpdateTokenData: jest.fn() };
+const notificationDelete = jest.fn((_id: number) => null);
 
+function createWrapper(
+  notification = TestData.notifications
+): Wrapper<NotificationCard> {
+  const state = { blockNumber: 100 };
+  const notificationsModule = {
+    namespaced: true,
+    mutations: { notificationDelete },
+  };
+
+  const store = new Vuex.Store({
+    state,
+    modules: { notifications: notificationsModule }
+  });
+
+  return mount(NotificationCard, {
+    vuetify,
+    store,
+    mocks: {
+      $router,
+      $raiden,
+      $t: (msg: string) => msg,
+    },
+    propsData: { notification },
+  });
+}
+
+describe('NotificationCard.vue', () => {
   beforeEach(() => {
-    vuetify = new Vuetify();
-    router = new VueRouter() as Mocked<VueRouter>;
-    wrapper = mount(NotificationCard, {
-      vuetify,
-      store,
-      mocks: {
-        $router: router,
-        $t: (msg: string) => msg,
-        $raiden: {
-          fetchAndUpdateTokenData: jest.fn(),
-        },
-      },
-      propsData: { notification: TestData.notifications },
-    });
+    jest.resetAllMocks();
   });
 
   test('displays notification title', () => {
+    const wrapper = createWrapper();
     const notificationTitle = wrapper.find('.notification-card__content__details__title');
 
     expect(notificationTitle.text()).toBe('Channel Settlement');
   });
 
   test('displays notification description', () => {
+    const wrapper = createWrapper();
     const { wrappers } = wrapper
       .find('.notification-card__content__details__description')
       .findAll('p');
@@ -58,44 +77,47 @@ describe('NotificationCard.vue', () => {
   });
 
   test('displays block count progress if supported by notification ', () => {
+    const wrapper = createWrapper();
     const notificationBlockCount = wrapper.find(
       '.notification-card__content__details__block-count',
     );
 
-    expect(notificationBlockCount.text()).toContain('123');
+    expect(notificationBlockCount.text()).toContain('23');
     expect(notificationBlockCount.text()).toContain('notifications.block-count-progress');
   });
 
   test('displays link if link is in notification', () => {
+    const wrapper = createWrapper();
     const notificationLink = wrapper.find('.notification-card__content__details__link');
 
     expect(notificationLink.text()).toContain('Visit the Withdrawal menu');
   });
 
   test('clicking link in notification routes user', () => {
-    router.push = jest.fn().mockResolvedValue(null);
-
+    const wrapper = createWrapper();
     const notificationLink = wrapper.find('.notification-card__content__details__link');
+
     notificationLink.trigger('click');
 
-    expect(router.push).toHaveBeenCalledTimes(1);
-    expect(router.push).toHaveBeenCalledWith(
+    expect($router.push).toHaveBeenCalledTimes(1);
+    expect($router.push).toHaveBeenCalledWith(
       expect.objectContaining({ name: RouteNames.ACCOUNT_WITHDRAWAL }),
     );
   });
 
   test('displays correctly formatted date', () => {
+    const wrapper = createWrapper();
     const notificationReceived = wrapper.find('.notification-card__content__details__received');
 
     expect(notificationReceived.text()).toBe('6/5/1986 12:00:00 AM');
   });
 
   test('clicking "trash"-icon calls method for deleting notification', async () => {
-    store.commit('notifications/notificationAddOrReplace', TestData.notifications);
+    const wrapper = createWrapper();
     const deleteNotificationButton = wrapper.find('button');
     deleteNotificationButton.trigger('click');
     await wrapper.vm.$nextTick();
 
-    expect(Object.keys(store.state.notifications.notifications)).toHaveLength(0);
+    expect(notificationDelete).toHaveBeenCalledTimes(1);
   });
 });
