afd632ecebda4f015cd67d86c3b6f12422c7e709
André Vitor de Lima Matos <andre.vmatos@gmail.com>
2021-02-08 17:48:47 -0300

    sdk: clear old unused mocks & fixtures

--- raiden-ts/tests/unit/fixtures.ts
+++ raiden-ts/tests/unit/fixtures.ts
@@ -1,37 +1,26 @@
 import {
-  makeMatrix,
-  MockRaidenEpicDeps,
   MockedRaiden,
   makeAddress,
   makeHash,
   waitBlock,
   providersEmit,
   makeLog,
+  sleep,
 } from './mocks';
 
-import { Subject } from 'rxjs';
-import { exhaustMap, first, pluck, scan } from 'rxjs/operators';
-import { Wallet } from '@ethersproject/wallet';
-import { AddressZero, Zero, HashZero } from '@ethersproject/constants';
+import { exhaustMap, first, pluck } from 'rxjs/operators';
+import { Zero, HashZero } from '@ethersproject/constants';
 import { BigNumber } from '@ethersproject/bignumber';
 import { defaultAbiCoder } from '@ethersproject/abi';
 
 import { Address, Hash, Int, UInt, Secret } from 'raiden-ts/utils/types';
-import { Processed, MessageType } from 'raiden-ts/messages/types';
 import {
-  makeMessageId,
   makePaymentId,
   makeSecret,
   getSecrethash,
   transferKey,
   getTransfer,
 } from 'raiden-ts/transfers/utils';
-import { IOU } from 'raiden-ts/services/types';
-import { RaidenState } from 'raiden-ts/state';
-import { pluckDistinct } from 'raiden-ts/utils/rx';
-import { RaidenAction } from 'raiden-ts/actions';
-import { raidenReducer } from 'raiden-ts/reducer';
-import { getLatest$ } from 'raiden-ts/epics';
 import { channelKey } from 'raiden-ts/channels/utils';
 import { tokenMonitored } from 'raiden-ts/channels/actions';
 import { ChannelState, Channel } from 'raiden-ts/channels';
@@ -40,110 +29,6 @@ import { transfer, transferSecret, transferUnlock } from 'raiden-ts/transfers/ac
 import { assert } from 'raiden-ts/utils';
 import { isResponseOf } from 'raiden-ts/utils/actions';
 import { matrixPresence } from 'raiden-ts/transport/actions';
-
-/**
- * Composes several constants used across epics
- *
- * @param depsMock - RaidenEpicDeps mock object constructed through raidenEpicDeps
- * @returns Diverse constant values and objects
- */
-export function epicFixtures(depsMock: MockRaidenEpicDeps) {
-  const wallet = new Wallet('0x3333333333333333333333333333333333333333333333333333333333333333'),
-    token = '0x0000000000000000000000000000000000010001' as Address,
-    tokenNetwork = '0x0000000000000000000000000000000000020001' as Address,
-    partner = wallet.address as Address,
-    target = '0x0100000000000000000000000000000000000005' as Address,
-    tokenNetworkContract = depsMock.getTokenNetworkContract(tokenNetwork),
-    tokenContract = depsMock.getTokenContract(token),
-    settleTimeout = 500,
-    channelId = 17,
-    isFirstParticipant = true,
-    matrixServer = 'matrix.raiden.test',
-    userId = `@${depsMock.address.toLowerCase()}:${matrixServer}`,
-    accessToken = 'access_token',
-    deviceId = 'device_id',
-    displayName = 'display_name',
-    partnerRoomId = `!partnerRoomId:${matrixServer}`,
-    partnerUserId = `@${partner.toLowerCase()}:${matrixServer}`,
-    targetUserId = `@${target.toLowerCase()}:${matrixServer}`,
-    matrix = makeMatrix(userId, matrixServer),
-    txHash = '0x0000000000000000000000000000000000000020111111111111111111111111' as Hash,
-    processed: Processed = { type: MessageType.PROCESSED, message_identifier: makeMessageId() },
-    paymentId = makePaymentId(),
-    fee = BigNumber.from(3) as Int<32>,
-    paths = [{ path: [partner], fee }],
-    metadata = { routes: [{ route: [partner] }] },
-    pfsAddress = '0x0900000000000000000000000000000000000009' as Address,
-    pfsTokenAddress = '0x0800000000000000000000000000000000000008' as Address,
-    pfsInfoResponse = {
-      message: 'pfs message',
-      network_info: {
-        chain_id: depsMock.network.chainId,
-        token_network_registry_address: depsMock.contractsInfo.TokenNetworkRegistry.address,
-      },
-      operator: 'pfs operator',
-      payment_address: pfsAddress,
-      price_info: 2,
-      version: '0.4.1',
-    },
-    iou = {
-      sender: depsMock.address,
-      receiver: pfsAddress,
-      one_to_n_address: '0x0A0000000000000000000000000000000000000a' as Address,
-      chain_id: BigNumber.from(depsMock.network.chainId) as UInt<32>,
-      expiration_block: BigNumber.from(3232341) as UInt<32>,
-      amount: BigNumber.from(100) as UInt<32>,
-    } as IOU,
-    key = channelKey({ tokenNetwork, partner }),
-    action$ = new Subject<RaidenAction>(),
-    state$ = new Subject<RaidenState>();
-
-  let initialState!: RaidenState;
-  depsMock.latest$.pipe(pluckDistinct('state'), first()).subscribe((s) => (initialState = s));
-
-  action$.pipe(scan((s, a) => raidenReducer(s, a), initialState)).subscribe(state$);
-  getLatest$(action$, state$, depsMock).subscribe(depsMock.latest$);
-
-  depsMock.registryContract.token_to_token_networks.mockImplementation(async (_token) =>
-    _token === token ? tokenNetwork : AddressZero,
-  );
-
-  return {
-    token,
-    tokenNetwork,
-    partner,
-    target,
-    matrix,
-    channelId,
-    settleTimeout,
-    isFirstParticipant,
-    tokenContract,
-    tokenNetworkContract,
-    accessToken,
-    deviceId,
-    displayName,
-    partnerRoomId,
-    partnerUserId,
-    targetUserId,
-    matrixServer,
-    userId,
-    txHash,
-    state: initialState,
-    partnerSigner: wallet,
-    processed,
-    paymentId,
-    fee,
-    paths,
-    metadata,
-    pfsAddress,
-    pfsTokenAddress,
-    pfsInfoResponse,
-    iou,
-    key,
-    action$,
-    state$,
-  };
-}
 
 // fixture constants
 export const token = makeAddress();
@@ -325,6 +210,7 @@ export async function ensureChannelIsClosed([raiden, partner]: [
   );
   await waitBlock(closeBlock);
   await waitBlock(closeBlock + confirmationBlocks + 1); // confirmation
+  await sleep();
   if (raiden.started)
     assert(closedStates.includes(getChannel(raiden, partner)?.state), 'Raiden channel not closed');
   if (partner.started)
