080c932fe11bf3571e1caef844c77aa97de82986
Thore Weilbier <thore@weilbier.net>
2021-03-10 11:23:06 +0100

    devops: rework e2e environment script to help CI
    In our CI pipeline it is the goal to have the end-to-end test run
    outside of the end-to-end Docker to make the workflow more clean without
    redundancy of installation and build jobs. Rather the container runs in
    parallel to the main executor image, having access to all the provided
    services needed to the tests.
    The thereby resulting issue is that there is no direct access to the
    deployment information files, nor the container itself (as we do it for
    local end-to-end test runs). Thereby it is required to store the
    deployment information files outside the container permanently and put
    them under version control. This way the main executor in the CI
    pipeline will have direct access to them.
    This strategy requires and additional step for each new build of the
    Docker image to extract the files. Simply documenting workflows is
    always error prone. Therefore a new script has been added to do the
    whole build. As result of the strategy, also the script to run the test
    had to change.
    Both scripts share a lot in common of variables, functions and shell
    settings. So despite these are "just" bash scripts, the common parts got
    exported into a shared script that acts like a to import lirbray. This
    way the actual to run script remain clean and focused. Furthermore
    nothing is redundant which could lead to unexpected scenarios/errors.

--- e2e-environment/run-e2e-tests.sh
+++ e2e-environment/run-e2e-tests.sh
@@ -1,45 +1,38 @@
 #!/usr/bin/env bash
+# shellcheck disable=SC1090
 
 # Notes:
 #   - runs end-to-end test suite based on current working directory
 #   - all arguments passed to this script get forwarded to the test command.
 
-DOCKER_CONTAINER_NAME=lc-e2e
+SHARED_SCRIPT_PATH="$(dirname "${BASH_SOURCE[0]}")/shared-script.sh"
+source "$SHARED_SCRIPT_PATH"
 
-DEPLOYMENT_INFO_DIR=$(mktemp -d) # Use short argument syntax to support MacOS
+if [[ ! -d "$DEPLOYMENT_INFORMATION_DIRECTORY" ]]; then
+  extract_deployment_information
+else
+  echo -e "\nWARNING: Please make sure that the local deployment information always match with the used image."
+fi
 
-function finish() {
-  echo "Tearing down Docker container..."
-  docker stop $DOCKER_CONTAINER_NAME >/dev/null 2>&1
-}
-
-set -e
-trap finish EXIT
-
-echo "Starting the Docker container..."
+echo -e "\nStart the Docker container with the end-to-end environment"
 docker run --detach --rm \
-  --name $DOCKER_CONTAINER_NAME \
+  --name "$DOCKER_CONTAINER_NAME" \
   --publish 127.0.0.1:80:80 \
   --publish 127.0.0.1:5555:5555 \
   --publish 127.0.0.1:5001:5001 \
   --publish 127.0.0.1:5002:5002 \
   --publish 127.0.0.1:8545:8545 \
-  raidennetwork/lightclient-e2e-environment \
+  "$DOCKER_IMAGE_TAG_NAME" \
   >/dev/null
 
-sleep 5
+sleep 5s
 
-echo "Getting DEPLOYMENT_INFO from docker image '$DOCKER_CONTAINER_NAME' ..."
-docker cp "$DOCKER_CONTAINER_NAME":/opt/deployment/deployment_private_net.json "$DEPLOYMENT_INFO_DIR/"
-docker cp "$DOCKER_CONTAINER_NAME":/opt/deployment/deployment_services_private_net.json "$DEPLOYMENT_INFO_DIR/"
-docker cp "$DOCKER_CONTAINER_NAME":/etc/profile.d/smartcontracts.sh "$DEPLOYMENT_INFO_DIR/"
+export DEPLOYMENT_INFO="${DEPLOYMENT_INFORMATION_DIRECTORY}/deployment_private_net.json"
+export DEPLOYMENT_SERVICES_INFO="${DEPLOYMENT_INFORMATION_DIRECTORY}/deployment_services_private_net.json"
+source "${DEPLOYMENT_INFORMATION_DIRECTORY}/smartcontracts.sh"
 
-export DEPLOYMENT_INFO="${DEPLOYMENT_INFO_DIR}/deployment_private_net.json"
-export DEPLOYMENT_SERVICES_INFO="${DEPLOYMENT_INFO_DIR}/deployment_services_private_net.json"
-source "${DEPLOYMENT_INFO_DIR}/smartcontracts.sh"
-
-echo "Run end-to-end tests for $( basename $( realpath . ) )..."
+echo -e "\nRun the end-to-end tests for $( basename "$(pwd)")"
 yarn run test:e2e "$@"
 
-echo "Getting the service logs..."
+echo -e "\nGet the log files of the run services"
 docker cp "$DOCKER_CONTAINER_NAME":/var/log/supervisor/. ./logs/
