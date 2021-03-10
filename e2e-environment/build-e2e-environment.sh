d154089c741bc73ee86c989e5f7c8cd52348e2ce
Thore Weilbier <thore@weilbier.net>
2021-03-10 11:23:06 +0100

    devops: rework e2e environment script to help CI
    In our CI pipeline it is the goal to have the end-to-end test run
    outside of the end-to-end Docker to make the workflow more clean without
    redundancy of installation and build jobs. Rather the container runs in
    parallel to the main executor image, having access to all the provided
    services needed to the tests.
    The thereby resulting issue is that there is no direct access to the
    deployment information files, nor the container itself (as we do it for
    local end-to-end test runs). Thereby it is required to store the
    deployment information files outside the container permanently and put
    them under version control. This way the main executor in the CI
    pipeline will have direct access to them.
    This strategy requires and additional step for each new build of the
    Docker image to extract the files. Simply documenting workflows is
    always error prone. Therefore a new script has been added to do the
    whole build. As result of the strategy, also the script to run the test
    had to change.
    Both scripts share a lot in common of variables, functions and shell
    settings. So despite these are "just" bash scripts, the common parts got
    exported into a shared script that acts like a to import lirbray. This
    way the actual to run script remain clean and focused. Furthermore
    nothing is redundant which could lead to unexpected scenarios/errors.

---
+++ e2e-environment/build-e2e-environment.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC1090
+
+SHARED_SCRIPT_PATH="$(dirname "${BASH_SOURCE[0]}")/shared-script.sh"
+source "$SHARED_SCRIPT_PATH"
+
+echo -e "\nBuild the Docker image for the end-to-end environment"
+docker build --tag "$DOCKER_IMAGE_TAG_NAME" "$E2E_ENVIRONMENT_DIRECTORY"
+
+extract_deployment_information
+
+git add "$DEPLOYMENT_INFORMATION_DIRECTORY"
+echo -e "\nPlease commit the new deployment information (already staged)"
---
+++ e2e-environment/shared-script.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# This script includes shared variables, functions and shell settings for all
+# script related to the end-to-end environment. It is meant to be sourced at the
+# very beginning of each script.
+
+DOCKER_IMAGE_TAG_NAME="raidennetwork/lightclient-e2e-environment"
+DOCKER_CONTAINER_NAME="lc-e2e"
+E2E_ENVIRONMENT_DIRECTORY="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
+DEPLOYMENT_INFORMATION_DIRECTORY="${E2E_ENVIRONMENT_DIRECTORY}/deployment_information"
+
+function finish() {
+  echo -e "\nShut down the Docker container"
+  docker stop "$DOCKER_CONTAINER_NAME" >/dev/null 2>&1
+}
+
+function extract_deployment_information() {
+  echo -e "\nExtract deployment information from Docker image"
+
+  rm --recursive --force "$DEPLOYMENT_INFORMATION_DIRECTORY"
+  mkdir --parents "$DEPLOYMENT_INFORMATION_DIRECTORY"
+
+  docker run --detach --rm --name "$DOCKER_CONTAINER_NAME" "$DOCKER_IMAGE_TAG_NAME" >/dev/null
+  sleep 5s
+
+  docker cp "${DOCKER_CONTAINER_NAME}:/opt/deployment/deployment_private_net.json" "${DEPLOYMENT_INFORMATION_DIRECTORY}/"
+  docker cp "${DOCKER_CONTAINER_NAME}:/opt/deployment/deployment_services_private_net.json" "${DEPLOYMENT_INFORMATION_DIRECTORY}/"
+  docker cp "${DOCKER_CONTAINER_NAME}:/etc/profile.d/smartcontracts.sh" "${DEPLOYMENT_INFORMATION_DIRECTORY}/"
+   
+  docker stop "$DOCKER_CONTAINER_NAME" >/dev/null
+}
+
+set -e
+trap finish EXIT
