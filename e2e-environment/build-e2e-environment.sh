0847df15cc52e8fe35a04eb50a8d71870bf0c061
Thore Strassburg <thore@weilbier.net>
2021-03-17 17:54:20 +0100

    devops: make it hard to overwrite e2e Docker image
    A new build of the end-to-end environment Docker image should always get
    a new version tag. To make it hard to forget this, the script is
    checking if there is such image already locally and require to a
    `--force` parameter to overwrite it.

--- e2e-environment/build-e2e-environment.sh
+++ e2e-environment/build-e2e-environment.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC1090
+#
+# Parameters:
+#   --force   Overwrite already existing image locally with same version tag.
 
 SHARED_SCRIPT_PATH="$(dirname "${BASH_SOURCE[0]}")/shared-script.sh"
 source "$SHARED_SCRIPT_PATH"
+
+if docker_image_exists_locally && [[ $1 != "--force" ]]; then
+  echo "There is already an image locally for this repository and tag version!"
+  echo "If you want to build a new version of the image, please adopt the version tag first."
+  echo "If you want to overwrite the existing image, provide the '--force' flag to the script."
+  exit 1
+fi
 
 echo -e "\nBuild the Docker image for the end-to-end environment"
 docker build --tag "$DOCKER_IMAGE_NAME" "$E2E_ENVIRONMENT_DIRECTORY"
