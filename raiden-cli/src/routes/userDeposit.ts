fd1869718662fd5c8801a00bf2a581c588710d22
Thore Strassburg <thore@weilbier.net>
2021-03-26 15:19:25 +0100

    cli: add user deposit endpoint for withdraw plans
    This commits adds the single endpoint the Python client exposes for user
    deposit interactions. Though it is only a single endpoint, it provides
    three different interactions possibilities. Which interactions should be
    executed depends on the bodies payload.
    This approach by the Python client is not RESTful at all. Having to
    artificially split a single endpoint into multiple is kind of a
    nightmare. But we have to align with their API, so it is required.
    This commits handles in first case the challenge to provide this parsing
    and decision making. It was a high goal to keep the code very simply by
    having each function to deal with a single layer of abstraction only. As
    downside, these three endpoints into one bring some large overhead just
    to keep the route clean and readable.
    Though all interactions (deposit, plan withdraw and withdraw) are fully
    working, their implementation is only rudimentary. This means that if
    anything is wrong with the parameters or similar, we will just response
    with an internal server error including the detailed message. This will
    be improved in the next commits, catching user mistakes early to provide
    diverse and meaningful error codes and messages (according to the
    API specification). To keep the commits reasonably small and focused
    this is a wrap here.
    
    There is at the moment the issue that we have no direct way to respond
    the user with the block number when a withdraw plan is ready. There is a
    hacky solution implemented that kinda works. But we need to clarify how
    the SDK can provide this information to the CLI.

--- raiden-cli/src/routes/userDeposit.ts
+++ raiden-cli/src/routes/userDeposit.ts
@@ -1,15 +1,119 @@
+import { BigNumber } from 'ethers';
 import type { Request, Response } from 'express';
 import { Router } from 'express';
 
-import type { Cli } from '../types';
+import { ErrorCodes } from 'raiden-ts';
 
-async function getUDCWithdrawPlan(this: Cli, _request: Request, response: Response) {
+import type { Cli } from '../types';
+import { isInsuficientFundsError, isInvalidParameterError } from '../utils/validation';
+
+type DepositResponse = { transaction_hash: string };
+type PlanWithdrawResponse = { transaction_hash: string; planned_withdraw_block_number: number };
+type WithdrawResponse = { transaction_hash: string };
+
+function getTotalDepositAmount(request: Request): string | undefined {
+  return request.body.total_deposit?.toString();
+}
+
+function getPlannedWithdrawAmount(request: Request): string | undefined {
+  return request.body.planned_withdraw_amount?.toString();
+}
+
+function getWithdrawAmount(request: Request): string | undefined {
+  return request.body.withdraw_amount?.toString();
+}
+
+function shouldDeposit(request: Request): boolean {
+  return (
+    getTotalDepositAmount(request) !== undefined &&
+    getPlannedWithdrawAmount(request) === undefined &&
+    getWithdrawAmount(request) === undefined
+  );
+}
+
+function shouldPlanWithdraw(request: Request): boolean {
+  return (
+    getTotalDepositAmount(request) === undefined &&
+    getPlannedWithdrawAmount(request) !== undefined &&
+    getWithdrawAmount(request) === undefined
+  );
+}
+
+function shouldWithdraw(request: Request): boolean {
+  return (
+    getTotalDepositAmount(request) === undefined &&
+    getPlannedWithdrawAmount(request) === undefined &&
+    getWithdrawAmount(request) !== undefined
+  );
+}
+
+async function deposit(this: Cli, totalDeposit: string): Promise<DepositResponse> {
+  const totalDepositTarget = BigNumber.from(totalDeposit); // TODO: handle error else 500;
+  const currentTotalDeposit = await this.raiden.getUDCTotalDeposit();
+  const depositDifference = totalDepositTarget.sub(currentTotalDeposit); // TODO: greater zero else 500!
+  const transactionHash = await this.raiden.depositToUDC(depositDifference);
+  return { transaction_hash: transactionHash };
+}
+
+async function planWithdraw(this: Cli, amount: string): Promise<PlanWithdrawResponse> {
+  const transactionHash = await this.raiden.planUDCWithdraw(amount);
+  const plannedWithdrawBlockNumber = (await this.raiden.getBlockNumber()) + 95; // TODO: !!!
+  return {
+    transaction_hash: transactionHash,
+    planned_withdraw_block_number: plannedWithdrawBlockNumber,
+  };
+}
+
+async function withdraw(this: Cli, amount: string): Promise<WithdrawResponse> {
+  const transactionHash = await this.raiden.withdrawFromUDC(amount);
+  return { transaction_hash: transactionHash };
+}
+
+async function determineAndExecuteRequestedInteraction(
+  this: Cli,
+  request: Request,
+): Promise<DepositResponse | PlanWithdrawResponse | WithdrawResponse> {
+  if (shouldDeposit(request)) {
+    const totalDeposit = getTotalDepositAmount(request)!; // Can't be undefined here.
+    return await deposit.call(this, totalDeposit);
+  } else if (shouldPlanWithdraw(request)) {
+    const amount = getPlannedWithdrawAmount(request)!; // Can't be undefined here.
+    return await planWithdraw.call(this, amount);
+  } else if (shouldWithdraw(request)) {
+    const amount = getWithdrawAmount(request)!; // Can't be undefined here.
+    return await withdraw.call(this, amount);
+  } else {
+    // TODO: This is not the actual error...
+    throw new Error(ErrorCodes.DTA_UNENCODABLE_DATA);
+  }
+}
+
+async function getUDCWithdrawPlan(
+  this: Cli,
+  _request: Request,
+  response: Response,
+): Promise<void> {
   const withdrawPlan = await this.raiden.getUDCWithdrawPlan();
 
   if (withdrawPlan !== undefined) {
     response.json({ ...withdrawPlan, amount: withdrawPlan.amount.toString() });
   } else {
     response.send();
+  }
+}
+
+async function interactWithUDC(this: Cli, request: Request, response: Response): Promise<void> {
+  try {
+    const interactionResponse = await determineAndExecuteRequestedInteraction.call(this, request);
+    response.json(interactionResponse);
+  } catch (error) {
+    if (isInvalidParameterError(error)) {
+      response.status(409).send(error.message);
+    } else if (isInsuficientFundsError(error)) {
+      response.status(402).send(error.message);
+    } else {
+      response.status(500).send(error.message);
+    }
   }
 }
 
@@ -21,6 +125,7 @@ export function makeUserDepositRouter(this: Cli): Router {
   const router = Router();
 
   router.get('/withdraw_plan', getUDCWithdrawPlan.bind(this));
+  router.post('/', interactWithUDC.bind(this));
 
   return router;
 }
