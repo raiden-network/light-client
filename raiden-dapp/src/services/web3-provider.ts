fa57399f7eebf3a1b68ac57e541e5c388c81c000
Thore Strassburg <thore@weilbier.net>
2021-04-22 13:59:07 +0200

    dapp: add WalletConnect as last resort provider

--- raiden-dapp/src/services/web3-provider.ts
+++ raiden-dapp/src/services/web3-provider.ts
@@ -1,3 +1,6 @@
+import WalletConnectProvider from '@walletconnect/web3-provider';
+import { providers } from 'ethers';
+
 export class Web3Provider {
   static async provider(rpcEndpoint?: string) {
     let provider = null;
@@ -14,10 +17,17 @@ export class Web3Provider {
       provider = window.ethereum;
     } else if (window.web3) {
       provider = window.web3.currentProvider;
+    } else {
+      const provider = new WalletConnectProvider({
+        infuraId: '1d7828db440547969591f9ef1f81a04d', // TODO: Replace with configuration value
+      });
+      await provider.enable();
+      return new providers.Web3Provider(provider);
     }
 
     /* istanbul ignore next */
     if (provider && provider.isMetaMask) {
+      // TODO: do the same for WalletConnect
       provider.on('chainChanged', () => window.location.replace(window.location.origin));
     }
 
