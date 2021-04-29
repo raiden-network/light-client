3cab36b996319f12d6dc06cd6e5a777715e61b5e
Thore Strassburg <thore@weilbier.net>
2021-04-29 15:44:59 +0200

    dapp: add reset connection button
    When a the establishment of the connection got stuck, the last resort is
    to reset the connection. This is especially the case for the
    WalletConnect provider as here the local session state mut be cleared.
    Else after the reload of the dApp it would pick up the old session
    again.

--- raiden-dapp/src/components/dialogs/ConnectionPendingDialog.vue
+++ raiden-dapp/src/components/dialogs/ConnectionPendingDialog.vue
@@ -8,12 +8,21 @@
       <spinner :size="60" class="my-4" />
       {{ $t('home.connection-pending-dialog.text') }}
     </v-card-text>
+
+    <v-card-actions>
+      <action-button
+        :text="$t('home.connection-pending-dialog.button')"
+        :enabled="true"
+        @click="resetConnection"
+      />
+    </v-card-actions>
   </raiden-dialog>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+import { Component, Emit, Vue } from 'vue-property-decorator';
 
+import ActionButton from '@/components/ActionButton.vue';
 import RaidenDialog from '@/components/dialogs/RaidenDialog.vue';
 import Spinner from '@/components/icons/Spinner.vue';
 
@@ -25,9 +34,14 @@ const SHOW_DIALOG_TIMEOUT = 7000;
   effect. Therefore this alternative approach of using the life-cycle hook and
   a timer is necessary.
 */
-@Component({ components: { RaidenDialog, Spinner } })
+@Component({ components: { ActionButton, RaidenDialog, Spinner } })
 export default class ConnectionPendingDialog extends Vue {
   dialogVisible = false;
+
+  @Emit('reset-connection')
+  resetConnection(): void {
+    // pass
+  }
 
   mounted() {
     this.delayVisibilityOfDialog();
