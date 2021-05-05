d447cc9dd168f45a582a13b2800a3e3580c1ab00
Thore Strassburg <thore@weilbier.net>
2021-05-05 16:57:09 +0200

    dapp: add infura id to wallet connect options

--- raiden-dapp/src/services/ethereum-connection/wallet-connect.ts
+++ raiden-dapp/src/services/ethereum-connection/wallet-connect.ts
@@ -13,21 +13,44 @@ export class WalletConnect extends EthereumConnection {
     this.provider = new providers.Web3Provider(walletConnectProvider);
   }
 
-  public static async connect(options: { rpcUrl: string }): Promise<WalletConnect> {
-    const temporaryJsonRpcProvider = new providers.JsonRpcProvider(options.rpcUrl);
-    const networkOfProvider = await temporaryJsonRpcProvider.getNetwork();
-    const walletConnectProvider = new WalletConnectProvider({
-      rpc: {
-        [networkOfProvider.chainId]: options.rpcUrl,
-      },
-    });
+  public static async connect(options: {
+    rpcUrl?: string;
+    infuraId?: string;
+  }): Promise<WalletConnect> {
+    let walletConnectProvider: WalletConnectProvider;
 
-    await walletConnectProvider.enable();
-    walletConnectProvider.on('chainChanged', resetHandler);
-    walletConnectProvider.on('disconnect', resetHandler);
+    if (options.rpcUrl === undefined && options.infuraId === undefined) {
+      throw new Error('One of the options RPC URL or Infura Id are required to connect.');
+    } else if (options.rpcUrl !== undefined && options.infuraId !== undefined) {
+      throw new Error('Only one connetion option allowed. Either a RPC URL or a Infura Id.');
+    } else if (options.rpcUrl !== undefined) {
+      walletConnectProvider = await getWalletConnectProviderWithRpcUrl(options.rpcUrl);
+    } else if (options.infuraId) {
+      walletConnectProvider = getWalletConnectProviderWithInfuraId(options.infuraId);
+    }
 
-    return new this(walletConnectProvider);
+    // The provider instance must be available here, though TypeScript can't see it.
+    await walletConnectProvider!.enable();
+    walletConnectProvider!.on('chainChanged', resetHandler);
+    walletConnectProvider!.on('disconnect', resetHandler);
+
+    return new this(walletConnectProvider!);
   }
+}
+
+async function getWalletConnectProviderWithRpcUrl(rpcUrl: string): Promise<WalletConnectProvider> {
+  const temporaryJsonRpcProvider = new providers.JsonRpcProvider(rpcUrl);
+  const networkOfProvider = await temporaryJsonRpcProvider.getNetwork();
+  const options = {
+    rpc: {
+      [networkOfProvider.chainId]: rpcUrl,
+    },
+  };
+  return new WalletConnectProvider(options);
+}
+
+function getWalletConnectProviderWithInfuraId(infuraId: string): WalletConnectProvider {
+  return new WalletConnectProvider({ infuraId });
 }
 
 function resetHandler() {
--- raiden-dapp/tests/unit/services/ethereum-connection/wallet-connect.spec.ts
+++ raiden-dapp/tests/unit/services/ethereum-connection/wallet-connect.spec.ts
@@ -19,8 +19,27 @@ jest.mock('ethers', () => {
 });
 
 describe('WalletConnect', () => {
-  test('it can connect', async () => {
+  test('fail to connect when none of the options is provided', () => {
+    expect(WalletConnect.connect({})).rejects.toThrow(
+      'One of the options RPC URL or Infura Id are required to connect.',
+    );
+  });
+
+  test('can connect with a RPC URL', async () => {
     await WalletConnect.connect({ rpcUrl: 'https://some.rpc.url' });
+  });
+
+  test('can connect with an Infura ID', async () => {
+    await WalletConnect.connect({ infuraId: '6d333faba41b4c3d8ae979417e281832' });
+  });
+
+  test('fail to connect when multiple options are provided', () => {
+    expect(
+      WalletConnect.connect({
+        rpcUrl: 'https://some.rpc.url',
+        infuraId: '6d333faba41b4c3d8ae979417e281832',
+      }),
+    ).rejects.toThrow('One of the options RPC URL or Infura Id are required to connect.');
   });
 
   // TODO: we need more tests to get details like that the chain ID got
