286b21eda2a12bfe49f27153b0c723390dc0f6dd
Thore Strassburg <thore@weilbier.net>
2021-05-06 15:12:18 +0200

    dapp: fix wrong snake case formatting

--- raiden-dapp/src/services/ethereum-connection/wallet-connect.ts
+++ raiden-dapp/src/services/ethereum-connection/wallet-connect.ts
@@ -4,7 +4,7 @@ import { providers } from 'ethers';
 import { EthereumConnection } from './types';
 
 export class WalletConnect extends EthereumConnection {
-  public static readonly connection_name = 'wallet_connect';
+  public static readonly connectionName = 'wallet_connect';
   public static readonly isAvailable = true;
   public readonly provider: providers.JsonRpcProvider;
   public readonly account = 0; // Refers to the currently selected account in the wallet.
