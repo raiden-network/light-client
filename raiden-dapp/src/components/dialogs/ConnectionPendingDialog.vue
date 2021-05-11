7143481742183026e9e38e039dfb9ec566160789
Thore Strassburg <thore@weilbier.net>
2021-05-11 15:02:13 +0200

    dapp: remove delay from connection pending dialog
    The dialog gets know only shown once the provider link got established.
    Therefore it is fine and also way simpler to show this dialog right
    away. Having the delay feels awkward with the new setup.

--- raiden-dapp/src/components/dialogs/ConnectionPendingDialog.vue
+++ raiden-dapp/src/components/dialogs/ConnectionPendingDialog.vue
@@ -1,5 +1,5 @@
 <template>
-  <raiden-dialog :visible="dialogVisible" :hide-close="true">
+  <raiden-dialog :visible="true" :hide-close="true">
     <v-card-title>
       {{ $t('connection-manager.dialogs.connection-pending.title') }}
     </v-card-title>
@@ -26,33 +26,11 @@ import ActionButton from '@/components/ActionButton.vue';
 import RaidenDialog from '@/components/dialogs/RaidenDialog.vue';
 import Spinner from '@/components/icons/Spinner.vue';
 
-const SHOW_DIALOG_TIMEOUT = 7000;
-
-/*
-  Please note that because the underlying dialog component by Vuetify, it is not
-  possible to implement the delayed rendering with a Vue transition or pure CSS
-  effect. Therefore this alternative approach of using the life-cycle hook and
-  a timer is necessary.
-*/
 @Component({ components: { ActionButton, RaidenDialog, Spinner } })
 export default class ConnectionPendingDialog extends Vue {
-  dialogVisible = false;
-
   @Emit('reset-connection')
   resetConnection(): void {
     // pass
-  }
-
-  mounted() {
-    this.delayVisibilityOfDialog();
-  }
-
-  delayVisibilityOfDialog(): void {
-    setTimeout(this.showDialog, SHOW_DIALOG_TIMEOUT);
-  }
-
-  showDialog(): void {
-    this.dialogVisible = true;
   }
 }
 </script>
