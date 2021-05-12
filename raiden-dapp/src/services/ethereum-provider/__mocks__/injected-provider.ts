53602b71701ed0a42ef891052186898559e4f4a3
Thore Strassburg <thore@weilbier.net>
2021-05-12 17:38:14 +0200

    dapp: disable not available provider in manager

--- raiden-dapp/src/services/ethereum-provider/__mocks__/injected-provider.ts
+++ raiden-dapp/src/services/ethereum-provider/__mocks__/injected-provider.ts
@@ -2,5 +2,9 @@ export class InjectedProvider {
   public static readonly providerName = 'injected_provider_mock';
   public readonly account = 0;
 
+  public static get isAvailable() {
+    return true;
+  }
+
   public static link = jest.fn().mockResolvedValue(undefined);
 }
