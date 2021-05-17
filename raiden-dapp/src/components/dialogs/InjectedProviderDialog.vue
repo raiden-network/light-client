b7bead4390c77650000e10eab293e091c15f6b0e
Thore Strassburg <thore@weilbier.net>
2021-05-17 16:12:17 +0200

    dapp: add ethereum provider base dialog component
    This again exports commonly shared code from the dialogs to configure
    and link Ethereun providers. This makes more of the dialogs re-usable
    and make them more focused on their responsive providers. The same goes
    for the tests.

--- raiden-dapp/src/components/dialogs/InjectedProviderDialog.vue
+++ raiden-dapp/src/components/dialogs/InjectedProviderDialog.vue
@@ -1,50 +1,23 @@
 <template>
-  <raiden-dialog width="472" class="injected-provider" :visible="true" @close="emitCancel">
-    <v-card-title>
-      {{ $t('connection-manager.dialogs.wallet-connect-provider.header') }}
-    </v-card-title>
-
-    <v-card-text>
-      <spinner v-if="linkingInProgress" />
-
-      <v-alert
-        v-if="linkingFailed"
-        class="injected-provider__error-message text-left font-weight-light"
-        color="error"
-        icon="warning"
-      >
-        {{ $t('connection-manager.dialogs.injected-provider.error-message') }}
-      </v-alert>
-    </v-card-text>
-
-    <v-card-actions>
-      <action-button
-        :enabled="!linkingInProgress"
-        class="injected-provider__link-button"
-        :text="$t('connection-manager.dialogs.injected-provider.link-button')"
-        width="200px"
-        @click="link"
-      />
-    </v-card-actions>
-  </raiden-dialog>
+  <ethereum-provider-base-dialog
+    :header="$t('connection-manager.dialogs.injected-provider.header')"
+    :can-link="canLink"
+    :linking-in-progress="linkingInProgress"
+    :linking-failed="linkingFailed"
+    :error-message="$t('connection-manager.dialogs.injected-provider.error-message')"
+    @link="link"
+    @cancel="emitCancel"
+  />
 </template>
 
 <script lang="ts">
 import { Component, Mixins } from 'vue-property-decorator';
 
-import ActionButton from '@/components/ActionButton.vue';
-import RaidenDialog from '@/components/dialogs/RaidenDialog.vue';
-import Spinner from '@/components/icons/Spinner.vue';
+import EthereumProviderBaseDialog from '@/components/dialogs/EthereumProviderBaseDialog.vue';
 import EthereumProviderDialogMixin from '@/mixins/ethereum-provider-dialog-mixin';
 import { InjectedProvider } from '@/services/ethereum-provider';
 
-@Component({
-  components: {
-    ActionButton,
-    RaidenDialog,
-    Spinner,
-  },
-})
+@Component({ components: { EthereumProviderBaseDialog } })
 export default class InjectedProviderDialog extends Mixins(EthereumProviderDialogMixin) {
   providerFactory = InjectedProvider;
 }
---
+++ raiden-dapp/tests/unit/components/dialogs/ethereum-provider-base-dialog.spec.ts
@@ -0,0 +1,99 @@
+import { $t } from '../../utils/mocks';
+
+import type { Wrapper } from '@vue/test-utils';
+import { mount } from '@vue/test-utils';
+import Vue from 'vue';
+import Vuetify from 'vuetify';
+
+import ActionButton from '@/components/ActionButton.vue';
+import EthereumProviderBaseDialog from '@/components/dialogs/EthereumProviderBaseDialog.vue';
+import Spinner from '@/components/icons/Spinner.vue';
+
+Vue.use(Vuetify);
+
+const vuetify = new Vuetify();
+
+function createWrapper(options?: {
+  header?: string;
+  canLink?: boolean;
+  linkingInProgress?: boolean;
+  linkingFailed?: boolean;
+  errorMessage?: string;
+}): Wrapper<EthereumProviderBaseDialog> {
+  return mount(EthereumProviderBaseDialog, {
+    vuetify,
+    propsData: {
+      header: options?.header ?? 'header',
+      canLink: options?.canLink ?? true,
+      linkingInProgress: options?.linkingInProgress ?? false,
+      linkingFailed: options?.linkingFailed ?? false,
+      errorMessage: options?.errorMessage ?? 'error',
+    },
+    stubs: { 'v-dialog': true },
+    mocks: { $t },
+  });
+}
+
+describe('EthereumProviderBaseDialog.vue', () => {
+  test('shows header', () => {
+    const wrapper = createWrapper({ header: 'testHeader' });
+    const header = wrapper.find('.ethereum-provider-base-dialog__header');
+
+    expect(header.isVisible()).toBeTruthy();
+    expect(header.text()).toMatch('testHeader');
+  });
+
+  test('shows spinner if in progress', () => {
+    const wrapper = createWrapper({ linkingInProgress: true });
+    const spinner = wrapper.findComponent(Spinner);
+
+    expect(spinner.isVisible()).toBeTruthy();
+  });
+
+  test('shows given error message when linking failed', () => {
+    const wrapper = createWrapper({ linkingFailed: true, errorMessage: 'failed' });
+    const error = wrapper.find('.ethereum-provider-base-dialog__error');
+
+    expect(error.isVisible()).toBeTruthy();
+    expect(error.text()).toMatch('failed');
+  });
+
+  test('shows button to link', () => {
+    const wrapper = createWrapper({ linkingInProgress: true });
+    const button = wrapper.findComponent(ActionButton).find('button');
+
+    expect(button.isVisible()).toBeTruthy();
+    expect(button.text()).toBe('connection-manager.dialogs.base.link-button');
+  });
+
+  test('button is disabled when linking in progress', () => {
+    const wrapper = createWrapper({ linkingInProgress: true });
+    const button = wrapper.findComponent(ActionButton).find('button');
+
+    expect(button.attributes('disabled')).toBeTruthy();
+  });
+
+  test('button is disabled when linking not possible', () => {
+    const wrapper = createWrapper({ canLink: false });
+    const button = wrapper.findComponent(ActionButton).find('button');
+
+    expect(button.attributes('disabled')).toBeTruthy();
+  });
+
+  test('button is enabled when linking possible and not in progress', () => {
+    const wrapper = createWrapper({ canLink: true, linkingInProgress: false });
+    const button = wrapper.findComponent(ActionButton).find('button');
+
+    expect(button.attributes('disabled')).toBeFalsy();
+  });
+
+  test('button emits link event on click', () => {
+    const wrapper = createWrapper();
+    const button = wrapper.findComponent(ActionButton).find('button');
+    expect(wrapper.emitted('link')).toBeUndefined();
+
+    button.trigger('click');
+
+    expect(wrapper.emitted('link')?.length).toBe(1);
+  });
+});
