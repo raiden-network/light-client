1d21352ebb5863fdae0d0d9c5aaca8aecf19899c
Thore Strassburg <thore@weilbier.net>
2021-05-17 16:12:17 +0200

    dapp: add ethereum provider base dialog component
    This again exports commonly shared code from the dialogs to configure
    and link Ethereun providers. This makes more of the dialogs re-usable
    and make them more focused on their responsive providers. The same goes
    for the tests.

---
+++ raiden-dapp/src/components/dialogs/EthereumProviderBaseDialog.vue
@@ -0,0 +1,80 @@
+<template>
+  <raiden-dialog
+    class="ethereum-provider-base-dialog"
+    width="472"
+    :visible="true"
+    @close="emitCancel"
+  >
+    <v-card-title class="ethereum-provider-base-dialog__header">{{ header }}</v-card-title>
+
+    <v-card-text>
+      <slot v-if="!linkingInProgress" />
+
+      <spinner v-if="linkingInProgress" />
+
+      <v-alert
+        v-if="linkingFailed"
+        class="ethereum-provider-base-dialog__error text-left font-weight-light"
+        color="error"
+        icon="warning"
+      >
+        {{ errorMessage }}
+      </v-alert>
+    </v-card-text>
+
+    <v-card-actions>
+      <action-button
+        :enabled="buttonEnabled"
+        :text="$t('connection-manager.dialogs.base.link-button')"
+        width="200px"
+        @click="emitLink"
+      />
+    </v-card-actions>
+  </raiden-dialog>
+</template>
+
+<script lang="ts">
+import { Component, Emit, Prop, Vue } from 'vue-property-decorator';
+
+import ActionButton from '@/components/ActionButton.vue';
+import RaidenDialog from '@/components/dialogs/RaidenDialog.vue';
+import Spinner from '@/components/icons/Spinner.vue';
+
+@Component({
+  components: {
+    ActionButton,
+    RaidenDialog,
+    Spinner,
+  },
+})
+export default class EthereumProviderBaseDialog extends Vue {
+  @Prop({ type: String, required: true })
+  header!: string;
+
+  @Prop({ type: Boolean, required: true })
+  canLink!: boolean;
+
+  @Prop({ type: Boolean, required: true })
+  linkingInProgress!: boolean;
+
+  @Prop({ type: Boolean, required: true })
+  linkingFailed!: boolean;
+
+  @Prop({ type: String, required: true })
+  errorMessage!: string;
+
+  get buttonEnabled(): boolean {
+    return this.canLink && !this.linkingInProgress;
+  }
+
+  @Emit('cancel')
+  emitCancel(): void {
+    // pass
+  }
+
+  @Emit('link')
+  emitLink(): void {
+    // pass
+  }
+}
+</script>
--- raiden-dapp/src/components/dialogs/WalletConnectProviderDialog.vue
+++ raiden-dapp/src/components/dialogs/WalletConnectProviderDialog.vue
@@ -1,83 +1,63 @@
 <template>
-  <raiden-dialog width="472" class="wallet-connect-provider" :visible="true" @close="emitCancel">
-    <v-card-title>
-      {{ $t('connection-manager.dialogs.wallet-connect-provider.header') }}
-    </v-card-title>
+  <ethereum-provider-base-dialog
+    :header="$t('connection-manager.dialogs.wallet-connect-provider.header')"
+    :can-link="canLink"
+    :linking-in-progress="linkingInProgress"
+    :linking-failed="linkingFailed"
+    :error-message="$t('connection-manager.dialogs.wallet-connect-provider.error-message')"
+    @link="link"
+    @cancel="emitCancel"
+  >
+    <text-input-with-toggle
+      v-model="bridgeUrl"
+      class="wallet-connect-provider__options__bridge-url"
+      :name="$t('connection-manager.dialogs.wallet-connect-provider.options.bridge-url.name')"
+      :details="
+        $t('connection-manager.dialogs.wallet-connect-provider.options.bridge-url.details')
+      "
+      :placeholder="
+        $t('connection-manager.dialogs.wallet-connect-provider.options.bridge-url.placeholder')
+      "
+      optional
+    />
 
-    <v-card-text>
-      <text-input-with-toggle
-        v-model="bridgeUrl"
-        class="wallet-connect-provider__options__bridge-url"
-        :name="$t('connection-manager.dialogs.wallet-connect-provider.options.bridge-url.name')"
-        :details="
-          $t('connection-manager.dialogs.wallet-connect-provider.options.bridge-url.details')
-        "
-        :placeholder="
-          $t('connection-manager.dialogs.wallet-connect-provider.options.bridge-url.placeholder')
-        "
-        optional
-      />
+    <v-btn-toggle mandatory>
+      <v-btn class="wallet-connect-provider__option-toggle-button" @click="showInfuraIdOption">
+        {{ $t('connection-manager.dialogs.wallet-connect-provider.options.infura-id.name') }}
+      </v-btn>
+      <v-btn class="wallet-connect-provider__option-toggle-button" @click="showRpcUrlOption">
+        {{ $t('connection-manager.dialogs.wallet-connect-provider.options.rpc-url.name') }}
+      </v-btn>
+    </v-btn-toggle>
 
-      <v-btn-toggle mandatory>
-        <v-btn class="wallet-connect-provider__option-toggle-button" @click="showInfuraIdOption">
-          {{ $t('connection-manager.dialogs.wallet-connect-provider.options.infura-id.name') }}
-        </v-btn>
-        <v-btn class="wallet-connect-provider__option-toggle-button" @click="showRpcUrlOption">
-          {{ $t('connection-manager.dialogs.wallet-connect-provider.options.rpc-url.name') }}
-        </v-btn>
-      </v-btn-toggle>
+    <text-input-with-toggle
+      v-if="infuraIdOptionVisible"
+      v-model="infuraId"
+      class="wallet-connect-provider__options__infura-id"
+      :name="$t('connection-manager.dialogs.wallet-connect-provider.options.infura-id.name')"
+      :details="$t('connection-manager.dialogs.wallet-connect-provider.options.infura-id.details')"
+      :placeholder="
+        $t('connection-manager.dialogs.wallet-connect-provider.options.infura-id.placeholder')
+      "
+    />
 
-      <text-input-with-toggle
-        v-if="infuraIdOptionVisible"
-        v-model="infuraId"
-        class="wallet-connect-provider__options__infura-id"
-        :name="$t('connection-manager.dialogs.wallet-connect-provider.options.infura-id.name')"
-        :details="
-          $t('connection-manager.dialogs.wallet-connect-provider.options.infura-id.details')
-        "
-        :placeholder="
-          $t('connection-manager.dialogs.wallet-connect-provider.options.infura-id.placeholder')
-        "
-      />
-
-      <text-input-with-toggle
-        v-if="rpcUrlOptionVisible"
-        v-model="rpcUrl"
-        class="wallet-connect-provider__options__rpc-url"
-        :name="$t('connection-manager.dialogs.wallet-connect-provider.options.rpc-url.name')"
-        :details="$t('connection-manager.dialogs.wallet-connect-provider.options.rpc-url.details')"
-        :placeholder="
-          $t('connection-manager.dialogs.wallet-connect-provider.options.rpc-url.placeholder')
-        "
-      />
-
-      <v-alert
-        v-if="linkingFailed"
-        class="wallet-connect-provider__error-message text-left font-weight-light"
-        color="error"
-        icon="warning"
-      >
-        {{ $t('connection-manager.dialogs.wallet-connect-provider.error-message') }}
-      </v-alert>
-    </v-card-text>
-
-    <v-card-actions>
-      <action-button
-        :enabled="canLink"
-        class="wallet-connect-provider__link-button"
-        :text="$t('connection-manager.dialogs.wallet-connect-provider.link-button')"
-        width="200px"
-        @click="link"
-      />
-    </v-card-actions>
-  </raiden-dialog>
+    <text-input-with-toggle
+      v-if="rpcUrlOptionVisible"
+      v-model="rpcUrl"
+      class="wallet-connect-provider__options__rpc-url"
+      :name="$t('connection-manager.dialogs.wallet-connect-provider.options.rpc-url.name')"
+      :details="$t('connection-manager.dialogs.wallet-connect-provider.options.rpc-url.details')"
+      :placeholder="
+        $t('connection-manager.dialogs.wallet-connect-provider.options.rpc-url.placeholder')
+      "
+    />
+  </ethereum-provider-base-dialog>
 </template>
 
 <script lang="ts">
 import { Component, Mixins } from 'vue-property-decorator';
 
-import ActionButton from '@/components/ActionButton.vue';
-import RaidenDialog from '@/components/dialogs/RaidenDialog.vue';
+import EthereumProviderBaseDialog from '@/components/dialogs/EthereumProviderBaseDialog.vue';
 import TextInputWithToggle from '@/components/TextInputWithToggle.vue';
 import EthereumProviderDialogMixin from '@/mixins/ethereum-provider-dialog-mixin';
 import { WalletConnectProvider } from '@/services/ethereum-provider';
@@ -91,9 +71,8 @@ type WalletConnectProviderOptions = Parameters<typeof WalletConnectProvider.link
 
 @Component({
   components: {
-    ActionButton,
+    EthereumProviderBaseDialog,
     TextInputWithToggle,
-    RaidenDialog,
   },
 })
 export default class WalletConnectProviderDialog extends Mixins(EthereumProviderDialogMixin) {
