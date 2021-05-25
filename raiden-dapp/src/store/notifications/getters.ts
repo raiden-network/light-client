6b5199d5adfdcbd0f6e155ef1149848d4d88b385
Thore Strassburg <thore@weilbier.net>
2021-05-25 12:00:55 +0200

    dapp: make sure notification queue is sorted
    The notification queue which is used to display snackbars to the user
    should be sorted. Else, if this property would not be guaranteed, it
    could be that the user sees the notifications in a wrong order. The
    list used for the notification panel was already sorted. Therefore it
    was simple to share this feature between both store getters.

--- raiden-dapp/src/store/notifications/getters.ts
+++ raiden-dapp/src/store/notifications/getters.ts
@@ -4,24 +4,25 @@ import { NotificationImportance } from '@/store/notifications/notification-impor
 import type { NotificationPayload, NotificationsState } from '@/store/notifications/types';
 import type { RootState } from '@/types';
 
+function compareById(a: NotificationPayload, b: NotificationPayload): number {
+  return b.id - a.id;
+}
+
 type Getters = {
   notifications(state: NotificationsState): NotificationPayload[];
-  nextNotificationId({ notifications }: NotificationsState): number;
   notificationQueue(state: NotificationsState): NotificationPayload[];
 };
 
 export const getters: GetterTree<NotificationsState, RootState> & Getters = {
   notifications: (state: NotificationsState): NotificationPayload[] => {
-    return Object.values(state.notifications).sort((a, b) => b.id - a.id);
-  },
-  nextNotificationId: ({ notifications }: NotificationsState): number => {
-    const id = Object.values(notifications).map((notification) => notification.id);
-    return Math.max(...id, 0) + 1;
+    return Object.values(state.notifications).sort(compareById);
   },
   notificationQueue: (state: NotificationsState): NotificationPayload[] => {
-    return Object.values(state.notifications).filter(
-      (notification) =>
-        notification.importance === NotificationImportance.HIGH && notification.display === true,
-    );
+    return Object.values(state.notifications)
+      .filter(
+        (notification) =>
+          notification.importance === NotificationImportance.HIGH && notification.display === true,
+      )
+      .sort(compareById);
   },
 };
