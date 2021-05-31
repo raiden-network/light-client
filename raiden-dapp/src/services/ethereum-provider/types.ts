df4747c065918a52a27bd402763209ab8e53c1a8
Thore Strassburg <thore@weilbier.net>
2021-05-31 15:23:33 +0200

    dapp: allow providers to be disabled by config
    The administrator of a dApp server should be in charge to disable specific
    ethereum providers. So do we aim for disabling the direct RPC provider in our
    publicly hosted version due to security issues. In contrast to not available
    providers they should be completely hidden in the UI.
    To achieve this, the configuration files got extended to include such data.
    Then the EthereumProvider base class implements a function to check if the
    current provider is listed or not. As result providers are enabled per default.
    Note that the availability of a provider has to be distinguished from this. The
    availability refers to technical inspection of the environment the dApp runs
    inside.
    It is not really possible to integrate the disabled property of a provider into
    the constructor to make it fail like for the availability. The root cause for
    this is the asynchronous reading of the configuration file. Though as the
    disabled property is not a technical limitation this should be fine. It is
    intended to be used to conditionally render providers in the UI to give the
    user access to it or not.

--- raiden-dapp/src/services/ethereum-provider/types.ts
+++ raiden-dapp/src/services/ethereum-provider/types.ts
@@ -1,5 +1,7 @@
 import type { providers } from 'ethers';
 
+import { ConfigProvider } from '@/services/config-provider';
+
 // The type evaluation for static class members works slightly different for
 // the moment. Thereby it is not possible to have any better type restrictions
 // here. Though having the named type here in place will allows us to adopt it
@@ -9,6 +11,7 @@ export type EthereumProviderOptions = any; // eslint-disable-line @typescript-es
 export interface EthereumProviderFactory {
   providerName: string;
   isAvailable: boolean;
+  isDisabled: () => Promise<boolean>;
   link: (options: EthereumProviderOptions) => Promise<EthereumProvider>;
 }
 
@@ -27,5 +30,11 @@ export abstract class EthereumProvider {
     if (!isAvailable) {
       throw new Error('The provider is not available.');
     }
+  }
+
+  static async isDisabled(): Promise<boolean> {
+    const configuration = await ConfigProvider.configuration();
+    const disabled_ethereum_providers = configuration.disabled_ethereum_providers ?? [];
+    return disabled_ethereum_providers.includes(this.providerName);
   }
 }
