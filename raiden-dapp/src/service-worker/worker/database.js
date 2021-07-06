09726f1eb84b50168530b77f2fc53cd0cf69408d
Thore Strassburg <thore@weilbier.net>
2021-07-06 13:21:01 +0200

    dapp: remove idb package usage for service worker
    While this package makes it super easy to work promise based with the
    indexed database API, it is super hard to write deep tests with it. As
    for the service worker will aim to be partially
    integration tests, it is not just fine to mock the whole package as is.
    I tried to mock the API and still use `idb`. But is was a lot of work
    and in the end it was still not fully working. The package
    implementation is quite complex. Finally it was so much more simple to
    just use the API directly and ditch the package. This allows made the
    mocking very easy as only necessary parts have to be provided.

--- raiden-dapp/src/service-worker/worker/database.js
+++ raiden-dapp/src/service-worker/worker/database.js
@@ -1,16 +1,41 @@
-import { openDB } from 'idb';
 import { cacheNames } from 'workbox-core';
 
 const DATABASE_NAME = 'ServiceWorker';
-const STORE_NAME = cacheNames.precache;
+const OBJECT_STORE_NAME = cacheNames.precache;
 const PRECACHE_ENTRIES_KEY = 'precacheEntries';
 
-async function getDatabase() {
-  return await openDB(DATABASE_NAME, 1, {
-    upgrade(database) {
-      database.createObjectStore(STORE_NAME);
-    },
+function resolveTransaction(transaction) {
+  return new Promise((resolve, reject) => {
+    transaction.addEventListener('complete', () => resolve());
+    transaction.addEventListener('error', () => reject(transaction.error));
   });
+}
+
+function resolveRequest(request) {
+  return new Promise((resolve, reject) => {
+    request.addEventListener('success', () => resolve(request.result));
+    request.addEventListener('error', () => reject(request.error));
+  });
+}
+
+async function getDatabase() {
+  return new Promise((resolve, reject) => {
+    const request = indexedDB.open(DATABASE_NAME);
+    request.addEventListener('success', () => resolve(request.result));
+    request.addEventListener('error', () => reject(request.error));
+    request.addEventListener('upgradeneeded', async () => {
+      const database = request.result;
+      const objectStore = database.createObjectStore(OBJECT_STORE_NAME);
+      await resolveTransaction(objectStore.transaction);
+      resolve(database);
+    });
+  });
+}
+
+async function getPrecacheObjectStore(mode = 'readonly') {
+  const database = await getDatabase();
+  const transaction = database.transaction(OBJECT_STORE_NAME, mode);
+  return transaction.objectStore(OBJECT_STORE_NAME);
 }
 
 /**
@@ -20,8 +45,9 @@ async function getDatabase() {
  * @param precacheEntries - list of precache entries to save
  */
 export async function saveToPreservePrecacheEntries(precacheEntries) {
-  const database = await getDatabase();
-  database.put(STORE_NAME, precacheEntries, PRECACHE_ENTRIES_KEY);
+  const objectStore = await getPrecacheObjectStore('readwrite');
+  const request = objectStore.put(precacheEntries, PRECACHE_ENTRIES_KEY);
+  await resolveRequest(request);
 }
 
 /**
@@ -30,14 +56,16 @@ export async function saveToPreservePrecacheEntries(precacheEntries) {
  * @returns precache entry list or undefined
  */
 export async function getPreservedPrecacheEntries() {
-  const database = await getDatabase();
-  return database.get(STORE_NAME, PRECACHE_ENTRIES_KEY);
+  const objectStore = await getPrecacheObjectStore();
+  const request = objectStore.get(PRECACHE_ENTRIES_KEY);
+  return resolveRequest(request);
 }
 
 /**
  * Remove the list of precache entries in the database.
  */
 export async function deletePreservedPrecacheEntries() {
-  const database = await getDatabase();
-  await database.delete(STORE_NAME, PRECACHE_ENTRIES_KEY);
+  const objectStore = await getPrecacheObjectStore('readwrite');
+  const request = objectStore.delete(PRECACHE_ENTRIES_KEY);
+  await resolveRequest(request);
 }
