4bdbd1f6df379355c4ba3d7ba6d7ae3ebddfe735
Thore Strassburg <thore@weilbier.net>
2021-07-26 14:53:09 +0200

    dApp: use incognito mode for Cypress
    Using the incognito mode makes sure that the whole environment is 100%
    clean. This solves the issue that old test runs affect new ones. The
    manual removal of all possible data in the `beforeAll` and `beforeAll`
    hooks were not reliably working and where a additional work to maintain.
    
    A big part of the diff within this commits comes from some code
    restructuring in the plugin file. It attempts to make the core of the
    plugin easier to read. After all it only got the following additions:
      - force the usage of Chromium based browser engines
      - add the incognito parameter for the browser

--- raiden-dapp/tests/e2e/plugins/index.js
+++ raiden-dapp/tests/e2e/plugins/index.js
@@ -1,46 +1,50 @@
 const path = require('path');
 
-const wp = require('@cypress/webpack-preprocessor');
+const webpackPreprocessor = require('@cypress/webpack-preprocessor');
 
-// https://docs.cypress.io/guides/guides/plugins-guide.html
-
-// if you need a custom webpack configuration you can uncomment the following import
-// and then use the `file:preprocessor` event
-// as explained in the cypress docs
-// https://docs.cypress.io/api/plugins/preprocessors-api.html#Examples
-
-// /* eslint-disable import/no-extraneous-dependencies, global-require */
-// const webpack = require('@cypress/webpack-preprocessor')
+const preprocessingOptions = {
+  webpackOptions: {
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js'],
+      // add the alias object
+      alias: {
+        '@': path.resolve(__dirname, '../../../src'),
+      },
+    },
+    module: {
+      rules: [
+        {
+          // Include ts, tsx, js, and jsx files.
+          test: /\.(ts|js)x?$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+        },
+        {
+          test: /\.js$/,
+          use: ['source-map-loader'],
+          enforce: 'pre',
+        },
+      ],
+    },
+  },
+};
 
 module.exports = (on, config) => {
   require('@cypress/code-coverage/task')(on, config);
-  const options = {
-    webpackOptions: {
-      resolve: {
-        extensions: ['.ts', '.tsx', '.js'],
-        // add the alias object
-        alias: {
-          '@': path.resolve(__dirname, '../../../src'),
-        },
-      },
-      module: {
-        rules: [
-          {
-            // Include ts, tsx, js, and jsx files.
-            test: /\.(ts|js)x?$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader',
-          },
-          {
-            test: /\.js$/,
-            use: ['source-map-loader'],
-            enforce: 'pre',
-          },
-        ],
-      },
-    },
-  };
-  on('file:preprocessor', wp(options));
+
+  on('file:preprocessor', webpackPreprocessor(preprocessingOptions));
+
+  on('before:browser:launch', (browser = {}, launchOptions) => {
+    if (browser.family === 'chromium') {
+      launchOptions.args.push('--incognito');
+    }
+
+    return launchOptions;
+  });
+
+  // Only allow Chromium based browser to make sure the necessary incognito mode
+  // works to get a clean environment.
+  config.browsers = config.browsers.filter((browser) => browser.family === 'chromium');
 
   return Object.assign({}, config);
 };
--- raiden-dapp/tests/e2e/specs/scenario.spec.ts
+++ raiden-dapp/tests/e2e/specs/scenario.spec.ts
@@ -50,13 +50,6 @@ describe('dApp e2e tests', () => {
   const partnerAddress = '0xCBC49ec22c93DB69c78348C90cd03A323267db86';
   const thirdAddres = '0x517aAD51D0e9BbeF3c64803F86b3B9136641D9ec';
 
-  before(async () => {
-    const allIndexedDBs = await window.indexedDB.databases();
-    allIndexedDBs.forEach((indexedDB) => {
-      window.indexedDB.deleteDatabase(indexedDB.name);
-    });
-  });
-
   it('should run all dApp e2e scenarios', () => {
     cy.viewport('macbook-13');
     navigateToDisclaimer();
