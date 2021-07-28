ba03115b8b4b810b0960a9ba839c2b5387ff0646
André Vitor de Lima Matos <andre.vmatos@gmail.com>
2021-07-28 18:43:15 -0300

    deps: sdk: fix strict check of BigNumber instanceof
    By adding a new custom symmetric and assymetric jest matcher:
    expect.toBeBigNumber(), which uses BigNumber.isBigNumber typeguard
    instead of the usual error-prone constructor inheritance check

--- raiden-ts/tests/setupAfter.ts
+++ raiden-ts/tests/setupAfter.ts
@@ -1,6 +1,8 @@
 import 'jest-extended';
 
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import type { BigNumberish } from '@ethersproject/bignumber';
+import { BigNumber } from '@ethersproject/bignumber';
 import type { LogMessage, LogType } from '@jest/console';
 import { CustomConsole } from '@jest/console';
 import util from 'util';
@@ -87,4 +89,31 @@ afterEach(() => {
     testConsole.clear();
   }
   jasmine.currentTest = null;
+});
+
+// expect.toBeBigNumber to match a BigNumber using typeguard instead of instanceof
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace jest {
+    interface Expect {
+      toBeBigNumber(expected?: BigNumberish): any;
+    }
+    interface Matchers<R> {
+      toBeBigNumber(expected?: BigNumberish): R;
+    }
+  }
+}
+
+expect.extend({
+  toBeBigNumber(received?: any, expected?: BigNumberish) {
+    if (!BigNumber.isBigNumber(received))
+      return { pass: false, message: () => `${JSON.stringify(received)} is not a BigNumber` };
+    if (expected && !received.eq(expected)) {
+      return {
+        pass: false,
+        message: () => `expected BigNumber=${expected}, but received=${received.toString()}`,
+      };
+    }
+    return { pass: true, message: () => '' };
+  },
 });
