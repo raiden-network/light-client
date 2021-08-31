2df7d697e7991ca49d5b6bc3853cd7a4b4ba3541
Thore Strassburg <thore@weilbier.net>
2021-08-31 13:42:41 +0200

    dApp: add new channel action form component
    This component is meant to be used for handling the parameters of
    a channel related action (e.g. opening a channel). It consists of the
    three values that relate to channel actions which are the token
    (address), the partner (address) and the token amount (e.g. to deposit).
    Each value can be either just be displayed or is allowed to be edited by
    the user. Values can be also completely hidden if they are clear for the
    context (e.g. acting on a selected channel). Inputs can be restricted
    according to special scenarios (e.g. exclude channel partners when
    opening a new channel).
    
    The form component finally provides a single component which is
    responsible to fetch token information and monitor them. Before (or
    actually still is yet) this was done everywhere we needed it. As this is
    an asynchronous process, this was causing a lot of issues in some
    components. Now they must only handle the addresses as strings and the
    form will do the work.
    
    The form will be bound to a specific action to execute. It also allows
    to customize some labeling for the specific action and some other visual
    adoptions. Of course it only allows to execute the action if all form
    values are valid. This does not apply for fixed values that are not
    editable. They are provided by the parent component and expected to be
    correct. This might be spot to improve at a later point (especially if
    the values come from URL parameters).
    
    The component emits events according to the outcome of the executed
    action.
    
    Note that for the moment the token value of the form is not editable and always
    fixed. This is because we don't have yet a token input component. Only the big
    component for the select token route. This might be added at a later point.

---
+++ raiden-dapp/src/components/channels/ChannelActionForm.vue
@@ -0,0 +1,271 @@
+<template>
+  <div data-cy="channel-action-form">
+    <v-form
+      v-model="inputsAreValid"
+      class="channel-action-form"
+      autocomplete="off"
+      @submit.prevent="submit"
+    >
+      <template v-if="!hideTokenAddress">
+        <address-input
+          v-if="tokenAddressEditable"
+          v-model="tokenAddressInput"
+          class="channel-action-form__token-address channel-action-form__editable-input"
+          :value="tokenAddressInput"
+        />
+
+        <div
+          v-else
+          class="channel-action-form__token-address channel-action-form__fixed-input-wrapper"
+        >
+          <token-information :token="token" />
+        </div>
+      </template>
+
+      <template v-if="!hidePartnerAddress">
+        <address-input
+          v-if="partnerAddressEditable"
+          v-model="partnerAddressInput"
+          class="channel-action-form__partner-address channel-action-form__editable-input"
+          :value="partnerAddressInput"
+          :restricted="restrictedPartnerAddresses"
+          restricted-error-message="channel-action-form.restricted-partner-address-error"
+          :exclude="excludedPartnerAddresses"
+          exclude-error-message="channel-action-form.exclude-partner-address-error"
+        />
+
+        <div
+          v-else
+          class="channel-action-form__partner-address channel-action-form__fixed-input-wrapper"
+        >
+          <address-display
+            :label="$t('channel-action-form.partner')"
+            :address="partnerAddress"
+            full-width
+          />
+        </div>
+      </template>
+
+      <template v-if="!hideTokenAmount">
+        <amount-input
+          v-if="tokenAmountEditable"
+          v-model="tokenAmountInput"
+          class="channel-action-form__token-amount channel-action-form__editable-input"
+          data-cy="channel-action-form__token-amount__input"
+          :token="token"
+          :placeholder="$t('channel-action-form.amount')"
+          :max="tokenAmountLimit"
+          :limit="!!tokenAmountLimit"
+        />
+
+        <div
+          v-else
+          class="channel-action-form__token-amount channel-action-form__fixed-input-wrapper"
+        >
+          <amount-display
+            :label="$t('channel-action-form.amount')"
+            :amount="tokenAmount"
+            :token="token"
+            exact-amount
+            full-width
+          />
+        </div>
+      </template>
+
+      <action-button
+        data-cy="channel-action-form__button"
+        :text="confirmButtonLabel"
+        :enabled="inputsAreValid"
+        :sticky="stickyButton"
+      />
+    </v-form>
+  </div>
+</template>
+
+<script lang="ts">
+import { BigNumber } from 'ethers';
+import { Component, Prop, Vue, Watch } from 'vue-property-decorator';
+import { mapGetters } from 'vuex';
+
+import type { RaidenChannel } from 'raiden-ts';
+
+import ActionButton from '@/components/ActionButton.vue';
+import AddressDisplay from '@/components/AddressDisplay.vue';
+import AddressInput from '@/components/AddressInput.vue';
+import AmountDisplay from '@/components/AmountDisplay.vue';
+import AmountInput from '@/components/AmountInput.vue';
+import TokenInformation from '@/components/TokenInformation.vue';
+import type { Token } from '@/model/types';
+import { BalanceUtils } from '@/utils/balance-utils';
+
+@Component({
+  components: {
+    ActionButton,
+    AddressDisplay,
+    AddressInput,
+    AmountDisplay,
+    AmountInput,
+    TokenInformation,
+  },
+  computed: {
+    ...mapGetters({
+      getToken: 'token',
+      getChannels: 'channels',
+    }),
+  },
+})
+export default class ChannelActionForm extends Vue {
+  @Prop({ type: String, default: '' })
+  readonly tokenAddress!: string;
+
+  @Prop({ type: Boolean, default: false })
+  readonly hideTokenAddress!: boolean;
+
+  // Future prop when we get a token input (dropdown)
+  // @Prop({ required: false, type: Boolean, default: false })
+  readonly tokenAddressEditable = false;
+
+  @Prop({ type: String, default: '' })
+  readonly partnerAddress!: string;
+
+  @Prop({ type: Boolean, default: false })
+  readonly hidePartnerAddress!: boolean;
+
+  @Prop({ type: Boolean, default: false })
+  readonly partnerAddressEditable!: boolean;
+
+  @Prop({ type: Boolean, default: false })
+  readonly restrictToChannelPartners!: boolean;
+
+  @Prop({ type: Boolean, default: false })
+  readonly excludeChannelPartners!: boolean;
+
+  @Prop({ type: String, default: '' })
+  readonly tokenAmount!: string;
+
+  @Prop({ type: Boolean, default: false })
+  readonly tokenAmountEditable!: boolean;
+
+  @Prop({ type: Boolean, default: false })
+  readonly limitToTokenBalance!: boolean;
+
+  @Prop({ type: Boolean, default: false })
+  readonly limitToChannelWithdrawable!: boolean;
+
+  @Prop({ type: Boolean, default: false })
+  readonly hideTokenAmount!: boolean;
+
+  @Prop({ type: String, required: true })
+  readonly confirmButtonLabel!: string;
+
+  @Prop({ type: Boolean, default: false })
+  readonly stickyButton!: boolean;
+
+  @Prop({ required: true })
+  readonly runAction!: (options: unknown) => Promise<void>;
+
+  getToken!: (address: string) => Token;
+  getChannels!: (address: string) => RaidenChannel[];
+
+  tokenAddressInput = '';
+  partnerAddressInput = '';
+  tokenAmountInput = '';
+  inputsAreValid = false;
+
+  @Watch('tokenAddress', { immediate: true })
+  async onTokenAddressChange(tokenAddress: string): Promise<void> {
+    // TODO: This needs to be extended to also update if the user
+    // selects/changes the token when the token address becomes editable.
+    this.tokenAddressInput = tokenAddress;
+    await this.$raiden.fetchAndUpdateTokenData([tokenAddress]);
+    await this.$raiden.monitorToken(tokenAddress);
+  }
+
+  @Watch('partnerAddress', { immediate: true })
+  onPartnerAddressChange(partnerAddress: string): void {
+    this.partnerAddressInput = partnerAddress;
+  }
+
+  @Watch('tokenAmount', { immediate: true })
+  onTokenAmountChange(tokenAmount: string): void {
+    this.tokenAmountInput = tokenAmount;
+  }
+
+  get token(): Token {
+    return this.getToken(this.tokenAddressInput);
+  }
+
+  get channelPartnerAddresses(): Array<string> | undefined {
+    const channels = this.getChannels(this.tokenAddressInput);
+    return channels.map((channel) => channel.partner);
+  }
+
+  get restrictedPartnerAddresses(): Array<string> | undefined {
+    if (this.restrictToChannelPartners) {
+      return this.channelPartnerAddresses;
+    }
+  }
+
+  get excludedPartnerAddresses(): Array<string> | undefined {
+    if (this.excludeChannelPartners) {
+      return this.channelPartnerAddresses;
+    }
+  }
+
+  get tokenAmountLimit(): BigNumber | undefined {
+    if (this.limitToTokenBalance) {
+      return BigNumber.from(this.token.balance);
+    } else if (this.limitToChannelWithdrawable) {
+      const channels = this.getChannels(this.tokenAddressInput);
+      const channelWithPartner = channels.filter(
+        (channel) => channel.partner == this.partnerAddressInput,
+      )[0];
+      return channelWithPartner?.ownWithdrawable;
+    }
+  }
+
+  get parsedTokenAmount(): BigNumber {
+    return BalanceUtils.parse(this.tokenAmountInput, this.token.decimals!);
+  }
+
+  async submit(): Promise<void> {
+    if (!this.inputsAreValid) {
+      return;
+    }
+
+    await this.runAction({
+      tokenAddress: this.tokenAddressInput,
+      partnerAddress: this.partnerAddressInput,
+      tokenAmount: this.parsedTokenAmount,
+    });
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/scss/colors';
+
+.channel-action-form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  &__fixed-input-wrapper {
+    display: flex;
+    align-items: center;
+    width: 100%;
+    height: 48px;
+    margin: 10px 0;
+    padding: 0 22px 0 16px;
+    border-radius: 8px;
+    background-color: $transfer-screen-bg-color;
+  }
+
+  &__editable-input {
+    width: 100%;
+
+    // Avoid jumping due to validation messages
+    min-height: 110px;
+  }
+}
+</style>
---
+++ raiden-dapp/tests/unit/components/channels/channel-action-form.spec.ts
@@ -0,0 +1,365 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { $t } from '../../utils/mocks';
+
+import type { Wrapper } from '@vue/test-utils';
+import { shallowMount } from '@vue/test-utils';
+import { BigNumber } from 'ethers';
+import Vue from 'vue';
+import Vuetify from 'vuetify';
+import Vuex from 'vuex';
+
+import type { Address, RaidenChannel, UInt } from 'raiden-ts';
+
+import ActionButton from '@/components/ActionButton.vue';
+import ChannelActionForm from '@/components/channels/ChannelActionForm.vue';
+import type { Token } from '@/model/types';
+
+import { generateChannel, generateToken } from '../../utils/data-generator';
+
+Vue.use(Vuex);
+
+const vuetify = new Vuetify();
+const token = generateToken();
+
+async function createWrapper(options?: {
+  tokenAddress?: string;
+  hideTokenAddress?: boolean;
+  hidePartnerAddress?: boolean;
+  token?: Token;
+  partnerAddress?: string;
+  partnerAddressEditable?: boolean;
+  restrictToChannelPartners?: boolean;
+  excludeChannelPartners?: boolean;
+  channels?: RaidenChannel[];
+  tokenAmount?: string;
+  hideTokenAmount?: boolean;
+  tokenAmountEditable?: boolean;
+  limitToTokenBalance?: boolean;
+  limitToChannelWithdrawable?: boolean;
+  confirmButtonLabel?: string;
+  stickyButton?: boolean;
+  runAction?: () => void;
+  inputsAreValid?: boolean;
+  fetchAndUpdateTokenData?: () => void;
+  monitorToken?: () => void;
+}): Promise<Wrapper<ChannelActionForm>> {
+  const getters = {
+    token: () => () => options?.token ?? token,
+    channels: () => () => options?.channels ?? [],
+  };
+  const store = new Vuex.Store({ getters });
+
+  const $raiden = {
+    fetchAndUpdateTokenData: options?.fetchAndUpdateTokenData ?? jest.fn(),
+    monitorToken: options?.monitorToken ?? jest.fn(),
+  };
+
+  const wrapper = shallowMount(ChannelActionForm, {
+    vuetify,
+    store,
+    mocks: { $raiden, $t },
+    propsData: {
+      tokenAddress: options?.tokenAddress,
+      hideTokenAddress: options?.hideTokenAddress,
+      partnerAddress: options?.partnerAddress,
+      hidePartnerAddress: options?.hidePartnerAddress,
+      partnerAddressEditable: options?.partnerAddressEditable,
+      restrictToChannelPartners: options?.restrictToChannelPartners,
+      excludeChannelPartners: options?.excludeChannelPartners,
+      tokenAmount: options?.tokenAmount,
+      hideTokenAmount: options?.hideTokenAmount,
+      tokenAmountEditable: options?.tokenAmountEditable,
+      limitToTokenBalance: options?.limitToTokenBalance,
+      limitToChannelWithdrawable: options?.limitToChannelWithdrawable,
+      confirmButtonLabel: options?.confirmButtonLabel ?? 'confirm label',
+      stickyButton: options?.stickyButton,
+      runAction: options?.runAction ?? jest.fn(),
+    },
+    stubs: ['v-form'],
+  });
+
+  if (options?.inputsAreValid !== undefined) {
+    (wrapper.vm as any).inputsAreValid = options.inputsAreValid;
+    await wrapper.vm.$nextTick();
+  }
+
+  return wrapper;
+}
+
+async function submitForm(wrapper: Wrapper<ChannelActionForm>): Promise<void> {
+  const form = wrapper.get('.channel-action-form');
+
+  form.vm.$emit('submit', { preventDefault: () => null });
+  await wrapper.vm.$nextTick();
+}
+
+describe('ChannelActionForm.vue', () => {
+  describe('token address', () => {
+    test('hides form section', async () => {
+      const wrapper = await createWrapper({ hideTokenAddress: true });
+      const display = wrapper.find(
+        '.channel-action-form__token-address.channel-action-form__fixed-input-wrapper',
+      );
+      const input = wrapper.find(
+        '.channel-action-form__token-address.channel-action-form__editable-input',
+      );
+
+      expect(display.exists()).toBeFalsy();
+      expect(input.exists()).toBeFalsy();
+    });
+
+    test('only displays value if not set to be editable', async () => {
+      // Note that this is atm the default as the token address is not editable yet.
+      const wrapper = await createWrapper({ tokenAddress: token.address });
+      const display = wrapper.find(
+        '.channel-action-form__token-address.channel-action-form__fixed-input-wrapper',
+      );
+      const input = wrapper.find(
+        '.channel-action-form__token-address.channel-action-form__editable-input',
+      );
+
+      expect(display.exists()).toBeTruthy();
+      expect(display.html()).toContain(token);
+      expect(input.exists()).toBeFalsy();
+    });
+
+    test('updates token information initially and on change', async () => {
+      const fetchAndUpdateTokenData = jest.fn();
+      const monitorToken = jest.fn();
+      const wrapper = await createWrapper({
+        tokenAddress: '0xToken',
+        fetchAndUpdateTokenData,
+        monitorToken,
+      });
+
+      expect(fetchAndUpdateTokenData).toHaveBeenCalledTimes(1);
+      expect(fetchAndUpdateTokenData).toHaveBeenLastCalledWith(['0xToken']);
+      expect(monitorToken).toHaveBeenCalledTimes(1);
+      expect(monitorToken).toHaveBeenLastCalledWith('0xToken');
+
+      wrapper.setProps({ ...wrapper.props, tokenAddress: '0xOtherToken' });
+      await wrapper.vm.$nextTick();
+
+      expect(fetchAndUpdateTokenData).toHaveBeenCalledTimes(2);
+      expect(fetchAndUpdateTokenData).toHaveBeenLastCalledWith(['0xOtherToken']);
+      expect(monitorToken).toHaveBeenCalledTimes(2);
+      expect(monitorToken).toHaveBeenLastCalledWith('0xOtherToken');
+    });
+  });
+
+  describe('partner address', () => {
+    test('hides form section', async () => {
+      const wrapper = await createWrapper({ hidePartnerAddress: true });
+      const display = wrapper.find(
+        '.channel-action-form__partner-address.channel-action-form__fixed-input-wrapper',
+      );
+      const input = wrapper.find(
+        '.channel-action-form__partner-address.channel-action-form__editable-input',
+      );
+
+      expect(display.exists()).toBeFalsy();
+      expect(input.exists()).toBeFalsy();
+    });
+
+    test('only displays value if not set to be editable', async () => {
+      const wrapper = await createWrapper({
+        partnerAddressEditable: false,
+        partnerAddress: '0xPartner',
+      });
+      const display = wrapper.find(
+        '.channel-action-form__partner-address.channel-action-form__fixed-input-wrapper',
+      );
+      const input = wrapper.find(
+        '.channel-action-form__partner-address.channel-action-form__editable-input',
+      );
+
+      expect(display.exists()).toBeTruthy();
+      expect(display.html()).toContain('0xPartner');
+      expect(input.exists()).toBeFalsy();
+    });
+
+    test('allows to edit value if set so', async () => {
+      const wrapper = await createWrapper({
+        partnerAddressEditable: true,
+        partnerAddress: '0xPartner',
+      });
+      const display = wrapper.find(
+        '.channel-action-form__partner-address.channel-action-form__fixed-input-wrapper',
+      );
+      const input = wrapper.find(
+        '.channel-action-form__partner-address.channel-action-form__editable-input',
+      );
+
+      expect(display.exists()).toBeFalsy();
+      expect(input.exists()).toBeTruthy();
+      expect(input.html()).toContain('0xPartner');
+    });
+
+    test('restricts to existing channel partners if set so', async () => {
+      const channel = generateChannel({ partner: '0xPartner' as Address });
+      const wrapper = await createWrapper({
+        restrictToChannelPartners: true,
+        partnerAddressEditable: true,
+        tokenAddress: channel.token,
+        channels: [channel],
+      });
+      const input = wrapper.get(
+        '.channel-action-form__partner-address.channel-action-form__editable-input',
+      );
+
+      expect(input.html()).toContain('0xPartner');
+    });
+
+    test('excludes existing channel partners if set so', async () => {
+      const channel = generateChannel({ partner: '0xPartner' as Address });
+      const wrapper = await createWrapper({
+        excludeChannelPartners: true,
+        partnerAddressEditable: true,
+        tokenAddress: channel.token,
+        channels: [channel],
+      });
+      const input = wrapper.get(
+        '.channel-action-form__partner-address.channel-action-form__editable-input',
+      );
+
+      expect(input.html()).toContain('0xPartner');
+    });
+  });
+
+  describe('token amount', () => {
+    test('hides form section if set so', async () => {
+      const wrapper = await createWrapper({ hideTokenAmount: true });
+      const display = wrapper.find(
+        '.channel-action-form__token-amount.channel-action-form__fixed-input-wrapper',
+      );
+      const input = wrapper.find(
+        '.channel-action-form__token-amount.channel-action-form__editable-input',
+      );
+
+      expect(display.exists()).toBeFalsy();
+      expect(input.exists()).toBeFalsy();
+    });
+
+    test('only displays value if not set to be editable', async () => {
+      const wrapper = await createWrapper({ tokenAmountEditable: false, tokenAmount: '0.1' });
+      const display = wrapper.find(
+        '.channel-action-form__token-amount.channel-action-form__fixed-input-wrapper',
+      );
+      const input = wrapper.find(
+        '.channel-action-form__token-amount.channel-action-form__editable-input',
+      );
+
+      expect(display.exists()).toBeTruthy();
+      expect(display.html()).toContain('0.1');
+      expect(input.exists()).toBeFalsy();
+    });
+
+    test('allows to edit value if set so', async () => {
+      const wrapper = await createWrapper({ tokenAmountEditable: true, tokenAmount: '0.1' });
+      const display = wrapper.find(
+        '.channel-action-form__token-amount.channel-action-form__fixed-input-wrapper',
+      );
+      const input = wrapper.find(
+        '.channel-action-form__token-amount.channel-action-form__editable-input',
+      );
+
+      expect(display.exists()).toBeFalsy();
+      expect(input.exists()).toBeTruthy();
+      expect(input.html()).toContain('0.1');
+    });
+
+    test('limits input to token balance if set so', async () => {
+      const token = generateToken({ balance: '10' });
+      const wrapper = await createWrapper({
+        limitToTokenBalance: true,
+        tokenAmountEditable: true,
+        tokenAddress: token.address,
+        token,
+      });
+      const input = wrapper.get(
+        '.channel-action-form__token-amount.channel-action-form__editable-input',
+      );
+
+      expect(input.html()).toContain('10');
+    });
+
+    test('limits input to channel withdrawable if set so', async () => {
+      const channel = generateChannel({ ownWithdrawable: BigNumber.from('10') as UInt<32> });
+      const wrapper = await createWrapper({
+        limitToChannelWithdrawable: true,
+        tokenAmountEditable: true,
+        tokenAddress: channel.token,
+        partnerAddress: channel.partner,
+        channels: [channel],
+      });
+      const input = wrapper.get(
+        '.channel-action-form__token-amount.channel-action-form__editable-input',
+      );
+
+      expect(input.html()).toContain('10');
+    });
+  });
+
+  describe('action button', () => {
+    test('displays set label', async () => {
+      const wrapper = await createWrapper({ confirmButtonLabel: 'test label' });
+      const button = wrapper.findComponent(ActionButton);
+
+      expect(button.html()).toContain('test label');
+    });
+
+    test('is disabled if inputs are not valid', async () => {
+      const wrapper = await createWrapper({ inputsAreValid: false });
+      const button = wrapper.findComponent(ActionButton);
+
+      expect(button.attributes('enabled')).toBeFalsy();
+    });
+
+    test('is enabled if inputs are valid', async () => {
+      const wrapper = await createWrapper({ inputsAreValid: true });
+      const button = wrapper.findComponent(ActionButton);
+
+      expect(button.attributes('enabled')).toBeTruthy();
+    });
+
+    test('is sticky if set so', async () => {
+      const wrapper = await createWrapper({ stickyButton: true });
+      const button = wrapper.findComponent(ActionButton);
+
+      expect(button.attributes('sticky')).toBeTruthy();
+    });
+  });
+
+  describe('submit', () => {
+    test('executes given action with all form values', async () => {
+      const token = generateToken({ address: '0xToken', decimals: 0 });
+      const runAction = jest.fn();
+      const wrapper = await createWrapper({
+        tokenAddress: token.address,
+        token,
+        partnerAddress: '0xPartner',
+        tokenAmount: '1',
+        inputsAreValid: true,
+        runAction,
+      });
+
+      await submitForm(wrapper);
+
+      expect(runAction).toHaveBeenCalledTimes(1);
+      expect(runAction).toHaveBeenLastCalledWith({
+        tokenAddress: '0xToken',
+        partnerAddress: '0xPartner',
+        tokenAmount: BigNumber.from('1'),
+      });
+    });
+
+    test('does not execute action if form is invalid', async () => {
+      const runAction = jest.fn();
+      const wrapper = await createWrapper({ inputsAreValid: false, runAction });
+
+      await submitForm(wrapper);
+
+      expect(runAction).not.toHaveBeenCalled();
+    });
+  });
+});
