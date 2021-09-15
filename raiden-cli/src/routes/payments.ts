e5aeff35301dd067f0332962c80637ccefd836a3
André Vitor de Lima Matos <andre@brainbot.com>
2021-09-15 17:48:39 -0300

    cli: passthrough /payments body, including 'paths'
    If options.paths doesn't match the format, E400 is returned.

--- raiden-cli/src/routes/payments.ts
+++ raiden-cli/src/routes/payments.ts
@@ -86,7 +86,11 @@ async function doTransfer(this: Cli, request: Request, response: Response, next:
       request.params.tokenAddress,
       request.params.targetAddress,
       request.body.amount.toString(),
-      { paymentId: request.body.identifier?.toString(), lockTimeout: request.body.lock_timeout },
+      {
+        ...request.body,
+        paymentId: request.body.identifier?.toString(),
+        lockTimeout: request.body.lock_timeout,
+      },
     );
     const transfer = await this.raiden.waitTransfer(transferKey);
     response.send(transformSdkTransferToApiPayment(transfer));
