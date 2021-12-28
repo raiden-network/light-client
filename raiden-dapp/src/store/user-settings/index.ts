619a1c4791bfdaf5eef7c99659b781da5cad0e6e
Thore Strassburg <thore@weilbier.net>
2021-12-28 18:04:16 +0100

    dapp: enable testing for store module persistence
    Some Vuex store modules have a plugin that ensure that the modules state gets
    persisted to a storage backend. This ensures that the state survives a reload of
    the application. This feature is mostly implemented by a 3rd party library and
    was never tested. But it is an important feature of the dApp, especially for
    upcoming new store modules. Therefore we need to ensure that it works as intended.
    To enable testing, the store module plugins get now created by a function. This
    allows to specify an optional parameter to define the storage backend. Per
    default this is `window.localStorage` as always. The tests for the store module
    can now simply provide a mocked version of the storage backend. It is a clean
    separation of modules with clear interfaces. No need to mock global variables in
    an awkward manner. The test cases do not hide anything and can be read and
    understood easily.

--- raiden-dapp/src/store/user-settings/index.ts
+++ raiden-dapp/src/store/user-settings/index.ts
@@ -1,4 +1,4 @@
-import type { Module } from 'vuex';
+import type { Module, Plugin } from 'vuex';
 import { VuexPersistence } from 'vuex-persist';
 
 import { getters } from './getters';
@@ -13,9 +13,21 @@ export const userSettings: Module<UserSettingsState, RootStateWithUserSettings> 
   mutations,
 };
 
-export const userSettingsLocalStorage = new VuexPersistence<RootStateWithUserSettings>({
-  key: 'raiden_dapp_settings',
-  modules: ['userSettings'],
-});
+/**
+ * @param storage - object implementing the Storage interface to persist the
+ *                  state (defaults to `window.localStorage`)
+ * @returns plugin for the root store to persist data
+ */
+export function createUserSettingsPersistencePlugin(
+  storage: Storage = window.localStorage,
+): Plugin<RootStateWithUserSettings> {
+  const vuexPersistence = new VuexPersistence<RootStateWithUserSettings>({
+    key: 'raiden_dapp_settings',
+    storage: storage,
+    modules: ['userSettings'],
+  });
+
+  return vuexPersistence.plugin;
+}
 
 export * from './types';
