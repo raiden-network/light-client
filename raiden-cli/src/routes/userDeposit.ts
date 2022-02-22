a0af2d11096112e25fd43c614652163aba192294
André Vitor de Lima Matos <andre@brainbot.com>
2022-02-22 11:55:12 -0300

    cli: add some more information to address and user_deposit endpoints

--- raiden-cli/src/routes/userDeposit.ts
+++ raiden-cli/src/routes/userDeposit.ts
@@ -109,8 +109,12 @@ async function determineAndExecuteRequestedInteraction(
 async function getUDCInfo(this: Cli, _request: Request, response: Response): Promise<void> {
   const balance = await this.raiden.getUDCCapacity();
   const totalDeposit = await this.raiden.getUDCTotalDeposit();
+  const udcTokenAddress = await this.raiden.userDepositTokenAddress();
+  const udcAddress = this.raiden.contractsInfo.UserDeposit.address;
 
   response.json({
+    user_deposit_token_address: udcTokenAddress,
+    udc_address: udcAddress,
     balance: balance.toString(),
     total_deposit: totalDeposit.toString(),
   });
