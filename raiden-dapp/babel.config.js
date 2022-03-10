9b9e6bb01c44440a94e26cb5df2ee7ef9881e38b
André Vitor de Lima Matos <andre@brainbot.com>
2022-03-09 21:47:38 -0300

    deps: dapp: add @babel/plugin-proposal-class-static-block
    needed after TS@4.6.2

--- raiden-dapp/babel.config.js
+++ raiden-dapp/babel.config.js
@@ -1,6 +1,7 @@
 const plugins = [
   '@babel/plugin-proposal-nullish-coalescing-operator',
   '@babel/plugin-proposal-optional-chaining',
+  '@babel/plugin-proposal-class-static-block',
 ];
 
 if (process.env.E2E) {
